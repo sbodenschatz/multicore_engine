be27b89cd74af4be491620cf631656a6310a8ddf
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-10-04 01:27:34 +0200

    Fixed typo in directory name.

--- multicore_engine_demo/src/main.cpp
+++ multicore_engine_demo/src/main.cpp
@@ -12,46 +12,45 @@
 struct X {
 	int v;
 	int dummy[63];
-	X(int v) :
-			v(v) {
-//		std::cout<<"Constr. "<<v<<std::endl;
+	X(int v) : v(v) {
+		// std::cout<<"Constr. "<<v<<std::endl;
 	}
 	~X() {
-//		std::cout<<"Destr. "<<v<<std::endl;
+		// std::cout<<"Destr. "<<v<<std::endl;
 	}
 };
 
-static const size_t block_size=1024;
+static const size_t block_size = 1024;
 
-void test_run(int runs, int objects, mce::containers::unordered_object_pool<X, block_size>& uop, int outer_run) {
+void test_run(int runs, int objects, mce::containers::unordered_object_pool<X, block_size>& uop,
+			  int outer_run) {
 	for(int run = 0; run < runs; ++run) {
 		if(run % ((runs / 10) ? runs / 10 : 1) == 0) std::cout << outer_run << " " << run;
-		for(int i = 0; i < objects; ++i)
-			uop.emplace(i);
+		for(int i = 0; i < objects; ++i) uop.emplace(i);
 
-//		std::cout<<std::endl;
+		// std::cout<<std::endl;
 		auto& test_obj = *(uop.begin());
-//		std::cout<<test_obj.v<<"###";
-//		auto test_obj_it = uop.find(test_obj);
-//		assert(test_obj_it==uop.begin());
+		// std::cout<<test_obj.v<<"###";
+		// auto test_obj_it = uop.find(test_obj);
+		// assert(test_obj_it==uop.begin());
 		uop.find_and_erase(test_obj);
 
 		const X* x = uop.insert(X(42424242));
-		assert(x->v==42424242);
+		assert(x->v == 42424242);
 		(void)x;
 
 		{
-			auto deleter = [&](X* d){
-				uop.find_and_erase(*d);//Not optimal performance-wise
+			auto deleter = [&](X* d) {
+				uop.find_and_erase(*d); // Not optimal performance-wise
 			};
-			auto x2 = std::unique_ptr<X,decltype(deleter)>(uop.emplace(X(424242)),deleter);
-			assert(x2->v==424242);
+			auto x2 = std::unique_ptr<X, decltype(deleter)>(uop.emplace(X(424242)), deleter);
+			assert(x2->v == 424242);
 		}
 
-//		for (auto& v : uop) {
-//				std::cout<<v.v<<" ";
-//		}
-//		std::cout<<std::endl;
+		//		for (auto& v : uop) {
+		//				std::cout<<v.v<<" ";
+		//		}
+		//		std::cout<<std::endl;
 
 		auto it = uop.begin();
 		for(int i = 0; i < 32; ++i) {
@@ -61,18 +60,16 @@ void test_run(int runs, int objects, mce::containers::unordered_object_pool<X, b
 			it = uop.erase(it);
 		}
 		uop.reorganize();
-		//for(int i = objects; i < objects + 100; ++i)
+		// for(int i = objects; i < objects + 100; ++i)
 		//	uop.emplace(i);
 
-		//auto it2 = uop.begin();
-		//std::advance(it2, 20);
-		//uop.erase(uop.begin(), it2);
+		// auto it2 = uop.begin();
+		// std::advance(it2, 20);
+		// uop.erase(uop.begin(), it2);
 
 		if(run % ((runs / 10) ? runs / 10 : 1) == 0)
 			std::cout << " " << uop.size() << " " << uop.capacity() << std::endl;
-		for (auto& v : uop) {
-				std::cout<<v.v<<std::endl;
-		}
+		for(auto& v : uop) { std::cout << v.v << std::endl; }
 	}
 }
 
@@ -83,7 +80,7 @@ int main() {
 	for(int outer_run = 0; outer_run < 1; ++outer_run) {
 		test_run(1, 0x80, uop, outer_run);
 		if(outer_run % 10) uop.clear();
-		//else uop.clear_and_reorganize();
+		// else uop.clear_and_reorganize();
 	}
 
 	mce::containers::unordered_object_pool<X, block_size> uop3 = uop;
@@ -94,4 +91,3 @@ int main() {
 	std::chrono::duration<double> diff = end - start;
 	std::cout << "Test " << diff.count() << std::endl;
 }
-
