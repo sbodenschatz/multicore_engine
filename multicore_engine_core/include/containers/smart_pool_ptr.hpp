e5cf9de57121da18a5a66f606fe6dd0ae967a490
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-10-07 21:37:49 +0200

    Implemented weak_pool_ptr.
    Refs #1

--- multicore_engine_core/include/containers/smart_pool_ptr.hpp
+++ multicore_engine_core/include/containers/smart_pool_ptr.hpp
@@ -7,11 +7,16 @@
 #ifndef CONTAINERS_SMART_POOL_PTR_HPP_
 #define CONTAINERS_SMART_POOL_PTR_HPP_
 
+#include <utility>
+
 namespace mce {
 namespace containers {
 
 template <typename T>
 class weak_pool_ptr;
+
+template <typename U, size_t block_size>
+class smart_object_pool;
 
 namespace detail {
 // Interface used for type erasure to remove template parameters of the pool
@@ -38,6 +43,8 @@ class smart_pool_ptr {
 
 	template <typename U, size_t block_size>
 	friend class smart_object_pool;
+	template <typename U>
+	friend class weak_pool_ptr;
 
 	smart_pool_ptr(T* object, detail::smart_object_pool_block_interface* block) noexcept
 			: object{object},
@@ -194,6 +201,167 @@ public:
 	}
 };
 
+// Interface loosely follows that of std::weak_ptr.
+template <typename T>
+class weak_pool_ptr {
+	T* object;
+	void* managed_object; // May differ from object if this weak_pool_ptr was constructed using the aliasing
+						  // constructor
+	detail::smart_object_pool_block_interface* block;
+
+public:
+	template <typename U>
+	friend class smart_pool_ptr;
+
+	typedef detail::smart_object_pool_block_interface::ref_count_t ref_count_t;
+	weak_pool_ptr() noexcept : object{nullptr}, managed_object{nullptr}, block{nullptr} {}
+	weak_pool_ptr(const weak_pool_ptr& other) noexcept : object{other.object},
+														 managed_object{other.managed_object},
+														 block{other.block} {
+		if(block) block->increment_weak_ref(managed_object);
+	}
+	weak_pool_ptr(weak_pool_ptr&& other) noexcept : object{other.object},
+													managed_object{other.managed_object},
+													block{other.block} {
+		other.object = nullptr;
+		other.managed_object = nullptr;
+		other.block = nullptr;
+	}
+	// TODO: Verify is this satisfies the requirement that also applies to the equivalent in std::shared_ptr
+	// to only participate in overload resolution if U* is convertible to T*.
+	template <typename U>
+	weak_pool_ptr(const weak_pool_ptr<U>& other) noexcept : object(other.object),
+															managed_object{other.managed_object},
+															block{other.block} {
+		if(block) block->increment_weak_ref(managed_object);
+	}
+	// TODO: Verify is this satisfies the requirement that also applies to the equivalent in std::shared_ptr
+	// to only participate in overload resolution if U* is convertible to T*.
+	template <typename U>
+	weak_pool_ptr(weak_pool_ptr<U>&& other) noexcept : object(other.object),
+													   managed_object{other.managed_object},
+													   block{other.block} {
+		other.object = nullptr;
+		other.managed_object = nullptr;
+		other.block = nullptr;
+	}
+	template <typename U>
+	weak_pool_ptr(smart_pool_ptr<U>&& other)
+			: object(other.object), managed_object{other.managed_object}, block{other.block} {
+		if(block) block->increment_weak_ref(managed_object);
+	}
+	weak_pool_ptr<T>& operator=(const weak_pool_ptr& other) noexcept {
+		if(other.managed_object == managed_object) {
+			object = other.object;
+			return *this;
+		}
+		if(block) block->decrement_weak_ref(managed_object);
+		object = other.object;
+		managed_object = other.managed_object;
+		block = other.block;
+		if(block) block->increment_weak_ref(managed_object);
+		return *this;
+	}
+	// TODO: Verify is this satisfies the requirement that also applies to the equivalent in std::shared_ptr
+	// to only participate in overload resolution if U* is convertible to T*.
+	template <typename U>
+	weak_pool_ptr<T>& operator=(const weak_pool_ptr<U>& other) noexcept {
+		if(other.managed_object == managed_object) {
+			object = other.object;
+			return *this;
+		}
+		if(block) block->decrement_weak_ref(managed_object);
+		object = other.object;
+		managed_object = other.managed_object;
+		block = other.block;
+		if(block) block->increment_weak_ref(managed_object);
+		return *this;
+	}
+	template <typename U>
+	weak_pool_ptr<T>& operator=(const smart_pool_ptr<U>& other) noexcept {
+		if(other.managed_object == managed_object) {
+			object = other.object;
+			return *this;
+		}
+		if(block) block->decrement_weak_ref(managed_object);
+		object = other.object;
+		managed_object = other.managed_object;
+		block = other.block;
+		if(block) block->increment_weak_ref(managed_object);
+		return *this;
+	}
+	weak_pool_ptr<T>& operator=(weak_pool_ptr&& other) noexcept {
+		assert(this != &other);
+		if(block) block->decrement_weak_ref(managed_object);
+		object = other.object;
+		managed_object = other.managed_object;
+		block = other.block;
+		other.object = nullptr;
+		other.managed_object = nullptr;
+		other.block = nullptr;
+		return *this;
+	}
+	// TODO: Verify is this satisfies the requirement that also applies to the equivalent in std::shared_ptr
+	// to only participate in overload resolution if U* is convertible to T*.
+	template <typename U>
+	weak_pool_ptr<T>& operator=(weak_pool_ptr<U>&& other) noexcept {
+		assert(this != &other);
+		if(block) block->decrement_weak_ref(managed_object);
+		object = other.object;
+		managed_object = other.managed_object;
+		block = other.block;
+		other.object = nullptr;
+		other.managed_object = nullptr;
+		other.block = nullptr;
+		return *this;
+	}
+	~weak_pool_ptr() {
+		if(block) block->decrement_weak_ref(managed_object);
+	}
+
+	void swap(weak_pool_ptr& other) noexcept {
+		using std::swap;
+		swap(object, other.object);
+		swap(managed_object, other.managed_object);
+		swap(block, other.block);
+	}
+
+	T* get() const noexcept {
+		return object;
+	}
+
+	T& operator*() const noexcept {
+		assert(block);
+		assert(object);
+		return *object;
+	}
+
+	T* operator->() const noexcept {
+		assert(block);
+		assert(object);
+		return object;
+	}
+
+	ref_count_t use_count() const noexcept {
+		if(!block) return 0;
+		if(!object) return 0;
+		auto rc = block->strong_ref_count(managed_object);
+		assert(rc > 0);
+		return rc;
+	}
+
+	bool unique() const noexcept {
+		return use_count() == 1;
+	}
+
+	explicit operator bool() const noexcept {
+		if(!block) return false;
+		if(object)
+			return true;
+		else
+			return false;
+	}
+};
 } // namespace containers
 } // namespace mce
 
