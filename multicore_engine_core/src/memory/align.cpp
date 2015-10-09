c06c6f923724b35734bd3ff35e782af3bdc9400e
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-10-09 08:33:45 +0200

    Added missing <cstdint> include in align.cpp

--- multicore_engine_core/src/memory/align.cpp
+++ multicore_engine_core/src/memory/align.cpp
@@ -5,12 +5,13 @@
  */
 
 #include <cstddef>
+#include <cstdint>
 
 namespace mce {
 namespace memory {
 
 // Implements the same task as std::align to compensate for lacking compiler support.
-void* align(std::size_t alignment, std::size_t size, void*& ptr, std::size_t& space) {
+void* align(size_t alignment, size_t size, void*& ptr, size_t& space) {
 	char* tmp_ptr = reinterpret_cast<char*>(ptr);
 	uintptr_t tmp_int = reinterpret_cast<uintptr_t>(tmp_ptr);
 	size_t unalignment = tmp_int & (alignment-1);
