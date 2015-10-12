5da830487383de79764714819381cfedb1137450
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-10-12 17:20:58 +0200

    Added basic thread safety unit test for smart_object_pool.
    Refs #1

--- multicore_engine_core_tests/src/containers/smartobjectpool_test.cpp
+++ multicore_engine_core_tests/src/containers/smartobjectpool_test.cpp
@@ -9,6 +9,7 @@
 #include <unordered_set>
 #include <string>
 #include <algorithm>
+#include <future>
 
 namespace mce {
 namespace containers {
@@ -16,10 +17,8 @@ namespace containers {
 struct smart_object_pool_fixture {
 	typedef long long element;
 	mce::containers::smart_object_pool<element> sop;
-	smart_object_pool_fixture() {
-	}
-	~smart_object_pool_fixture() {
-	}
+	smart_object_pool_fixture() {}
+	~smart_object_pool_fixture() {}
 };
 
 BOOST_AUTO_TEST_SUITE(containers)
@@ -28,37 +27,65 @@ BOOST_FIXTURE_TEST_SUITE(smart_object_pool_test, smart_object_pool_fixture)
 BOOST_AUTO_TEST_CASE(emplace_and_destroy_one) {
 	auto ptr = sop.emplace(42);
 	BOOST_CHECK(*ptr == 42);
-	BOOST_CHECK(sop.size()==1);
+	BOOST_CHECK(sop.size() == 1);
 	ptr.reset();
-	BOOST_CHECK(sop.size()==0);
+	BOOST_CHECK(sop.size() == 0);
 	BOOST_CHECK(sop.empty());
 }
 
 BOOST_AUTO_TEST_CASE(emplace_and_destroy_many) {
 	std::vector<smart_pool_ptr<element>> elem_ptrs;
-	for(int i = 0; i<512; ++i){
-		auto ptr=sop.emplace(i);
+	for(int i = 0; i < 512; ++i) {
+		auto ptr = sop.emplace(i);
 		elem_ptrs.emplace_back(ptr);
 		BOOST_CHECK(*ptr == i);
 	}
-	BOOST_CHECK(sop.size()==512);
-	for(int i =0;i<512;++i){
-		BOOST_CHECK(*(elem_ptrs[i])==i);
-	}
+	BOOST_CHECK(sop.size() == 512);
+	for(int i = 0; i < 512; ++i) { BOOST_CHECK(*(elem_ptrs[i]) == i); }
 	elem_ptrs.clear();
-	BOOST_CHECK(sop.size()==0);
+	BOOST_CHECK(sop.size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(iterator_holds_object) {
 	auto ptr = sop.emplace(42);
 	BOOST_CHECK(*ptr == 42);
-	BOOST_CHECK(sop.size()==1);
-	auto it=sop.begin();
+	BOOST_CHECK(sop.size() == 1);
+	auto it = sop.begin();
 	ptr.reset();
-	BOOST_CHECK(sop.size()==1);
-	it=decltype(sop)::iterator();
-	BOOST_CHECK(sop.size()==0);
+	BOOST_CHECK(sop.size() == 1);
+	it = decltype(sop)::iterator();
+	BOOST_CHECK(sop.size() == 0);
 	BOOST_CHECK(sop.empty());
+}
+
+BOOST_AUTO_TEST_CASE(mt_emplace_and_destroy_many) {
+	auto t1 = std::chrono::high_resolution_clock::now();
+	sop.reserve(128 * 0x10000);
+	auto t2 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff1 = t2 - t1;
+	std::cout << diff1.count() << std::endl;
+	std::vector<std::future<bool>> futures;
+	for(int t = 0; t < 256; ++t) {
+		futures.emplace_back(std::async(std::launch::async, [&](int t) {
+			bool res = true;
+			std::vector<smart_pool_ptr<element>> elem_ptrs;
+			elem_ptrs.reserve(0x1000);
+			for(int i = 0; i < 0x1000; ++i) {
+				auto ptr = sop.emplace((t << 16) | i);
+				elem_ptrs.emplace_back(ptr);
+				res = res && (*ptr == ((t << 16) | i));
+			}
+			for(int i = 0; i < 0x1000; ++i) {
+				res = res && (*(elem_ptrs[i]) == ((t << 16) | i));
+			}
+			elem_ptrs.clear();
+			return res;
+		}, t));
+	}
+	for(auto& f : futures) { BOOST_CHECK(f.get()); }
+	auto t3 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff2 = t3 - t2;
+	std::cout << diff2.count() << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
