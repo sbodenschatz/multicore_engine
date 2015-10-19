868bafa98d8706201c11daeb166de667ba4b9fe7
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-10-19 12:12:33 +0200

    Automatically run PDF-LaTeX after doxygen.

--- generate_doc.bat
+++ generate_doc.bat
@@ -1,1 +1,7 @@
 doxygen Doxyfile
+cd doc\latex
+pdflatex refman.tex > refman.1.log
+pdflatex refman.tex > refman.2.log
+bibtex refman
+pdflatex refman.tex > refman.3.log
+copy /Y refman.pdf ..\refman.pdf
