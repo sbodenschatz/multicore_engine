8416896b4a6a016336c9a9692a736c1f509549b1
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-11-22 19:40:09 +0100

    Implemented file_asset_loader in terms of file_reader.

---
+++ multicore_engine_core/include/asset/file_asset_loader.hpp
@@ -0,0 +1,33 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/asset/file_asset_loader.hpp
+ * Copyright 2015 by Stefan Bodenschatz
+ */
+
+#ifndef ASSET_FILE_ASSET_LOADER_HPP_
+#define ASSET_FILE_ASSET_LOADER_HPP_
+
+#include <string>
+#include <vector>
+#include <shared_mutex>
+#include "asset_loader.hpp"
+
+namespace mce {
+namespace asset {
+
+class file_asset_loader : public asset_loader {
+	std::shared_timed_mutex load_units_rw_lock;
+	std::vector<std::string> load_units;
+	const std::vector<path_prefix> prefixes;
+
+public:
+	file_asset_loader(std::vector<path_prefix> prefixes);
+	virtual bool start_load_asset(const std::shared_ptr<asset>& asset) override;
+	virtual void pin_load_unit(const std::string& name, asset_manager& asset_manager) override;
+	virtual void unpin_load_unit(const std::string& name, asset_manager& asset_manager) override;
+};
+
+} // namespace asset
+} // namespace mce
+
+#endif /* ASSET_FILE_ASSET_LOADER_HPP_ */
---
+++ multicore_engine_core/src/asset/file_asset_loader.cpp
@@ -0,0 +1,47 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/src/asset/file_asset_loader.cpp
+ * Copyright 2015 by Stefan Bodenschatz
+ */
+
+#include <asset/file_asset_loader.hpp>
+#include <algorithm>
+#include <asset/file_reader.hpp>
+
+namespace mce {
+namespace asset {
+file_asset_loader::file_asset_loader(std::vector<path_prefix> prefixes) : prefixes(std::move(prefixes)) {
+	load_units.push_back("");
+}
+
+bool file_asset_loader::start_load_asset(const std::shared_ptr<asset>& asset) {
+	std::shared_lock<std::shared_timed_mutex> lock(load_units_rw_lock);
+	std::string file_path;
+	file_path.reserve(128);
+	for(const auto& prefix : prefixes) {
+		for(const auto& load_unit : load_units) {
+			file_path = load_unit;
+			file_path += '/';
+			file_path += asset->name();
+			file_reader::file_content_ptr file_content;
+			file_reader::file_size file_size = 0u;
+			std::tie(file_content, file_size) = prefix.reader->read_file(prefix.prefix, file_path);
+			if(file_content) {
+				finish_loading(asset, file_content, file_size);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+void file_asset_loader::pin_load_unit(const std::string& name, asset_manager&) {
+	std::unique_lock<std::shared_timed_mutex> lock(load_units_rw_lock);
+	load_units.push_back(name);
+}
+void file_asset_loader::unpin_load_unit(const std::string& name, asset_manager&) {
+	std::unique_lock<std::shared_timed_mutex> lock(load_units_rw_lock);
+	load_units.erase(std::remove(load_units.begin(), load_units.end(), name), load_units.end());
+}
+
+} // namespace asset
+} // namespace mce
