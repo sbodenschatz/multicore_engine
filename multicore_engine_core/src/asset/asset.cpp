0178a0222a03ee5dc578fa8ac68a3ca4d16e52bd
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-11-22 19:36:25 +0100

    Minor restructuring of asset::complete_loading (removed unneeded lock).

--- multicore_engine_core/src/asset/asset.cpp
+++ multicore_engine_core/src/asset/asset.cpp
@@ -16,11 +16,11 @@ void asset::complete_loading(std::shared_ptr<const char> loaded_data, size_t siz
 	this->data_ = loaded_data;
 	size_ = size;
 	current_state_ = state::ready;
-	lock.unlock();
-	completed_cv.notify_all();
 	auto this_shared = this->shared_from_this();
+	lock.unlock();
+	// From here on the asset object is immutable and can therefore be read without holding a lock
+	completed_cv.notify_all();
 	for(auto& handler : completion_handlers) { handler(this_shared); }
-	lock.lock();
 	completion_handlers.clear();
 	completion_handlers.shrink_to_fit();
 }
