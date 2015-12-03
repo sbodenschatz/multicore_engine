88a69b9af56575d0f56ca04e9dbd4f6cadecf7d6
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-12-03 14:25:10 +0100

    Added self-documenting UNUSED macro.
    Conflicts:
    	multicore_engine_core/src/asset/pack_file_reader.cpp

---
+++ multicore_engine_core/include/util/unused.hpp
@@ -0,0 +1,12 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/util/unused.hpp
+ * Copyright 2015 by Stefan Bodenschatz
+ */
+
+#ifndef UTIL_UNUSED_HPP_
+#define UTIL_UNUSED_HPP_
+
+#define UNUSED(X) static_cast<void>(X)
+
+#endif /* UTIL_UNUSED_HPP_ */
