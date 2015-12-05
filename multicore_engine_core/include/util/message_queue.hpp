aa638ecf8df43a0dd66aa3c36dedd23c70c0af64
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-12-05 13:50:06 +0100

    Implemented message_queue unit test and fixed bugs found using it.

--- multicore_engine_core/include/util/message_queue.hpp
+++ multicore_engine_core/include/util/message_queue.hpp
@@ -14,17 +14,31 @@
 
 namespace mce {
 namespace util {
+namespace detail {
 
+template <typename Lock>
+struct cond_var_mapper {
+	typedef std::condition_variable_any condition_variable;
+};
+
+template <>
+struct cond_var_mapper<std::mutex> {
+	typedef std::condition_variable condition_variable;
+};
+
+} // namespace detail
+
+// TODO Add (conditional) noexcepts
 template <typename T, typename Lock = spin_lock>
 class message_queue {
 private:
 	std::queue<T> queue;
-	Lock lock;
-	std::condition_variable cv;
+	mutable Lock lock;
+	typename detail::cond_var_mapper<Lock>::condition_variable cv;
 
 public:
 	T pop() {
-		std::unique_lock<std::mutex> guard(lock);
+		std::unique_lock<Lock> guard(lock);
 		cv.wait(guard, [this] { return !queue.empty(); });
 		T value = std::move(queue.front());
 		queue.pop();
@@ -32,7 +46,7 @@ public:
 	}
 
 	bool try_pop(T& target) {
-		std::lock_guard<std::mutex> guard(lock);
+		std::lock_guard<Lock> guard(lock);
 		if(queue.empty()) return false;
 		target = std::move(queue.front());
 		queue.pop();
@@ -42,10 +56,20 @@ public:
 	template <typename U>
 	void push(U&& value) { // Forwarding reference to move value into the queue if possible
 		{
-			std::lock_guard<std::mutex> guard(lock);
+			std::lock_guard<Lock> guard(lock);
 			queue.push(std::forward<U>(value));
 		} // Exit critical section here to avoid waking and immediately blocking thread
 		cv.notify_one();
+	}
+
+	bool empty() const {
+		std::lock_guard<Lock> guard(lock);
+		return queue.empty();
+	}
+
+	size_t size() const {
+		std::lock_guard<Lock> guard(lock);
+		return queue.size();
 	}
 };
 
