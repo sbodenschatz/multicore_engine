899ea25980c05dd2a931b17f6547e631c68fadd6
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-12-09 22:24:07 +0100

    Implemented vector_iobstream to read/write data from/to a std::vector<char> buffer.

---
+++ multicore_engine_core/include/bstream/vector_iobstream.hpp
@@ -0,0 +1,40 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/bstream/vector_iobstream.hpp
+ * Copyright 2015 by Stefan Bodenschatz
+ */
+
+#ifndef BSTREAM_VECTOR_IOBSTREAM_HPP_
+#define BSTREAM_VECTOR_IOBSTREAM_HPP_
+
+#include <bstream/iobstream.hpp>
+#include <cstdint>
+#include <vector>
+
+namespace mce {
+namespace bstream {
+
+class vector_iobstream : public iobstream {
+	std::vector<char> data;
+	size_t read_position = 0;
+	size_t write_position = 0;
+
+public:
+	vector_iobstream(){};
+	virtual size_t read_bytes(char* buffer, size_t count) noexcept override;
+	virtual size_t size() const noexcept override;
+	virtual size_t tell_read() const noexcept override;
+	virtual void seek_read(size_t position) override;
+	virtual bool write_bytes(const char* buffer, size_t count) noexcept override;
+	virtual size_t tell_write() const noexcept override;
+	virtual void seek_write(size_t position) override;
+	void reset() {
+		clear_write_errors();
+		clear_read_errors();
+	}
+};
+
+} // namespace bstream
+} // namespace mce
+
+#endif /* BSTREAM_VECTOR_IOBSTREAM_HPP_ */
---
+++ multicore_engine_core/src/bstream/vector_iobstream.cpp
@@ -0,0 +1,48 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/src/bstream/vector_iobstream.cpp
+ * Copyright 2015 by Stefan Bodenschatz
+ */
+
+#include <bstream/vector_iobstream.hpp>
+
+namespace mce {
+namespace bstream {
+
+size_t vector_iobstream::read_bytes(char* buffer, size_t count) noexcept {
+	size_t i = 0;
+	for(; i < count && read_position < data.size(); ++i, ++read_position) { buffer[i] = data[read_position]; }
+	return i;
+}
+size_t vector_iobstream::size() const noexcept {
+	return data.size();
+}
+size_t vector_iobstream::tell_read() const noexcept {
+	return read_position;
+}
+void vector_iobstream::seek_read(size_t position) {
+	read_position = position;
+}
+bool vector_iobstream::write_bytes(const char* buffer, size_t count) noexcept {
+	size_t i = 0;
+	size_t old_write_position = write_position;
+	for(; i < count && write_position < data.size(); ++i, ++write_position) {
+		data[write_position] = buffer[i];
+	}
+	try {
+		for(; i < count; ++i, ++write_position) { data.push_back(buffer[i]); }
+		return true;
+	} catch(...) {
+		write_position = old_write_position;
+		return false;
+	}
+}
+size_t vector_iobstream::tell_write() const noexcept {
+	return write_position;
+}
+void vector_iobstream::seek_write(size_t position) {
+	write_position = position;
+}
+
+} // namespace bstream
+} // namespace mce
