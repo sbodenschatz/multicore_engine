2ce31e012787eb622df1ec8eac5b902c725f8f92
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-12-15 01:40:51 +0100

    Extracted calculate_program_name utility function.

---
+++ multicore_engine_core/src/util/program_name.cpp
@@ -0,0 +1,30 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/src/util/program_name.cpp
+ * Copyright 2015 by Stefan Bodenschatz
+ */
+
+#include <util/program_name.hpp>
+#include <algorithm>
+#include <cctype>
+
+namespace mce {
+namespace util {
+
+std::string calculate_program_name(const std::string& arg0) {
+	std::string program_name = arg0;
+	auto slash_pos = program_name.find_last_of("\\/");
+	if(slash_pos != program_name.npos) program_name = program_name.substr(slash_pos + 1);
+#ifdef MULTICORE_ENGINE_WINDOWS
+	auto name_without_suffix_length = std::max(program_name.size(), std::string::size_type(4)) - 4;
+	std::string suffix = ".exe";
+	if(std::equal(program_name.begin() + name_without_suffix_length, program_name.end(), suffix.begin(),
+				  suffix.end(), [](auto a, auto b) { return std::tolower(a) == std::tolower(b); })) {
+		program_name = program_name.substr(0, name_without_suffix_length);
+	}
+#endif
+	return program_name;
+}
+
+} // namespace util
+} // namespace mce
--- multicore_engine_load_unit_gen/src/main.cpp
+++ multicore_engine_load_unit_gen/src/main.cpp
@@ -11,8 +11,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <cctype>
 #include <boost/filesystem.hpp>
+#include <util/program_name.hpp>
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
@@ -35,19 +35,8 @@ int main(int argc, char* argv[]) {
 	po::variables_map vars;
 	po::store(po::parse_command_line(argc, argv, desc), vars);
 	po::notify(vars);
-	std::string program_name = argv[0];
-	auto slash_pos = program_name.find_last_of("\\/");
-	if(slash_pos != program_name.npos) program_name = program_name.substr(slash_pos + 1);
-#ifdef MULTICORE_ENGINE_WINDOWS
-	auto name_without_suffix_length = std::max(program_name.size(), std::string::size_type(4)) - 4;
-	std::string suffix = ".exe";
-	if(std::equal(program_name.begin() + name_without_suffix_length, program_name.end(), suffix.begin(),
-				  suffix.end(), [](auto a, auto b) { return std::tolower(a) == std::tolower(b); })) {
-		program_name = program_name.substr(0, name_without_suffix_length);
-	}
-#endif
 	if(vars.count("help") || argc == 1) {
-		std::cout << "Usage: " << program_name << " [options]" << std::endl;
+		std::cout << "Usage: " << mce::util::calculate_program_name(argv[0]) << " [options]" << std::endl;
 		std::cout << desc;
 	}
 	std::cout << description_file << std::endl;
