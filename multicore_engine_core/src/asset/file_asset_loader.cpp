87ae9f41bafc2dd887d6b27352437e4b57c40dab
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2015-12-19 22:09:07 +0100

    Don't request ./name instead of name for root load unit from file reader

--- multicore_engine_core/src/asset/file_asset_loader.cpp
+++ multicore_engine_core/src/asset/file_asset_loader.cpp
@@ -11,11 +11,11 @@
 namespace mce {
 namespace asset {
 file_asset_loader::file_asset_loader(const std::vector<path_prefix>& prefixes) : prefixes(prefixes) {
-	load_units.push_back(".");
+	load_units.push_back("");
 }
 
 file_asset_loader::file_asset_loader(std::vector<path_prefix>&& prefixes) : prefixes(std::move(prefixes)) {
-	load_units.push_back(".");
+	load_units.push_back("");
 }
 
 bool file_asset_loader::start_load_asset(const std::shared_ptr<asset>& asset, asset_manager&, bool) {
@@ -24,8 +24,10 @@ bool file_asset_loader::start_load_asset(const std::shared_ptr<asset>& asset, as
 	file_path.reserve(128);
 	for(const auto& prefix : prefixes) {
 		for(const auto& load_unit : load_units) {
-			file_path = load_unit;
-			file_path += '/';
+			if(!load_unit.empty()) {
+				file_path = load_unit;
+				file_path += '/';
+			}
 			file_path += asset->name();
 			file_content_ptr file_content;
 			file_size file_size = 0u;
