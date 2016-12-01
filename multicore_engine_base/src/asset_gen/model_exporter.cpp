82cc4e394644b91b7ef77df1145e35bacbb32a55
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2016-12-01 02:23:56 +0100

    Implemented export of static model collision data.
    See issue #59

--- multicore_engine_base/src/asset_gen/model_exporter.cpp
+++ multicore_engine_base/src/asset_gen/model_exporter.cpp
@@ -63,9 +63,10 @@ void static_model_exporter::export_model(const static_model& model_data,
 
 void static_model_exporter::export_model(const model::static_model_collision_data& collision_data,
 										 const std::string& output_file) const {
-	// TODO: Implement
-	UNUSED(collision_data);
-	UNUSED(output_file);
+	std::fstream file_stream(output_file, std::ios::out | std::ios::trunc | std::ios::binary);
+	if(!file_stream) throw std::runtime_error("Can't open '" + output_file + "' for writing.");
+	bstream::iostream_bstream stream(file_stream);
+	stream << collision_data;
 }
 
 } /* namespace asset_gen */
