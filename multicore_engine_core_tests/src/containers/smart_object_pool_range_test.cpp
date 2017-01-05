08124d029e337d818dd44238af0676f1516782d8
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-05 04:37:45 +0100

    Added stub for smart_object_pool_range unit test.
    See issue #4

---
+++ multicore_engine_core_tests/src/containers/smart_object_pool_range_test.cpp
@@ -0,0 +1,27 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core_tests/src/containers/smart_object_pool_range_test.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <containers/smart_object_pool_range.hpp>
+
+namespace mce {
+namespace containers {
+
+struct dummy_iterator {};
+
+BOOST_AUTO_TEST_SUITE(containers)
+BOOST_AUTO_TEST_SUITE(smart_object_pool_range_test)
+
+BOOST_AUTO_TEST_CASE(construct) {}
+BOOST_AUTO_TEST_CASE(split) {}
+BOOST_AUTO_TEST_CASE(empty) {}
+BOOST_AUTO_TEST_CASE(divisibility) {}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+
+} // namespace containers
+} // namespace mce
