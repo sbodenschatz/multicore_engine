6ab66229524da9813c96a244d14534bd09ecb94c
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-11 01:07:50 +0100

    Documented asset/model_meta_data.hpp.
    See issue #77

--- multicore_engine_base/include/asset/model_meta_data.hpp
+++ multicore_engine_base/include/asset/model_meta_data.hpp
@@ -13,15 +13,19 @@
 namespace mce {
 namespace asset {
 
+/// Represents the meta data for a single static mesh.
 struct static_mesh_meta_data {
-	std::string name;
-	offset_range index_data;
+	std::string name;		 ///< The name of the static mesh.
+	offset_range index_data; ///< The offset range of the index data of the static mesh within the model file.
 };
 
+/// Represents the meta data for a static model consisting of arbitrarily many static meshes.
 struct static_model_meta_data {
-	std::string name;
+	std::string name; ///< The name of the model.
+
+	/// The offset range of the vertex data of the static model within the model file.
 	offset_range vertex_data;
-	std::vector<static_mesh_meta_data> meshes;
+	std::vector<static_mesh_meta_data> meshes; ///< The meta data for the meshes in the model.
 };
 
 } // namespace asset
