b41bcc239be1921fdc4c0fe78721be8b99fbd959
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-13 21:53:04 +0100

    Documented asset_gen/load_unit_description_parser.hpp
    See issue #77

--- multicore_engine_base/include/asset_gen/load_unit_description_parser.hpp
+++ multicore_engine_base/include/asset_gen/load_unit_description_parser.hpp
@@ -17,14 +17,19 @@ namespace parser {
 struct load_unit_description_grammar;
 struct load_unit_description_skipper;
 
+/// Implements the parser front end for load unit description files.
 class load_unit_description_parser {
 	std::unique_ptr<load_unit_description_grammar> grammar;
 	std::unique_ptr<load_unit_description_skipper> skipper;
 
 public:
+	/// Constructs the load unit description parser.
 	load_unit_description_parser();
+	/// Explicit destructor required because the grammar and skipper are only complete types in the cpp file.
 	~load_unit_description_parser();
+	/// Parses the given character sequence using the given filename for error tracking.
 	ast::load_unit_ast_root parse(const std::string& filename, const char*& first, const char* last);
+	/// Loads and parses the named file.
 	ast::load_unit_ast_root load_file(const std::string& filename);
 };
 
