5467d061bc8c97be5e42b8e980a794cec518ac61
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-15 00:17:41 +0100

    Updated copyright dates.

--- multicore_engine_base/include/bstream/buffer_ibstream.hpp
+++ multicore_engine_base/include/bstream/buffer_ibstream.hpp
@@ -1,7 +1,7 @@
 /*
  * Multi-Core Engine project
  * File /multicore_engine_core/include/bstream/buffer_ibstream.hpp
- * Copyright 2015-2016 by Stefan Bodenschatz
+ * Copyright 2015-2017 by Stefan Bodenschatz
  */
 
 #ifndef BSTREAM_BUFFER_IBSTREAM_HPP_
