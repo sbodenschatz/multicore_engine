bffb7e16b3f8aa458a184a2572d74fcfdf54b247
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-14 03:49:31 +0100

    Documented asset_gen/pack_file_description_ast_compare.hpp.
    See issue #77

--- multicore_engine_base/include/asset_gen/pack_file_description_ast_compare.hpp
+++ multicore_engine_base/include/asset_gen/pack_file_description_ast_compare.hpp
@@ -13,16 +13,20 @@ namespace mce {
 namespace asset_gen {
 namespace ast {
 
+/// Enables comparison of pack file entries for equal.
 inline bool operator==(const pack_file_entry& o1, const pack_file_entry& o2) {
 	return o1.external_path == o2.external_path && o1.lookup == o2.lookup &&
 		   o1.internal_path == o2.internal_path;
 }
+/// Enables comparison of pack file entries for not equal.
 inline bool operator!=(const pack_file_entry& o1, const pack_file_entry& o2) {
 	return !(o1 == o2);
 }
+/// Enables comparison of pack file sections for equal.
 inline bool operator==(const pack_file_section& o1, const pack_file_section& o2) {
 	return o1.name == o2.name && o1.entries == o2.entries;
 }
+/// Enables comparison of pack file sections for not equal.
 inline bool operator!=(const pack_file_section& o1, const pack_file_section& o2) {
 	return !(o1 == o2);
 }
