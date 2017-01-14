ad184f1555196010628640d8848bc43e213f11bb
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-14 23:34:41 +0100

    Documented bstream/asset_ibstream.hpp
    See issue #77

--- multicore_engine_base/include/bstream/asset_ibstream.hpp
+++ multicore_engine_base/include/bstream/asset_ibstream.hpp
@@ -17,17 +17,24 @@ class asset;
 } // namespace asset
 namespace bstream {
 
+/// Provides a binary stream interface for assets for reading.
 class asset_ibstream : public ibstream {
 	std::shared_ptr<const asset::asset> asset;
 	size_t read_position = 0;
 
 public:
+	/// Creates a binary stream interface for the given asset and participate in it's ownership.
 	// cppcheck-suppress passedByValue
 	explicit asset_ibstream(std::shared_ptr<const asset::asset> asset) : asset(std::move(asset)){};
+	/// Reads bytes from the asset at the current read position.
 	virtual size_t read_bytes(char* buffer, size_t count) noexcept override;
+	/// Returns the size of the asset.
 	virtual size_t size() const noexcept override;
+	/// Returns the current read position in the asset.
 	virtual size_t tell_read() const noexcept override;
+	/// Changes the current read position in the asset.
 	virtual void seek_read(size_t position) override;
+	/// Resets the state of the stream by clearing error flags and resetting the read position to 0.
 	void reset() noexcept {
 		clear_read_errors();
 		read_position = 0;
