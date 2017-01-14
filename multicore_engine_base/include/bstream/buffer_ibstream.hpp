6d104970e7eb99e89831014deaa0a619719fa364
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-14 23:35:20 +0100

    Documented bstream/buffer_ibstream.hpp.
    See issue #77

--- multicore_engine_base/include/bstream/buffer_ibstream.hpp
+++ multicore_engine_base/include/bstream/buffer_ibstream.hpp
@@ -14,6 +14,7 @@
 namespace mce {
 namespace bstream {
 
+/// Provides a binary stream interface for reading data from an arbitrary buffer in memory.
 class buffer_ibstream : public ibstream {
 	const char* buffer_;
 	size_t size_;
@@ -21,13 +22,20 @@ class buffer_ibstream : public ibstream {
 	std::shared_ptr<const char> owned_buffer;
 
 public:
+	/// Constructs a binary stream interface for a buffer in memory without taking ownership.
 	buffer_ibstream(const char* buffer, size_t size) : buffer_{buffer}, size_{size} {};
+	/// Constructs a binary stream interface for a buffer manager by a shared_ptr.
 	buffer_ibstream(const std::shared_ptr<const char>& owned_buffer, size_t size)
 			: buffer_{owned_buffer.get()}, size_{size}, owned_buffer{owned_buffer} {};
+	/// Reads bytes from the buffer at the current read position.
 	virtual size_t read_bytes(char* buffer, size_t count) noexcept override;
+	/// Returns the size of the buffer as specified during stream creation.
 	virtual size_t size() const noexcept override;
+	/// Returns the current read position in the buffer.
 	virtual size_t tell_read() const noexcept override;
+	/// Changes the current read position in the buffer.
 	virtual void seek_read(size_t position) override;
+	/// Resets the state of the stream by clearing error flags and resetting the read position to 0.
 	void reset() noexcept {
 		clear_read_errors();
 		read_position = 0;
