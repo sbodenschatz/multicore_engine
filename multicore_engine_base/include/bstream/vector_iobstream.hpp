6fc7cabf439066161fbc621c61d74fa6bdd5921d
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-15 00:30:14 +0100

    Documented bstream/vector_iobstream.hpp.
    See issue #77

--- multicore_engine_base/include/bstream/vector_iobstream.hpp
+++ multicore_engine_base/include/bstream/vector_iobstream.hpp
@@ -14,20 +14,30 @@
 namespace mce {
 namespace bstream {
 
+/// Implements a binary input / output stream backed by a dynamically growing vector of bytes.
 class vector_iobstream : public iobstream {
 	std::vector<char> data;
 	size_t read_position = 0;
 	size_t write_position = 0;
 
 public:
+	/// Creates a vector I/O binary stream.
 	vector_iobstream(){};
+	/// Reads bytes from the stream at the current read position.
 	virtual size_t read_bytes(char* buffer, size_t count) noexcept override;
+	/// Returns the current size of the vector buffer.
 	virtual size_t size() const noexcept override;
+	/// Returns the current read position.
 	virtual size_t tell_read() const noexcept override;
+	/// Changes the current read position.
 	virtual void seek_read(size_t position) override;
+	/// Writes bytes to the stream at the current write position possibly growing the vector buffer.
 	virtual bool write_bytes(const char* buffer, size_t count) noexcept override;
+	/// Returns the current write position.
 	virtual size_t tell_write() const noexcept override;
+	/// Changes the current write position.
 	virtual void seek_write(size_t position) override;
+	/// Clears all error flags.
 	void reset() noexcept {
 		clear_write_errors();
 		clear_read_errors();
