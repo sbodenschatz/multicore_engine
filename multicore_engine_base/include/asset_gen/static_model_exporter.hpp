0be09b6f3b085de2ebff9a3c67e4a937e71030a3
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-14 04:18:23 +0100

    Documented asset_gen/static_model_exporter.hpp.
    See issue #77

--- multicore_engine_base/include/asset_gen/static_model_exporter.hpp
+++ multicore_engine_base/include/asset_gen/static_model_exporter.hpp
@@ -13,9 +13,12 @@
 namespace mce {
 namespace asset_gen {
 
+/// Implements the functionality to export models into the engine format for polygon and collision models.
 class static_model_exporter {
 public:
+	/// Export the given static model into the given model file to be later used for rendering in the engine.
 	void export_model(const static_model& model_data, const std::string& output_file) const;
+	/// Export the given static model collision data into the given file for usage as collision geometry.
 	void export_model(const model::static_model_collision_data& collision_data,
 					  const std::string& output_file) const;
 };
