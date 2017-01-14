86507d7d115f3d33812cc0384c9f638806674c9a
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-15 00:28:21 +0100

    Documented bstream/iobstream.hpp.
    See issue #77

--- multicore_engine_base/include/bstream/iobstream.hpp
+++ multicore_engine_base/include/bstream/iobstream.hpp
@@ -13,20 +13,22 @@
 namespace mce {
 namespace bstream {
 
+/// Provides a base class for binary streams that allow reading as well as writing.
 class iobstream : public ibstream, public obstream {
 public:
+	/// Checks if the stream is in a valid state (no error flags are enabled, neither reading nor writing).
 	explicit operator bool() noexcept {
 		return !errors();
 	}
-
+	/// Checks if there are any errors (reading or writing).
 	bool errors() const noexcept {
 		return eof() || invalid();
 	}
-
+	/// Checks if any of the aspects (reading or writing) of the stream has the end of file flag set.
 	bool eof() const noexcept {
 		return ibstream::eof() || obstream::eof();
 	}
-
+	/// Checks if any of the aspects (reading or writing) of the stream has the invalid data flag set.
 	bool invalid() const noexcept {
 		return ibstream::invalid() || obstream::invalid();
 	}
