720aaddfae3c6cb861bfba37b7da1ed692d4b6e3
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-16 05:05:38 +0100

    Documented util/spin_lock.hpp.
    See issue #77

--- multicore_engine_core/include/util/spin_lock.hpp
+++ multicore_engine_core/include/util/spin_lock.hpp
@@ -12,17 +12,25 @@
 namespace mce {
 namespace util {
 
+/// Implements a spin lock on top of atomic_flag that fulfills the Lockable concept from the standard library.
+/**
+ * In contrast to std::mutex this lock does not relinquish the CPU time slice but waits in a tight loop for
+ * acquiring the lock. This type of lock is useful for scenarios where a lock is held very shortly.
+ */
 class spin_lock {
 	std::atomic_flag flag = ATOMIC_FLAG_INIT;
 
 public:
+	/// Waits until the lock is acquired by the calling thread.
 	void lock() noexcept {
 		while(flag.test_and_set(std::memory_order_acquire))
 			;
 	}
+	/// Attempts to acquire the lock immediately but doesn't wait for the lock if already taken.
 	bool try_lock() noexcept {
 		return !flag.test_and_set(std::memory_order_acquire);
 	}
+	/// Releases the lock.
 	void unlock() noexcept {
 		flag.clear(std::memory_order_release);
 	}
