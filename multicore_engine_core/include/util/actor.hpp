44186f1958c04bceb5feaa1ca8b6bdf99c30c2c9
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-01-22 03:42:50 +0100

    Documented util/actor.hpp
    See issue #77

--- multicore_engine_core/include/util/actor.hpp
+++ multicore_engine_core/include/util/actor.hpp
@@ -16,6 +16,14 @@
 namespace mce {
 namespace util {
 
+/// Provides a simple actor interface for objects that allows deferred operations on it from other threads.
+/**
+ * The actor interface provides a thread-save queue of operations on the target object of type T represented
+ * trough local_function objects with the maximum functor buffer size of Max_Size.
+ *
+ * The operations can be \link enqueue enqueued\endlink from different threads.
+ * They are executed using #process from a single thread.
+ */
 template <typename T, size_t Max_Size>
 class actor {
 private:
@@ -37,17 +45,25 @@ private:
 	};
 
 public:
+	/// Creates an actor interface for the given target object.
 	explicit actor(T* target) : target(target) {}
+
+	/// Enqueues an operation on the target object using a function object with the signature void(T*).
 	template <typename F, typename R = decltype(std::declval<std::decay_t<F>>()(std::declval<T*>())),
 			  typename Dummy = std::enable_if<!std::is_member_function_pointer<std::decay<F>>::value>>
 	void enqueue(F&& f) {
 		actions.push(f);
 	}
+	/// Enqueues a call to the given member function of the target object with the given arguments.
+	/**
+	 * The given argument values are saved in the functor in the queue until the call is processed.
+	 */
 	template <typename R, typename... Args, typename... Args2>
 	void enqueue(R (T::*member_function)(Args...), Args2&&... args) {
 		actions.push(member_functor<R, Args...>(member_function, std::forward<Args2>(args)...));
 	}
 
+	/// Processes the queued operations.
 	void process() {
 		local_function<Max_Size, void(T*)> action;
 		while(actions.try_pop(action)) {
