486ac6aa1717ba274df95a9994f97d4c5b3183d7
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-02 13:59:34 +0200

    Documented entity/component_property_assignment.hpp.
    See issue #77

--- multicore_engine_core/include/entity/component_property_assignment.hpp
+++ multicore_engine_core/include/entity/component_property_assignment.hpp
@@ -1,7 +1,7 @@
 /*
  * Multi-Core Engine project
  * File /multicore_engine_core/include/entity/component_property_assignment.hpp
- * Copyright 2015-2016 by Stefan Bodenschatz
+ * Copyright 2015-2017 by Stefan Bodenschatz
  */
 
 #ifndef ENTITY_COMPONENT_PROPERTY_ASSIGNMENT_HPP_
@@ -29,35 +29,49 @@ class property;
 } // namespace reflection
 
 namespace entity {
+/// \brief Represents the abstract base class for component_property_assignment template instances to allow
+/// inserting them into a polymorphic container.
 template <typename Root_Type>
 class abstract_component_property_assignment {
 protected:
-	core::engine& engine_;
-	bool valid_;
+	core::engine& engine_; ///< The referenced engine object.
+	bool valid_;		   ///< Determines if the assignment has a valid value assigned.
 
 public:
-	explicit abstract_component_property_assignment(core::engine& engine) noexcept
-			: engine_(engine), valid_(false) {}
+	/// Allows implementing classes to create an abstract_component_property_assignment for the given engine.
+	explicit abstract_component_property_assignment(core::engine& engine) noexcept : engine_(engine),
+																					 valid_(false) {}
+	/// Allows copy-construction of abstract_component_property_assignment.
 	abstract_component_property_assignment(const abstract_component_property_assignment&) = default;
+	/// Allows move-construction of abstract_component_property_assignment.
 	abstract_component_property_assignment(abstract_component_property_assignment&&) = default;
+	/// Allows copy-assignment of abstract_component_property_assignment.
 	abstract_component_property_assignment&
 	operator=(const abstract_component_property_assignment&) = default;
+	/// Allows move-assignment of abstract_component_property_assignment.
 	abstract_component_property_assignment& operator=(abstract_component_property_assignment&&) = default;
+	/// Enables virtual destruction for derived classes.
 	virtual ~abstract_component_property_assignment() = default;
+	/// Assigns the stored value to the referenced property.
 	virtual void assign(Root_Type& object) const = 0;
+	/// Parses a value from the given variable_value and stores it in the assignment object.
 	virtual void parse(const ast::variable_value& ast_value, const std::string& entity_context,
 					   const std::string& component_context) = 0;
+	/// Returns the property to which this assignment assigns a value.
 	virtual const mce::reflection::abstract_property<
 			Root_Type, mce::entity::abstract_component_property_assignment, core::engine&>&
 	abstract_property() noexcept = 0;
+	/// Returns a unique_ptr-managed copy of this assignment object.
 	virtual std::unique_ptr<abstract_component_property_assignment<Root_Type>> make_copy() const = 0;
 	// TODO: Implement interface for binary serialization of object configurations
 
+	/// Returns a bool indicating if this assignment is valid.
 	bool valid() const {
 		return valid_;
 	}
 };
 
+/// Represents a concrete assignment of a component property with a specific type of the stored value.
 template <typename Root_Type, typename T>
 class component_property_assignment : public abstract_component_property_assignment<Root_Type> {
 	const reflection::property<Root_Type, T, mce::entity::abstract_component_property_assignment,
@@ -80,35 +94,45 @@ class component_property_assignment : public abstract_component_property_assignm
 				  void (*convert)(const U&, V&) = ast::ast_value_mapper<U, V>::convert>
 		void operator()(const U& ast_value) {
 			convert(ast_value, pa.value_);
+			pa.valid_ = true;
 		}
 	};
 
 public:
+	/// Constructs a component_property_assignment for the given property and referenced engine object.
 	component_property_assignment(
 			const mce::reflection::property<Root_Type, T, mce::entity::abstract_component_property_assignment,
 											mce::entity::component_property_assignment, core::engine&>&
 					property,
 			core::engine& engine)
 			: abstract_component_property_assignment<Root_Type>(engine), property_(property) {}
+	/// Allows copy-construction of component_property_assignment.
 	component_property_assignment(const component_property_assignment&) = default;
+	/// Allows move-construction of component_property_assignment.
 	component_property_assignment(component_property_assignment&&) = default;
+	/// Allows copy-assignment of component_property_assignment.
 	component_property_assignment& operator=(const component_property_assignment&) = default;
+	/// Allows move-assignment of component_property_assignment.
 	component_property_assignment& operator=(component_property_assignment&&) = default;
+	/// Destroys the component_property_assignment object.
 	virtual ~component_property_assignment() = default;
-
+	/// Assigns the stored value to the referenced property.
 	virtual void assign(Root_Type& object) const override {
 		if(this->valid_) property_.set_value(object, value_);
 	}
+	/// Parses a value from the given variable_value and stores it in the assignment object.
 	virtual void parse(const ast::variable_value& ast_value, const std::string& entity_context,
 					   const std::string& component_context) override {
 		ast_visitor visitor(entity_context, component_context, *this);
 		ast_value.apply_visitor(visitor);
 	}
+	/// Returns the property to which this assignment assigns a value.
 	virtual const mce::reflection::abstract_property<
 			Root_Type, mce::entity::abstract_component_property_assignment, core::engine&>&
 	abstract_property() noexcept override {
 		return property_;
 	}
+	/// Returns a unique_ptr-managed copy of this assignment object.
 	virtual std::unique_ptr<abstract_component_property_assignment<Root_Type>> make_copy() const override {
 		return std::make_unique<component_property_assignment<Root_Type, T>>(*this);
 	}
