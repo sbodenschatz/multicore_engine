15b7e8c950ee7eaab8ec4950be25d9c280f66d90
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-02 17:38:56 +0200

    Documented entity/component.hpp.
    See issue #77

--- multicore_engine_core/include/entity/component.hpp
+++ multicore_engine_core/include/entity/component.hpp
@@ -1,7 +1,7 @@
 /*
  * Multi-Core Engine project
  * File /multicore_engine_core/include/entity/component.hpp
- * Copyright 2015 by Stefan Bodenschatz
+ * Copyright 2015-2017 by Stefan Bodenschatz
  */
 
 #ifndef ENTITY_COMPONENT_HPP_
@@ -21,36 +21,47 @@ namespace entity {
 class entity;
 class component_configuration;
 
+/// \brief Represents an abstract component of an entity (aka game object); derived classes mark the entity
+/// for specific functionality and store the associated data.
 class component {
 private:
 	entity& owner_;
 	const component_configuration& configuration_;
 
 protected:
+	/// \brief Allows derived classes to construct the base class with a given owner entity and reference to a
+	/// component_configuration.
 	component(entity& owner, component_configuration& configuration) noexcept
-			: owner_(owner), configuration_(configuration) {}
+			: owner_(owner),
+			  configuration_(configuration) {}
 
 public:
+	/// Specifies the type of the list of properties.
 	typedef std::vector<std::unique_ptr<
 			reflection::abstract_property<component, abstract_component_property_assignment, core::engine&>>>
 			property_list;
+	/// Enables virtual destruction for derived classes.
 	virtual ~component() = default;
-
+	/// Allows access to the component_configuration from which this component object was created.
 	const component_configuration& configuration() const {
 		return configuration_;
 	}
-
+	/// Allows access to the entity owning this component.
 	const entity& owner() const {
 		return owner_;
 	}
-
+	/// Allows access to the entity owning this component.
 	entity& owner() {
 		return owner_;
 	}
-
+	/// \brief The fill_property_list static member function of derived classes is called by component_type to
+	/// register the properties of the derived class. If the derived class specifies no such method this no-op
+	/// implementation is used through inheritance.
 	static void fill_property_list(property_list& properties);
 
 protected:
+	/// \brief Allows derived classes to register a property specified by the given name, getter and setter to
+	/// the given list of properties.
 	template <typename T, typename Comp>
 	static void register_component_property(
 			property_list& list, const std::string& name,
@@ -65,6 +76,8 @@ protected:
 										  component_property_assignment, core::engine&>(name, getter,
 																						setter));
 	}
+	/// \brief Allows derived classes to register a property specified by the given name and member variable
+	/// pointer to the given list of properties.
 	template <typename T, typename Comp>
 	static void
 	register_component_property(property_list& list, const std::string& name,
@@ -80,6 +93,8 @@ protected:
 } // namespace entity
 } // namespace mce
 
+/// \brief Allows more comfortable registration of properties by applying the convention that the property
+/// uses a getter and a setter that are both named like the property and differentiate by signature.
 #define REGISTER_COMPONENT_PROPERTY(LIST, COMP, TYPE, NAME)                                                  \
 	register_component_property<TYPE, COMP>(                                                                 \
 			LIST, #NAME,                                                                                     \
@@ -90,6 +105,8 @@ protected:
 					component, TYPE, COMP, mce::entity::abstract_component_property_assignment,              \
 					mce::entity::component_property_assignment, mce::core::engine&>::setter_t>(&COMP::NAME))
 
+/// \brief Allows more comfortable registration of properties by applying the convention that the property is
+/// represented by a member variable that has the same name as the property.
 #define REGISTER_COMPONENT_PROPERTY_DIRECT(LIST, COMP, TYPE, NAME)                                           \
 	register_component_property<TYPE, COMP>(LIST, #NAME, &COMP::NAME)
 
