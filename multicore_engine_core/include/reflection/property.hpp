abc3a5d1b573778e37218abb3fa2c97d895cbd9c
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-06 00:48:19 +0200

    Changed the handling of property data types to avoid unneeded virtual calls.
    See issue #21

--- multicore_engine_core/include/reflection/property.hpp
+++ multicore_engine_core/include/reflection/property.hpp
@@ -50,11 +50,13 @@ template <typename Root_Type, template <typename> class Abstract_Assignment = ab
 		  typename... Assignment_Param>
 class abstract_property {
 protected:
-	std::string name_; ///< Stores the name of the property.
+	std::string name_;			   ///< Stores the name of the property.
+	mce::reflection::type_t type_; ///< Stores the type representation of the property.
 
-	/// Constructs an abstract_property with the given name.
+	/// Constructs an abstract_property with the given name and type.
 	// cppcheck-suppress passedByValue
-	explicit abstract_property(std::string name) : name_(std::move(name)) {}
+	explicit abstract_property(std::string name, mce::reflection::type_t type)
+			: name_(std::move(name)), type_(type) {}
 
 public:
 	/// Forbids copy-construction.
@@ -68,7 +70,9 @@ public:
 	/// Allows polymorphic destruction.
 	virtual ~abstract_property() = default;
 	/// Returns a representation of the data type of the property.
-	virtual mce::reflection::type_t type() const noexcept = 0;
+	mce::reflection::type_t type() const noexcept {
+		return type_;
+	}
 	/// Creates an assignment object for the concrete property value type from the given parameters.
 	virtual std::unique_ptr<Abstract_Assignment<Root_Type>> make_assignment(Assignment_Param...) const = 0;
 	/// Returns the name of the property given on construction of the property object.
@@ -135,9 +139,11 @@ template <typename Root_Type, typename T,
 		  template <typename, typename> class Assignment = null_assignment, typename... Assignment_Param>
 class property : public abstract_property<Root_Type, Abstract_Assignment, Assignment_Param...> {
 protected:
-	/// Constructs a property with the given name.
+	/// \brief Constructs a property with the given name and a representation of the data type of the property
+	/// based on the template parameter T.
 	explicit property(const std::string& name)
-			: abstract_property<Root_Type, Abstract_Assignment, Assignment_Param...>(name) {}
+			: abstract_property<Root_Type, Abstract_Assignment, Assignment_Param...>(name,
+																					 type_info<T>::type) {}
 
 public:
 	static_assert(std::is_base_of<Abstract_Assignment<Root_Type>, Assignment<Root_Type, T>>::value,
@@ -154,10 +160,6 @@ public:
 	property& operator=(property&&) = delete;
 	/// Allows polymorphic destruction.
 	virtual ~property() = default;
-	/// Returns a representation of the data type of the property based on the template parameter T.
-	virtual mce::reflection::type_t type() const noexcept override {
-		return type_info<T>::type;
-	}
 	/// Provides read access to the property value for the given object.
 	/**
 	 * The return value type is T for primitive types and const T& for complex types (strings, vecN, etc.).
