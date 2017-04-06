967ced8d0818b95cbb8a1ba8bec4d74e3086a44b
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-06 10:45:51 +0200

    Implemented component::store_to_bstream.
    See issue #22

--- multicore_engine_core/src/entity/component.cpp
+++ multicore_engine_core/src/entity/component.cpp
@@ -1,15 +1,32 @@
 /*
  * Multi-Core Engine project
  * File /multicore_engine_core/src/entity/component.cpp
- * Copyright 2015 by Stefan Bodenschatz
+ * Copyright 2015-2017 by Stefan Bodenschatz
  */
 
 #include <entity/component.hpp>
+#include <util/unused.hpp>
+#include <entity/component_configuration.hpp>
+#include <entity/component_type.hpp>
 
 namespace mce {
 namespace entity {
 
 void component::fill_property_list(property_list&) {}
+
+void component::store_to_bstream(bstream::obstream& ostr, bool compatible) const {
+	for(const auto& prop : configuration_.type().properties()) {
+		if(compatible) {
+			ostr << prop->name();
+			ostr << prop->type();
+		}
+		prop->to_bstream(*this, ostr);
+	}
+}
+void component::load_from_bstream(bstream::ibstream& istr, bool compatible) {
+	UNUSED(istr);
+	UNUSED(compatible);
+}
 
 } // namespace entity
 } // namespace mce
