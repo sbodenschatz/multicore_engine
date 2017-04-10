e796251ffa6f040d0378568dbbe58ac83a138d9f
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-10 16:56:58 +0200

    Created entity component unit test stub.
    See issue #93

---
+++ multicore_engine_core_tests/src/entity/entity_component_test.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core_tests/src/entity/entity_component_test.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <boost/test/unit_test.hpp>
+
+namespace mce {
+namespace entity {
+
+BOOST_AUTO_TEST_SUITE(entity)
+BOOST_AUTO_TEST_SUITE(entity_component_test)
+BOOST_AUTO_TEST_CASE(empty_file_valid) {}
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+
+} // namespace entity
+} // namespace mce
