d3c0b12080798f3d446434302cb80f9c0e113da9
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-11 03:16:21 +0200

    Adapted ecs unit test stub to gtest.
    See issue #94
    See issue #93

--- multicore_engine_core_tests/src/entity/entity_component_test.cpp
+++ multicore_engine_core_tests/src/entity/entity_component_test.cpp
@@ -4,16 +4,12 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
-#include <boost/test/unit_test.hpp>
+#include <gtest.hpp>
 
 namespace mce {
 namespace entity {
 
-BOOST_AUTO_TEST_SUITE(entity)
-BOOST_AUTO_TEST_SUITE(entity_component_test)
-BOOST_AUTO_TEST_CASE(empty_file_valid) {}
-BOOST_AUTO_TEST_SUITE_END()
-BOOST_AUTO_TEST_SUITE_END()
+TEST(entity_entity_component_test, empty_file_valid) {}
 
 } // namespace entity
 } // namespace mce
