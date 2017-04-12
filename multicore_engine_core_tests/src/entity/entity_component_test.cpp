791842ce58576173b2788a35b3d1b362bff6e7d5
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-12 23:39:00 +0200

    Implemented minimal integration test for ecs string -> objects.
    See issue #93

--- multicore_engine_core_tests/src/entity/entity_component_test.cpp
+++ multicore_engine_core_tests/src/entity/entity_component_test.cpp
@@ -4,10 +4,12 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
+#include <asset/dummy_asset.hpp>
 #include <containers/smart_object_pool.hpp>
 #include <entity/component.hpp>
 #include <entity/entity_manager.hpp>
 #include <gtest.hpp>
+#include <util/finally.hpp>
 
 namespace mce {
 namespace entity {
@@ -56,6 +58,20 @@ TEST(entity_entity_component_test, load_entity_with_simple_component) {
 	entity_manager em(nullptr);
 	test_a_system tasys;
 	REGISTER_COMPONENT_TYPE_SIMPLE(em, test_a_1, tasys.create_component_1(owner, config), &tasys);
+	em.load_entities_from_text_file(asset::dummy_asset::create_dummy_asset(
+			"test.etf", "Test_Ent_Conf{test_a_1{name=\"TestComp\";values=(\"Hello\",\"World\");}}"
+						"Test_Ent_Conf test_ent (0,0,0),();"));
+	auto test_ent = em.find_entity("test_ent");
+	auto finally_v = util::finally([&em]() { 
+		em.clear_entities();
+	});
+	ASSERT_TRUE(test_ent);
+	auto test_a_1_comp = test_ent->component<test_a_1_component>();
+	ASSERT_TRUE(test_a_1_comp);
+	ASSERT_EQ("TestComp", test_a_1_comp->name());
+	ASSERT_EQ(2, test_a_1_comp->values().size());
+	ASSERT_EQ("Hello", test_a_1_comp->values()[0]);
+	ASSERT_EQ("World", test_a_1_comp->values()[1]);
 }
 
 } // namespace entity
