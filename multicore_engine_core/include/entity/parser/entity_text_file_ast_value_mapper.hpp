6f85c7782f3bedf278e17c8488187992704aa48e
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-13 20:07:37 +0200

    Added support for quaternion typed component properties in etf.
    Conflicts:
    	multicore_engine_core/include/entity/parser/entity_text_file_ast_value_mapper.hpp

--- multicore_engine_core/include/entity/parser/entity_text_file_ast_value_mapper.hpp
+++ multicore_engine_core/include/entity/parser/entity_text_file_ast_value_mapper.hpp
@@ -9,6 +9,7 @@
 
 #include <entity/entity_reference.hpp>
 #include <entity/parser/entity_text_file_ast.hpp>
+#include <exceptions.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <string>
@@ -143,6 +144,50 @@ struct ast_value_mapper<ast::entity_reference, mce::entity::entity_reference> {
 	static void convert(const ast::entity_reference& ast_val, mce::entity::entity_reference& val,
 						entity_manager& entity_manager) {
 		val = mce::entity::entity_reference(ast_val.referred_name, entity_manager);
+	}
+};
+
+/// Maps float lists from the AST to tquat.
+template <typename T, glm::precision p>
+struct ast_value_mapper<ast::float_list, glm::tquat<T, p>> {
+	/// Converts ast_val to tquat<T,p> and stores it in val.
+	static void convert(const ast::float_list& ast_val, glm::tquat<T, p>& val, entity_manager&) {
+		if(ast_val.empty()) val = glm::tquat<T, p>();
+		if(ast_val.size() < 4) throw value_type_exception("Invalid angle-axis quaternion literal.");
+		val = glm::angleAxis(glm::radians(T(ast_val[0])),
+							 glm::tvec3<T, p>(ast_val[1], ast_val[2], ast_val[3]));
+	}
+};
+
+/// Maps int lists from the AST to tquat.
+template <typename T, glm::precision p>
+struct ast_value_mapper<ast::int_list, glm::tquat<T, p>> {
+	/// Converts ast_val to tquat<T,p> and stores it in val.
+	static void convert(const ast::int_list& ast_val, glm::tquat<T, p>& val, entity_manager&) {
+		if(ast_val.empty()) val = glm::tquat<T, p>();
+		if(ast_val.size() < 4) throw value_type_exception("Invalid angle-axis quaternion literal.");
+		val = glm::angleAxis(glm::radians(T(ast_val[0])),
+							 glm::tvec3<T, p>(ast_val[1], ast_val[2], ast_val[3]));
+	}
+};
+
+/// Maps rotation lists from the AST to tquat.
+template <typename T, glm::precision p>
+struct ast_value_mapper<ast::rotation_list, glm::tquat<T, p>> {
+	/// Converts ast_val to tquat<T,p> and stores it in val.
+	static void convert(const ast::rotation_list& ast_val, glm::tquat<T, p>& val, entity_manager&) {
+		glm::tquat<T, p> orientation;
+		for(auto&& entry : ast_val) {
+			glm::tvec3<T, p> axis;
+			switch(entry.axis) {
+			case ast::rotation_axis::x: axis.x = T(1); break;
+			case ast::rotation_axis::y: axis.y = T(1); break;
+			case ast::rotation_axis::z: axis.z = T(1); break;
+			default: throw std::logic_error("Invalid rotation axis in AST."); break;
+			}
+			orientation = orientation * glm::angleAxis(glm::radians(entry.angle), axis);
+		}
+		val = orientation;
 	}
 };
 
