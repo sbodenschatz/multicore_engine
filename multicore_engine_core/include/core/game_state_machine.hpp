cb6cd98d1d2aa9e79006ecbe103c57e4e3cdaab5
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-18 19:03:57 +0200

    Added game_state_machine stub.
    See issue #91

---
+++ multicore_engine_core/include/core/game_state_machine.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/core/game_state_machine.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef CORE_GAME_STATE_MACHINE_HPP_
+#define CORE_GAME_STATE_MACHINE_HPP_
+
+namespace mce {
+namespace core {
+
+class game_state_machine {
+public:
+	game_state_machine();
+	~game_state_machine();
+};
+
+} /* namespace core */
+} /* namespace mce */
+
+#endif /* CORE_GAME_STATE_MACHINE_HPP_ */
---
+++ multicore_engine_core/src/core/game_state_machine.cpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/core/game_state_machine.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <core/game_state_machine.hpp>
+
+namespace mce {
+namespace core {
+
+game_state_machine::game_state_machine() {
+	// TODO Auto-generated constructor stub
+}
+
+game_state_machine::~game_state_machine() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace core */
+} /* namespace mce */
