14652013827c848a2b46e278b6485bc77175c6fb
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-20 15:21:28 +0200

    Documented system.
    See issue #91

--- multicore_engine_core/include/core/system.hpp
+++ multicore_engine_core/include/core/system.hpp
@@ -11,13 +11,20 @@ namespace mce {
 namespace core {
 struct frame_time;
 
+/// Provides the base class for systems providing functionality in the engine.
 class system {
 public:
+	/// Constructs a system.
 	system() = default;
+	/// Enables polymorphic destruction for systems.
 	virtual ~system() = default;
+	/// Hook function called at the start of the processing phase of the frame.
 	virtual void preprocess(const mce::core::frame_time& frame_time);
+	/// Hook function called at the end of the processing phase of the frame.
 	virtual void postprocess(const mce::core::frame_time& frame_time);
+	/// Hook function called at the start of the rendering phase of the frame.
 	virtual void prerender(const mce::core::frame_time& frame_time);
+	/// Hook function called at the end of the rendering phase of the frame.
 	virtual void postrender(const mce::core::frame_time& frame_time);
 };
 
