aedeccf277c8d9cf17aa97da0279209375dbaf5b
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-04-20 14:07:39 +0200

    Documented game_state.
    See issue #91

--- multicore_engine_core/include/core/game_state.hpp
+++ multicore_engine_core/include/core/game_state.hpp
@@ -17,17 +17,24 @@ namespace core {
 class system_state;
 struct frame_time;
 
+/// Represents the base class for game_states in the engine.
+/**
+ * The game_states are classes that implement logical states in the game, e.g. main menu, loading screen,
+ * playing, pause.
+ */
 class game_state {
-protected:
 	std::vector<std::pair<util::type_id_t, std::unique_ptr<system_state>>> system_states_;
 
-public:
-	game_state() = default;
-	virtual ~game_state();
-
+protected:
+	/// Adds the system_state implemented by the class supplied in T to the game_state.
 	/**
-	 * May only be called when no other threads are using the system_states collection.
-	 * Usually this is called in initialization code only.
+	 * An object of T is constructed using the given constructor arguments.
+	 * This member function may only be called when no other threads are using the system_states collection.
+	 * Usually it is called in initialization code only.
+	 * The order of the calls to add_system_state determines the order in which the system_states are called
+	 * within a frame.
+	 * Usually only one object of a given type should be added because a second one could not be looked-up
+	 * through get_system_state.
 	 */
 	template <typename T, typename... Args>
 	T* add_system_state(Args&&... args) {
@@ -36,6 +43,14 @@ public:
 		return system_states_.back().second.get();
 	}
 
+public:
+	/// Constructs the game_state.
+	game_state() = default;
+	/// Enables polymorphic destruction for game_state.
+	virtual ~game_state();
+
+	/// \brief Looks up a system_state object of the given type and returns a pointer to it or nullptr if no
+	/// such system_state exists.
 	template <typename T>
 	T* get_system_state() const {
 		auto tid = util::type_id<system_state>::id<T>();
@@ -45,16 +60,39 @@ public:
 		return nullptr;
 	}
 
+	/// Implements the processing phase of the frame for the game_state.
+	/**
+	 * First calls preprocess.
+	 * Then sytem_state::process is called on the system_states in the game_state in the order in
+	 * which the
+	 * system_states were added.
+	 * Finally postprocess is called.
+	 */
 	void process(const mce::core::frame_time& frame_time);
+	/// Implements the rendering phase of the frame for the game_state.
+	/**
+	 * First calls prerender.
+	 * Then sytem_state::render is called on the system_states in the game_state in the order in which the
+	 * system_states were added.
+	 * Finally postrender is called.
+	 */
 	void render(const mce::core::frame_time& frame_time);
 
+	/// Provides a hook for subclasses before system_state::process is called on system_states.
 	virtual void preprocess(const mce::core::frame_time& frame_time);
+	/// Provides a hook for subclasses after system_state::process is called on system_states.
 	virtual void postprocess(const mce::core::frame_time& frame_time);
+	/// Provides a hook for subclasses before system_state::render is called on system_states.
 	virtual void prerender(const mce::core::frame_time& frame_time);
+	/// Provides a hook for subclasses after system_state::render is called on system_states.
 	virtual void postrender(const mce::core::frame_time& frame_time);
 
+	/// Provides a hook for subclasses when the game_state is left because a new game_state was pushed on top.
 	virtual void leave_pop();
+	/// Provides a hook for subclasses when the game_state is left because it was popped.
 	virtual void leave_push();
+	/// \brief Provides a hook for subclasses when the game_state is reentered because the game_state on top
+	/// of it was popped.
 	virtual void reenter();
 };
 
