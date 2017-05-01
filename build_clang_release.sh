889eac91bdf8c6412583cdf2005e5519a1c86d9f
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-02 00:42:52 +0200

    Be more specific when cleaning cmake cache from previous builds to conserve internal cmake variables.
    See issue #100

--- build_clang_release.sh
+++ build_clang_release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cmake -E make_directory build_clang_release
 cd build_clang_release
-cmake -U* -G "Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release ..
+cmake -U*_INCLUDE -U*_LIBS -ULIBS_DIR -G "Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . -- -j 16
 echo "export LD_LIBRARY_PATH=/usr/local/lib/clang-release/"
 
