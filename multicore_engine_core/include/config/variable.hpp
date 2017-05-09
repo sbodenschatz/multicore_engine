ba140db032a8f03749fdbcce005643bb197675ab
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-09 12:26:37 +0200

    Implemented config::variable.
    See issue #101

--- multicore_engine_core/include/config/variable.hpp
+++ multicore_engine_core/include/config/variable.hpp
@@ -7,26 +7,70 @@
 #ifndef CONFIG_VARIABLE_HPP_
 #define CONFIG_VARIABLE_HPP_
 
+#include <memory>
+#include <reflection/property.hpp>
 #include <string>
+#include <util/traits.hpp>
 #include <util/type_id.hpp>
 
 namespace mce {
 namespace config {
+template <typename T>
+class variable;
 
-class abstract_variable {
-	std::string name_;
+class abstract_variable : public std::enable_shared_from_this<abstract_variable> {
+	std::string full_name_;
 	util::type_id_t type_id_;
 
 protected:
-	abstract_variable(const std::string& name, util::type_id_t type_id) : name_{name}, type_id_{type_id} {}
+	std::unique_ptr<reflection::abstract_property<abstract_variable>> property_;
+	abstract_variable(const std::string& full_name, util::type_id_t type_id)
+			: full_name_{full_name}, type_id_{type_id} {}
+
+public:
+	template <typename U>
+	std::shared_ptr<variable<U>> as_type();
+	template <typename U>
+	bool is_type() {
+		return util::type_id<abstract_variable>::id<U>() == type_id_;
+	}
+
+	const std::string& full_name() const {
+		return full_name_;
+	}
+
+	reflection::abstract_property<abstract_variable>* property() const {
+		return property_.get();
+	}
 };
 
 template <typename T>
-class variable_impl : public abstract_variable {
+class variable : public abstract_variable {
+	T value_;
+
 public:
-	explicit variable_impl(const std::string& name)
-			: abstract_variable(name, util::type_id<abstract_variable>::id<T>()) {}
+	explicit variable(const std::string& full_name)
+			: abstract_variable(full_name, util::type_id<abstract_variable>::id<T>()) {
+		property_ = reflection::make_property("value", &value_);
+	}
+
+	util::accessor_value_type_t<T> value() const {
+		return value_;
+	}
+
+	void value(util::accessor_value_type_t<T> value) {
+		value_ = value;
+	}
 };
+
+template <typename U>
+std::shared_ptr<variable<U>> abstract_variable::as_type() {
+	if(is_type<U>()) {
+		return std::static_pointer_cast<variable<U>>(shared_from_this());
+	} else {
+		return std::shared_ptr<variable<U>>();
+	}
+}
 
 } // namespace config
 } // namespace mce
