e16d24b4a2078924f86756daf60fc223bc22b37a
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-09 08:35:09 +0200

    Added data structures for config variables.
    See issue #101

--- multicore_engine_core/include/config/variable.hpp
+++ multicore_engine_core/include/config/variable.hpp
@@ -7,13 +7,26 @@
 #ifndef CONFIG_VARIABLE_HPP_
 #define CONFIG_VARIABLE_HPP_
 
+#include <string>
+#include <util/type_id.hpp>
+
 namespace mce {
 namespace config {
 
-class abstract_variable {};
+class abstract_variable {
+	std::string name_;
+	util::type_id_t type_id_;
+
+protected:
+	abstract_variable(const std::string& name, util::type_id_t type_id) : name_{name}, type_id_{type_id} {}
+};
 
 template <typename T>
-class variable_impl : public abstract_variable {};
+class variable_impl : public abstract_variable {
+public:
+	variable_impl(const std::string& name)
+			: abstract_variable(name, util::type_id<abstract_variable>::id<T>()) {}
+};
 
 } // namespace config
 } // namespace mce
--- multicore_engine_core/include/config/variable_group.hpp
+++ multicore_engine_core/include/config/variable_group.hpp
@@ -7,10 +7,18 @@
 #ifndef CONFIG_VARIABLE_GROUP_HPP_
 #define CONFIG_VARIABLE_GROUP_HPP_
 
+#include <boost/container/flat_map.hpp>
+#include <string>
+#include <memory>
+
 namespace mce {
 namespace config {
+class abstract_variable;
 
-class variable_group {};
+class variable_group {
+	boost::container::flat_map<std::string, variable_group> subgroups_;
+	boost::container::flat_map<std::string, std::shared_ptr<abstract_variable>> variables_;
+};
 
 } // namespace config
 } // namespace mce
