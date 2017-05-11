777f7db62e81140c863a8fb5866fa4b10f1b092f
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-11 08:19:09 +0200

    Added load_modify_save_load unit test for config_store.
    See issue #101

--- multicore_engine_tests/src/config/config_test.cpp
+++ multicore_engine_tests/src/config/config_test.cpp
@@ -225,5 +225,69 @@ TEST(config_config_store, load_save_unmodified_load) {
 	ASSERT_EQ("This was not set in default config", newvalue->value());
 }
 
+TEST(config_config_store, load_modify_save_load) {
+	std::stringstream dstr;
+	dstr.str("test1=123\n"
+			 "number=12345\n"
+			 "text=Hello World\n"
+			 "test123=987.65 43.21\n"
+			 "test=Test Test Test\n"
+			 "vector=123 456 789\n"
+			 "list=Hello;World\n");
+	std::stringstream ustr;
+	ustr.str("number=345\n"
+			 "text=Hello Test\n"
+			 "vector=12 45 78\n"
+			 "list=Hello;Test;World\n"
+			 "new.value=This was not set in default config\n");
+	std::stringstream ostr;
+	int new_number = 54321;
+	std::string new_text = "New Text";
+	glm::ivec3 new_vector{1234, 5678, 9123};
+	std::vector<std::string> new_list{"New", "List", "Value"};
+	std::string new_newvalue = "This is the newly set text.";
+	glm::vec4 new_vec4{123.4f, 567.8f, 912.3f, 456.7f};
+	{
+		config_store cs(ustr, dstr, [&](config_store::config_storer& s) { s.store(ostr); });
+		auto number = cs.resolve<int>("number");
+		number->value(new_number);
+		auto text = cs.resolve<std::string>("text");
+		text->value(new_text);
+		auto vector = cs.resolve<glm::ivec3>("vector");
+		vector->value(new_vector);
+		auto list = cs.resolve<std::vector<std::string>>("list");
+		list->value(new_list);
+		auto newvalue = cs.resolve<std::string>("new.value");
+		newvalue->value(new_newvalue);
+		auto vec4 = cs.resolve<glm::vec4>("vec4");
+		vec4->value(new_vec4);
+	}
+	ASSERT_EQ("number=54321\n"
+			  "text=New Text\n"
+			  "vector=1234 5678 9123\n"
+			  "list=New;List;Value\n"
+			  "new.value=This is the newly set text.\n"
+			  "vec4=123.4 567.8 912.3 456.7\n",
+			  ostr.str());
+	config_store cs(ustr, ostr, [&](config_store::config_storer&) {});
+	auto text = cs.resolve<std::string>("text");
+	ASSERT_EQ(new_text, text->value());
+	auto number = cs.resolve<int>("number");
+	ASSERT_EQ(new_number, number->value());
+	auto vector = cs.resolve<glm::ivec3>("vector");
+	ASSERT_EQ(new_vector.x, vector->value().x);
+	ASSERT_EQ(new_vector.y, vector->value().y);
+	ASSERT_EQ(new_vector.z, vector->value().z);
+	auto list = cs.resolve<std::vector<std::string>>("list");
+	ASSERT_EQ(new_list, list->value());
+	auto newvalue = cs.resolve<std::string>("new.value");
+	ASSERT_EQ(new_newvalue, newvalue->value());
+	auto vec4 = cs.resolve<glm::vec4>("vec4");
+	ASSERT_FLOAT_EQ(new_vec4.x, vec4->value().x);
+	ASSERT_FLOAT_EQ(new_vec4.y, vec4->value().y);
+	ASSERT_FLOAT_EQ(new_vec4.z, vec4->value().z);
+	ASSERT_FLOAT_EQ(new_vec4.w, vec4->value().w);
+}
+
 } // namespace config
 } // namespace mce
