f30e6c0c77a3a786937750838628b5857c9764d1
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-20 16:34:10 +0200

    Workaround for a wrong doxygen warning.
    See issue #103

--- multicore_engine_core/src/glfw_wrapper/cursor.cpp
+++ multicore_engine_core/src/glfw_wrapper/cursor.cpp
@@ -4,8 +4,8 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
-#include <glfw_wrapper/cursor.hpp>
 #include <GLFW/glfw3.h>
+#include <glfw_wrapper/cursor.hpp>
 
 namespace mce {
 namespace glfw_wrapper {
@@ -13,9 +13,11 @@ namespace glfw_wrapper {
 cursor::cursor(instance&, glm::ivec2 size, unsigned char* pixels, glm::ivec2 hot_spot)
 		: cursor_{nullptr, [](GLFWcursor*) {}} {
 	GLFWimage img{size.x, size.y, pixels};
+#ifndef DOXYGEN /// Workaround for doxygen wrongly warning about an undocumented member here
 	// cppcheck-suppress useInitializationList
 	cursor_ = std::unique_ptr<GLFWcursor, void (*)(GLFWcursor*)>{
 			glfwCreateCursor(&img, hot_spot.x, hot_spot.y), [](GLFWcursor* c) { glfwDestroyCursor(c); }};
+#endif
 }
 cursor::cursor(instance&, standard_cursor standard_cursor_type)
 		: cursor_{glfwCreateStandardCursor(static_cast<int>(standard_cursor_type)),
