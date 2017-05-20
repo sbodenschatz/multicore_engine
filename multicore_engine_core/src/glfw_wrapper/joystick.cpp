ce50c11f89fc7ca883d9abbc15b412d73b6549b6
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-20 03:33:46 +0200

    Added error check for unplugged joystick.
    See issue #103

--- multicore_engine_core/src/glfw_wrapper/joystick.cpp
+++ multicore_engine_core/src/glfw_wrapper/joystick.cpp
@@ -17,6 +17,10 @@ bool joystick::present() const {
 const std::vector<float>& joystick::poll_axis() {
 	int count = 0;
 	auto a = glfwGetJoystickAxes(id_, &count);
+	if(!a) {
+		axis_.clear();
+		return axis_;
+	}
 	axis_.resize(count);
 	std::copy(a, a + count, axis_.begin());
 	return axis_;
@@ -24,6 +28,10 @@ const std::vector<float>& joystick::poll_axis() {
 const std::vector<bool>& joystick::poll_buttons() {
 	int count = 0;
 	auto b = glfwGetJoystickButtons(id_, &count);
+	if(!b) {
+		buttons_.clear();
+		return buttons_;
+	}
 	buttons_.resize(count);
 	std::transform(b, b + count, buttons_.begin(), [](unsigned char c) { return c == GLFW_PRESS; });
 	return buttons_;
