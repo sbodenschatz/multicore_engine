b5d7e43d3286b4b800a010601adcab53900d7463
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-20 03:20:59 +0200

    Documented glfw_wrapper::cursor.
    See issue #103

--- multicore_engine_core/include/glfw_wrapper/cursor.hpp
+++ multicore_engine_core/include/glfw_wrapper/cursor.hpp
@@ -7,9 +7,9 @@
 #ifndef GLFW_WRAPPER_CURSOR_HPP_
 #define GLFW_WRAPPER_CURSOR_HPP_
 
-#include <memory>
-#include <glm/glm.hpp>
 #include <glfw_wrapper/glfw_defs.hpp>
+#include <glm/glm.hpp>
+#include <memory>
 
 struct GLFWcursor;
 
@@ -17,15 +17,25 @@ namespace mce {
 namespace glfw_wrapper {
 class instance;
 
+/// Represents a (mouse) cursor object in glfw.
 class cursor {
 	std::unique_ptr<GLFWcursor, void (*)(GLFWcursor*)> cursor_;
 
 	friend class window;
 
 public:
+	/// Creates a custom cursor with the given size, pixel data and hot spot point.
+	/**
+	 * The pixel data is passed through to glfw, which has the following contract for the data format:
+	 * "The image data is 32-bit, little-endian, non-premultiplied RGBA, i.e. eight bits per channel. The
+	 * pixels are arranged canonically as sequential rows, starting from the top-left corner."
+	 */
 	cursor(instance&, glm::ivec2 size, unsigned char* pixels, glm::ivec2 hot_spot = {0, 0});
+	/// Creates a cursor object using the given standard shape.
 	cursor(instance&, standard_cursor standard_cursor_type);
+	/// Creates a null-cursor.
 	cursor();
+	/// Destroys the cursor object.
 	~cursor();
 };
 
