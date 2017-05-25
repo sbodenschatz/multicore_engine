7088ba38a9020195f433fb9e560c9606887abe7e
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-05-25 02:19:55 +0200

    Refactored graphics::pipeline_cache into renderer lib.
    See issue #123
    See archived/41_basic_vulkan_renderer (MR !3) for original history.

---
+++ multicore_engine_renderer/include/mce/graphics/pipeline_cache.hpp
@@ -0,0 +1,50 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/mce/graphics/pipeline_cache.hpp
+ * Copyright 2016 by Stefan Bodenschatz
+ */
+
+#ifndef GRAPHICS_PIPELINE_CACHE_HPP_
+#define GRAPHICS_PIPELINE_CACHE_HPP_
+
+#include <mce/graphics/unique_handle.hpp>
+#include <string>
+#include <vulkan/vulkan.hpp>
+
+namespace mce {
+namespace graphics {
+class window;
+class device;
+
+class pipeline_cache {
+private:
+	device& device_;
+	unique_handle<vk::PipelineCache, true> native_pipeline_cache_;
+	uint8_t uuid_[VK_UUID_SIZE];
+	std::string uuid_str_;
+	std::string cache_filename_;
+
+	std::vector<char> read_file(const std::string& filename);
+	void write_file(const std::string& filename, const std::vector<char>& content);
+
+public:
+	explicit pipeline_cache(device& dev);
+	~pipeline_cache();
+
+	const vk::PipelineCache& native_pipeline_cache() const {
+		return native_pipeline_cache_.get();
+	}
+
+	const std::string& uuid_string() const {
+		return uuid_str_;
+	}
+
+	const std::string& cache_filename() const {
+		return cache_filename_;
+	}
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* GRAPHICS_PIPELINE_CACHE_HPP_ */
