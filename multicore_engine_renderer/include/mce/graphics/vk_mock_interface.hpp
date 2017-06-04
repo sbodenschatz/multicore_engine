963afff6a30fe6f9ce39ee30a21d99a885361df0
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-04 17:03:16 +0200

    Added device_memory_wrapper.
    See issue #126

--- multicore_engine_renderer/include/mce/graphics/vk_mock_interface.hpp
+++ multicore_engine_renderer/include/mce/graphics/vk_mock_interface.hpp
@@ -13,12 +13,51 @@
  * For the mocked versions see file /multicore_engine_tests/src/vk_mock_interface.cpp.
  */
 
+#include <boost/variant.hpp>
 #include <mce/graphics/device_memory_manager.hpp>
-#include <mce/graphics/unique_handle.hpp>
 
 namespace mce {
 namespace graphics {
 namespace vk_mock_interface {
+
+struct fake_device_memory_deleter {
+	void operator()(vk::DeviceMemory) {}
+};
+
+using fake_unique_device_memory = vk::UniqueHandle<vk::DeviceMemory, fake_device_memory_deleter>;
+
+class device_memory_wrapper {
+	boost::variant<vk::UniqueDeviceMemory, fake_unique_device_memory> handle_;
+
+public:
+	device_memory_wrapper(vk::UniqueDeviceMemory&& m) : handle_(std::move(m)){};
+	device_memory_wrapper(fake_unique_device_memory&& m) : handle_(std::move(m)){};
+
+	const vk::DeviceMemory& get() const {
+		struct visitor : boost::static_visitor<const vk::DeviceMemory&> {
+			const vk::DeviceMemory& operator()(const vk::UniqueDeviceMemory& m) const {
+				return *m;
+			}
+			const vk::DeviceMemory& operator()(const fake_unique_device_memory& m) const {
+				return *m;
+			}
+		};
+		visitor v;
+		return handle_.apply_visitor(v);
+	}
+	vk::DeviceMemory get() {
+		struct visitor : boost::static_visitor<vk::DeviceMemory> {
+			vk::DeviceMemory operator()(const vk::UniqueDeviceMemory& m) const {
+				return m.get();
+			}
+			vk::DeviceMemory operator()(const fake_unique_device_memory& m) const {
+				return m.get();
+			}
+		};
+		visitor v;
+		return handle_.apply_visitor(v);
+	}
+};
 
 /// Returns a bool indicating if the functions are replaced by the mocked version.
 bool is_mocked();
