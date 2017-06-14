cab030e23d6d1035fa56b170670b60903a6b4e57
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-15 01:36:16 +0200

    Refactored graphics::command_pool into renderer lib.
    See issue #113
    See archived/41_basic_vulkan_renderer (MR !3) for original history.

---
+++ multicore_engine_renderer/include/mce/graphics/command_pool.hpp
@@ -0,0 +1,40 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/mce/graphics/command_pool.hpp
+ * Copyright 2016-2017 by Stefan Bodenschatz
+ */
+
+#ifndef GRAPHICS_COMMAND_POOL_HPP_
+#define GRAPHICS_COMMAND_POOL_HPP_
+
+#include <vulkan/vulkan.hpp>
+
+namespace mce {
+namespace graphics {
+class device;
+
+class command_pool {
+private:
+	device& owner_device_;
+	vk::UniqueCommandPool native_command_pool_;
+
+public:
+	command_pool(device& dev, uint32_t queueFamilyIndex, bool transient = false,
+				 bool buffer_resettable = false);
+	~command_pool();
+
+	const vk::CommandPool& native_command_pool() {
+		return *native_command_pool_;
+	}
+	vk::CommandPool native_command_pool() const {
+		return native_command_pool_.get();
+	}
+	void reset(bool release_resources = false);
+	vk::UniqueCommandBuffer allocate_primary_command_buffer();
+	vk::UniqueCommandBuffer allocate_secondary_command_buffer();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* GRAPHICS_COMMAND_POOL_HPP_ */
