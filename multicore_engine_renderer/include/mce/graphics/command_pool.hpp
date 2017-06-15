5126bd3c0115433a56a19a262815d3ddbf485b60
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-15 12:15:13 +0200

    Documented graphics::command_pool.
    See issue #113

--- multicore_engine_renderer/include/mce/graphics/command_pool.hpp
+++ multicore_engine_renderer/include/mce/graphics/command_pool.hpp
@@ -7,29 +7,41 @@
 #ifndef GRAPHICS_COMMAND_POOL_HPP_
 #define GRAPHICS_COMMAND_POOL_HPP_
 
+/**
+ * \file
+ * Defines the class encapsulating vulkan command pools.
+ */
+
 #include <vulkan/vulkan.hpp>
 
 namespace mce {
 namespace graphics {
 class device;
 
+/// Provides the functionality of vulkan command pools.
 class command_pool {
 private:
 	device& owner_device_;
 	vk::UniqueCommandPool native_command_pool_;
 
 public:
+	/// Creates a command pool for the given queue family on the given device with the given parameters.
 	command_pool(device& dev, uint32_t queue_family_index, bool transient = false,
 				 bool buffer_resettable = false);
 
+	/// Allows access to the underlying command pool handle.
 	const vk::CommandPool& native_command_pool() {
 		return *native_command_pool_;
 	}
+	/// Allows access to the underlying command pool handle.
 	vk::CommandPool native_command_pool() const {
 		return native_command_pool_.get();
 	}
+	/// Resets all command buffers allocated from the pool, optionally releasing resources.
 	void reset(bool release_resources = false);
+	/// Allocates returns a primary command buffer from the pool.
 	vk::UniqueCommandBuffer allocate_primary_command_buffer();
+	/// Allocates returns a secondary command buffer from the pool.
 	vk::UniqueCommandBuffer allocate_secondary_command_buffer();
 };
 
