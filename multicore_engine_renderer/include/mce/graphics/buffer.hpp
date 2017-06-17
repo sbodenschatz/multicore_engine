6e2b89b1ba43bb77168fef89ee711faad588d44b
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-18 00:16:12 +0200

    Created stub class for buffer.
    See issue #110

---
+++ multicore_engine_renderer/include/mce/graphics/buffer.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/buffer.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_BUFFER_HPP_
+#define MCE_GRAPHICS_BUFFER_HPP_
+
+namespace mce {
+namespace graphics {
+
+class buffer {
+public:
+	buffer();
+	~buffer();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_BUFFER_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/buffer.cpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/buffer.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/buffer.hpp>
+
+namespace mce {
+namespace graphics {
+
+buffer::buffer() {
+	// TODO Auto-generated constructor stub
+}
+
+buffer::~buffer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace graphics */
+} /* namespace mce */
