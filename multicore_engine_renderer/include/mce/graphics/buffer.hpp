aa3e81719337680b69dbe267ad04ab8b5bd4bf95
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-20 19:05:11 +0200

    Documented graphics::buffer.
    See issue #110

--- multicore_engine_renderer/include/mce/graphics/buffer.hpp
+++ multicore_engine_renderer/include/mce/graphics/buffer.hpp
@@ -7,6 +7,11 @@
 #ifndef MCE_GRAPHICS_BUFFER_HPP_
 #define MCE_GRAPHICS_BUFFER_HPP_
 
+/**
+ * \file
+ * Provides the definition of the wrapper class for buffer objects.
+ */
+
 #include <mce/graphics/device_memory_handle.hpp>
 #include <vulkan/vulkan.hpp>
 
@@ -15,6 +20,7 @@ namespace graphics {
 class destruction_queue_manager;
 class device;
 
+/// Implements buffer objects, with which arbitrary data can be kept in device memory for different purposes.
 class buffer {
 	vk::UniqueBuffer buff_;
 	device_memory_handle memory_handle_;
@@ -23,34 +29,44 @@ class buffer {
 	vk::BufferUsageFlags usage_;
 
 public:
+	/// Constructs a buffer with the given parameters.
 	buffer(device& dev, device_memory_manager_interface& mem_mgr,
 		   destruction_queue_manager* destruction_manager, vk::DeviceSize size, vk::BufferUsageFlags usage,
 		   vk::MemoryPropertyFlags required_flags = vk::MemoryPropertyFlagBits::eDeviceLocal);
+	/// Destroys the buffer wrapper object and releases the used resources to the destruction queue.
 	~buffer();
 
+	/// Allows access to the underlying native buffer object handle.
 	vk::Buffer native_buffer() {
 		return buff_.get();
 	}
 
+	/// Allows access to the underlying native buffer object handle.
 	const vk::Buffer& native_buffer() const {
 		return *buff_;
 	}
 
+	/// Returns the size of the buffer object.
 	vk::DeviceSize size() const {
 		return size_;
 	}
 
+	/// Returns the allowed / requested usage modes for the buffer object.
 	vk::BufferUsageFlags usage() const {
 		return usage_;
 	}
 
+	/// Allows host access to the buffer content if it is host-visible.
 	const void* mapped_pointer() const {
 		return memory_handle_.mapped_pointer();
 	}
+	/// Allows host access to the buffer content if it is host-visible.
 	void* mapped_pointer() {
 		return memory_handle_.mapped_pointer();
 	}
+	/// Flushes the non-coherent buffer content from the host.
 	void flush_mapped(vk::Device& dev, vk::DeviceSize offset = 0, vk::DeviceSize size = VK_WHOLE_SIZE);
+	/// Invalidates non-coherent buffer content on the host.
 	void invalidate_mapped(vk::Device& dev, vk::DeviceSize offset = 0, vk::DeviceSize size = VK_WHOLE_SIZE);
 };
 
