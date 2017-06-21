dff87a6d0d78f5039108743df42c63bdc38efc86
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-21 15:11:20 +0200

    Created class stub for transfer_manager.
    See issue #130

---
+++ multicore_engine_renderer/src/graphics/transfer_manager.cpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/transfer_manager.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/transfer_manager.hpp>
+
+namespace mce {
+namespace graphics {
+
+transfer_manager::transfer_manager() {
+	// TODO Auto-generated constructor stub
+}
+
+transfer_manager::~transfer_manager() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace graphics */
+} /* namespace mce */
