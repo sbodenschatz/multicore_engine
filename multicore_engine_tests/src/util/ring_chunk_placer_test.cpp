ca9b675ff930e0a2310a12e34e2004cdd6da9c8e
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-25 01:15:16 +0200

    Added simple unit test for reusing buffer spaces to ring_chunk_placer.
    See issue #130

--- multicore_engine_tests/src/util/ring_chunk_placer_test.cpp
+++ multicore_engine_tests/src/util/ring_chunk_placer_test.cpp
@@ -28,11 +28,41 @@ TEST(util_ring_chunk_placer_test, fill_up_buffer) {
 		auto r = p.place_chunk(data, 16);
 		ASSERT_TRUE(r);
 	}
-	char dummy = 16;
+	char dummy = 17;
 	auto r = p.place_chunk(&dummy, 1);
 	ASSERT_FALSE(r);
 	for(int j = 0; j < 16; j++)
 		for(int i = 0; i < 16; ++i) ASSERT_EQ(j + 1, buffer[j * 16 + i]);
+}
+
+TEST(util_ring_chunk_placer_test, fill_up_reuse_one_buffer) {
+	char buffer[256];
+	ring_chunk_placer p(buffer, 256);
+	char data[128];
+	for(int j = 0; j < 16; j++) {
+		memset(data, j + 1, 16);
+		auto r = p.place_chunk(data, 16);
+		ASSERT_TRUE(r);
+	}
+	char dummy = 17;
+	auto r = p.place_chunk(&dummy, 1);
+	ASSERT_FALSE(r);
+	for(int j = 0; j < 16; j++)
+		for(int i = 0; i < 16; ++i) ASSERT_EQ(j + 1, buffer[j * 16 + i]);
+
+	p.free_to(1 * 16);
+	memset(data, 17, 16);
+	r = p.place_chunk(data, 16);
+	ASSERT_FALSE(r);
+
+	memset(data, 18, 15);
+	r = p.place_chunk(data, 15);
+	ASSERT_TRUE(r);
+
+	dummy = 19;
+	r = p.place_chunk(&dummy, 1);
+	ASSERT_FALSE(r);
+	for(int i = 0; i < 15; ++i) ASSERT_EQ(18, buffer[i]);
 }
 
 } // namespace util
