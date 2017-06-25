f107ea3d9b0ab715e049989f2440ec4f27b5435c
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-25 17:12:37 +0200

    Added unit test for ring_chunk_placer that fills the buffer and reuses 2 slots.
    See issue #130

--- multicore_engine_tests/src/util/ring_chunk_placer_test.cpp
+++ multicore_engine_tests/src/util/ring_chunk_placer_test.cpp
@@ -64,6 +64,41 @@ TEST(util_ring_chunk_placer_test, fill_up_reuse_one_buffer) {
 	ASSERT_FALSE(r);
 	for(int i = 0; i < 15; ++i) ASSERT_EQ(18, buffer[i]);
 }
+TEST(util_ring_chunk_placer_test, fill_up_reuse_two_buffers) {
+	char buffer[256];
+	ring_chunk_placer p(buffer, 256);
+	char data[128];
+	for(int j = 0; j < 16; j++) {
+		memset(data, j + 1, 16);
+		auto r = p.place_chunk(data, 16);
+		ASSERT_TRUE(r);
+	}
+	char dummy = 17;
+	auto r = p.place_chunk(&dummy, 1);
+	ASSERT_FALSE(r);
+	for(int j = 0; j < 16; j++)
+		for(int i = 0; i < 16; ++i) ASSERT_EQ(j + 1, buffer[j * 16 + i]);
+
+	p.free_to(1 * 16);
+	memset(data, 17, 16);
+	r = p.place_chunk(data, 16);
+	ASSERT_FALSE(r);
+
+	memset(data, 18, 15);
+	r = p.place_chunk(data, 15);
+	ASSERT_TRUE(r);
+	for(int i = 0; i < 15; ++i) ASSERT_EQ(18, buffer[i]);
+
+	p.free_to(2 * 16);
+	memset(data, 19, 16);
+	r = p.place_chunk(data, 16);
+	ASSERT_TRUE(r);
+
+	dummy = 20;
+	r = p.place_chunk(&dummy, 1);
+	ASSERT_FALSE(r);
+	for(int i = 0; i < 16; ++i) ASSERT_EQ(19, buffer[i + 15]);
+}
 
 } // namespace util
 } // namespace mce
