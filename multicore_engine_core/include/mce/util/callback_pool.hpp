6f777ec93e9e0024a98539683b41d7f1098c2db9
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-06-28 09:20:08 +0200

    Fixed file comment headers.
    See issue #136

--- multicore_engine_core/include/mce/util/callback_pool.hpp
+++ multicore_engine_core/include/mce/util/callback_pool.hpp
@@ -1,6 +1,6 @@
 /*
  * Multi-Core Engine project
- * File /multicore_engine_renderer/include/mce/util/callback_pool.hpp
+ * File /multicore_engine_core/include/mce/util/callback_pool.hpp
  * Copyright 2017 by Stefan Bodenschatz
  */
 
