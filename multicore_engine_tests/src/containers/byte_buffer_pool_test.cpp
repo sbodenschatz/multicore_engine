f091e1ebb35e606c68733837afa9081ada4b9b2c
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-07-02 22:09:41 +0200

    Added pool-buffer reuse unit test for byte_buffer_pool.
    See issue #130

--- multicore_engine_tests/src/containers/byte_buffer_pool_test.cpp
+++ multicore_engine_tests/src/containers/byte_buffer_pool_test.cpp
@@ -27,5 +27,34 @@ TEST(containers_byte_buffer_pool_test, allocate_multiple_buffers) {
 	}
 }
 
+TEST(containers_byte_buffer_pool_test, pool_buffers_reuse) {
+	byte_buffer_pool p;
+	std::vector<pooled_byte_buffer_ptr> buffs;
+	auto b = p.allocate_buffer(1024);
+	buffs.push_back(b);
+	auto cap = p.capacity();
+	size_t i = 0;
+	while(cap == p.capacity()) {
+		auto b = p.allocate_buffer(1024);
+		buffs.push_back(b);
+		memset(b, i % 128, b.size());
+		ASSERT_TRUE(std::all_of(b.begin(), b.end(), [i](auto c) { return size_t(c) == i % 128; }));
+		++i;
+	}
+	cap = p.capacity();
+	buffs.clear();
+	for(; i > 0; --i) {
+		auto b = p.allocate_buffer(512);
+		buffs.push_back(b);
+		memset(b, i % 128, b.size());
+		ASSERT_TRUE(std::all_of(b.begin(), b.end(), [i](auto c) { return size_t(c) == i % 128; }));
+		auto b2 = p.allocate_buffer(512);
+		buffs.push_back(b2);
+		memset(b2, i % 128, b2.size());
+		ASSERT_TRUE(std::all_of(b2.begin(), b2.end(), [i](auto c) { return size_t(c) == i % 128; }));
+	}
+	ASSERT_EQ(cap, p.capacity());
+}
+
 } // namespace containers
 } // namespace mce
