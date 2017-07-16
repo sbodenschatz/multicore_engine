e20beb380d04b2765a1e0eaf733fa7630d0aaba9
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-07-17 00:21:18 +0200

    Changed data param in upload_image to take shared_ptr<const char> rather than shared_ptr<void>.
    See issue #130

--- multicore_engine_renderer/include/mce/graphics/transfer_manager.hpp
+++ multicore_engine_renderer/include/mce/graphics/transfer_manager.hpp
@@ -283,7 +283,7 @@ public:
 		dst_img.set_layout_external(final_layout);
 	}
 	template <typename F>
-	void upload_image(const std::shared_ptr<void>& data, size_t data_size, base_image& dst_img,
+	void upload_image(const std::shared_ptr<const char>& data, size_t data_size, base_image& dst_img,
 					  vk::ImageLayout final_layout, vk::ArrayProxy<const vk::BufferImageCopy> regions,
 					  F&& callback = no_callback_tag{}) {
 		std::lock_guard<std::mutex> lock(manager_mutex);
