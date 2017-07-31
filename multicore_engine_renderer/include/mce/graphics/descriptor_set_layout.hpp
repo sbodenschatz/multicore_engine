263bcb75fd54ed1ace3bc669f382efa21ee2fe6a
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-01 01:36:26 +0200

    Created basic structure for descriptor_set_layout.
    See issue #143

---
+++ multicore_engine_renderer/include/mce/graphics/descriptor_set_layout.hpp
@@ -0,0 +1,47 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/descriptor_set_layout.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_DESCRIPTOR_SET_LAYOUT_HPP_
+#define MCE_GRAPHICS_DESCRIPTOR_SET_LAYOUT_HPP_
+
+#include <mce/graphics/destruction_queue_manager.hpp>
+#include <vector>
+#include <vulkan/vulkan.hpp>
+
+namespace mce {
+namespace graphics {
+
+class descriptor_set_layout {
+public:
+	struct binding {
+		uint32_t binding;
+		vk::DescriptorType descriptorType;
+		uint32_t descriptorCount;
+		vk::ShaderStageFlags stageFlags;
+		std::vector<vk::Sampler> pImmutableSamplers;
+	};
+
+private:
+	std::vector<binding> bindings_;
+	queued_handle<vk::UniqueDescriptorSetLayout> native_layout_;
+
+public:
+	descriptor_set_layout();
+	~descriptor_set_layout();
+
+	const std::vector<binding>& bindings() const {
+		return bindings_;
+	}
+
+	vk::DescriptorSetLayout native_layout() const {
+		return native_layout_.get();
+	}
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_DESCRIPTOR_SET_LAYOUT_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/descriptor_set_layout.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/descriptor_set_layout.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/descriptor_set_layout.hpp>
+
+namespace mce {
+namespace graphics {
+
+descriptor_set_layout::descriptor_set_layout() {
+	// TODO Auto-generated constructor stub
+}
+
+descriptor_set_layout::~descriptor_set_layout() {}
+
+} /* namespace graphics */
+} /* namespace mce */
