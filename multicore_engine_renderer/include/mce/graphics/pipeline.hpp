9127a1f060d9e5d88881c0e6bec3abb85dc0f287
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-07-31 11:25:36 +0200

    Documented graphics::pipeline.
    See issue #115

--- multicore_engine_renderer/include/mce/graphics/pipeline.hpp
+++ multicore_engine_renderer/include/mce/graphics/pipeline.hpp
@@ -7,6 +7,11 @@
 #ifndef GRAPHICS_PIPELINE_HPP_
 #define GRAPHICS_PIPELINE_HPP_
 
+/**
+ * \file
+ * Provides the pipeline encapsulation class.
+ */
+
 #include <mce/graphics/pipeline_config.hpp>
 #include <vector>
 #include <vulkan/vulkan.hpp>
@@ -16,6 +21,7 @@ namespace graphics {
 class device;
 class pipeline_cache;
 
+/// Encapsulates a vulkan graphics pipeline state object.
 class pipeline {
 private:
 	vk::UniquePipeline native_pipeline_;
@@ -24,18 +30,26 @@ private:
 	pipeline();
 
 public:
+	/// Allows move-construction.
 	pipeline(pipeline&&) = default;
+	/// Allows move-assignment.
 	pipeline& operator=(pipeline&&) = default;
+	/// Destroys the pipeline and releases the associated resources.
 	~pipeline();
+	/// \brief Creates a pipeline for each of the given pipeline_config on the given device using the given
+	/// pipeline_cache for build time improvement.
 	static std::vector<pipeline> create_pipelines(const device& dev, pipeline_cache& pipeline_cache,
 												  const std::vector<pipeline_config>& pipeline_configs);
 
+	/// Bind this pipeline in the given command buffer.
 	void bind(vk::CommandBuffer cb) const;
-	
+
+	/// Allows access to the underlying native vulkan pipeline object.
 	const vk::UniquePipeline& native_pipeline() const {
 		return native_pipeline_;
 	}
 
+	/// Allows access to the pipeline layout used by the pipeline.
 	const std::shared_ptr<vk::UniquePipelineLayout>& layout() const {
 		return layout_;
 	}
