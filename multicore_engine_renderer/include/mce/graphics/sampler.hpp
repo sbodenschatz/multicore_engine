db10bf3f8935e0527df01255c75342fc5813a29d
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-02 13:12:32 +0200

    Added stub for graphics::sampler class.
    See issue #144

---
+++ multicore_engine_renderer/include/mce/graphics/sampler.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/sampler.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_SAMPLER_HPP_
+#define MCE_GRAPHICS_SAMPLER_HPP_
+
+namespace mce {
+namespace graphics {
+
+class sampler {
+public:
+	sampler();
+	~sampler();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_SAMPLER_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/sampler.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/sampler.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/sampler.hpp>
+
+namespace mce {
+namespace graphics {
+
+sampler::sampler() {
+	// TODO Auto-generated constructor stub
+}
+
+sampler::~sampler() {}
+
+} /* namespace graphics */
+} /* namespace mce */
