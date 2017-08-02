1cd7e2c625e76f37ad8ed03ff994feedd90419df
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-03 00:53:22 +0200

    Created structure and implemented constructor for graphics::descriptor_pool.
    See issue #114

--- multicore_engine_renderer/include/mce/graphics/descriptor_pool.hpp
+++ multicore_engine_renderer/include/mce/graphics/descriptor_pool.hpp
@@ -7,12 +7,23 @@
 #ifndef MCE_GRAPHICS_DESCRIPTOR_POOL_HPP_
 #define MCE_GRAPHICS_DESCRIPTOR_POOL_HPP_
 
+#include <boost/container/flat_map.hpp>
+#include <vulkan/vulkan.hpp>
+
 namespace mce {
 namespace graphics {
+class device;
 
 class descriptor_pool {
+	vk::UniqueDescriptorPool native_pool_;
+	uint32_t max_sets_;
+	uint32_t available_sets_;
+	boost::container::flat_map<vk::DescriptorType, uint32_t> max_pool_sizes_;
+	boost::container::flat_map<vk::DescriptorType, uint32_t> available_pool_sizes_;
+
 public:
-	descriptor_pool();
+	descriptor_pool(device& dev, uint32_t max_sets, vk::ArrayProxy<const vk::DescriptorPoolSize> pool_sizes,
+					bool set_freeable = false);
 	~descriptor_pool();
 };
 
--- multicore_engine_renderer/src/graphics/descriptor_pool.cpp
+++ multicore_engine_renderer/src/graphics/descriptor_pool.cpp
@@ -5,12 +5,28 @@
  */
 
 #include <mce/graphics/descriptor_pool.hpp>
+#include <mce/graphics/device.hpp>
 
 namespace mce {
 namespace graphics {
 
-descriptor_pool::descriptor_pool() {
-	// TODO Auto-generated constructor stub
+descriptor_pool::descriptor_pool(device& dev, uint32_t max_sets,
+								 vk::ArrayProxy<const vk::DescriptorPoolSize> pool_sizes, bool set_freeable)
+		: max_sets_{max_sets}, available_sets_{max_sets} {
+	max_pool_sizes_.reserve(pool_sizes.size());
+	for(const vk::DescriptorPoolSize& dps : pool_sizes) {
+		max_pool_sizes_[dps.type] += dps.descriptorCount;
+	}
+	available_pool_sizes_ = max_pool_sizes_;
+	boost::container::small_vector<vk::DescriptorPoolSize, 16> pool_size_sums;
+	std::transform(max_pool_sizes_.begin(), max_pool_sizes_.end(), std::back_inserter(pool_size_sums),
+				   [](const std::pair<vk::DescriptorType, uint32_t>& ps) {
+					   return vk::DescriptorPoolSize(ps.first, ps.second);
+				   });
+	native_pool_ = dev->createDescriptorPoolUnique(
+			vk::DescriptorPoolCreateInfo(set_freeable ? vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet
+													  : vk::DescriptorPoolCreateFlags(),
+										 max_sets_, uint32_t(pool_size_sums.size()), pool_size_sums.data()));
 }
 
 descriptor_pool::~descriptor_pool() {}
