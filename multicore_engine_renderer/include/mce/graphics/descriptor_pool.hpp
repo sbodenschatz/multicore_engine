ca92159a73e832986e1ceef74c0ba4ea3059bf8f
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-02 22:50:23 +0200

    Added stub for the graphics::descriptor_pool class.
    See issue #114

---
+++ multicore_engine_renderer/include/mce/graphics/descriptor_pool.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/descriptor_pool.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_DESCRIPTOR_POOL_HPP_
+#define MCE_GRAPHICS_DESCRIPTOR_POOL_HPP_
+
+namespace mce {
+namespace graphics {
+
+class descriptor_pool {
+public:
+	descriptor_pool();
+	~descriptor_pool();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_DESCRIPTOR_POOL_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/descriptor_pool.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/descriptor_pool.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/descriptor_pool.hpp>
+
+namespace mce {
+namespace graphics {
+
+descriptor_pool::descriptor_pool() {
+	// TODO Auto-generated constructor stub
+}
+
+descriptor_pool::~descriptor_pool() {}
+
+} /* namespace graphics */
+} /* namespace mce */
