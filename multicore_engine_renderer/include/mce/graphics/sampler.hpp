8fc3f7c368602472e252b8d2ac28cabb663b2a4b
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-02 17:19:48 +0200

    Defined structure of class graphics::sampler.
    See issue #144

--- multicore_engine_renderer/include/mce/graphics/sampler.hpp
+++ multicore_engine_renderer/include/mce/graphics/sampler.hpp
@@ -7,16 +7,61 @@
 #ifndef MCE_GRAPHICS_SAMPLER_HPP_
 #define MCE_GRAPHICS_SAMPLER_HPP_
 
+#include <boost/optional.hpp>
+#include <mce/graphics/destruction_queue_manager.hpp>
+#include <vulkan/vulkan.hpp>
+
 namespace mce {
 namespace graphics {
 
 class sampler {
 public:
-	sampler();
+	class addressing_mode {
+		vk::SamplerAddressMode u_;
+		vk::SamplerAddressMode v_;
+		vk::SamplerAddressMode w_;
+
+	public:
+		addressing_mode(vk::SamplerAddressMode mode) : u_{mode}, v_{mode}, w_{mode} {}
+		addressing_mode(vk::SamplerAddressMode u, vk::SamplerAddressMode v, vk::SamplerAddressMode w)
+				: u_{u}, v_{v}, w_{w} {}
+
+		vk::SamplerAddressMode u() const {
+			return u_;
+		}
+
+		vk::SamplerAddressMode v() const {
+			return v_;
+		}
+
+		vk::SamplerAddressMode w() const {
+			return w_;
+		}
+	};
+
+private:
+	vk::Filter mag_filter_;
+	vk::Filter min_filter_;
+	vk::SamplerMipmapMode mipmap_mode_;
+	addressing_mode address_mode_;
+	float mip_lod_bias_;
+	boost::optional<float> max_anisotropy_;
+	boost::optional<vk::CompareOp> compare_op_;
+	float min_lod_;
+	float max_lod_;
+	vk::BorderColor border_color_;
+	bool unnormalized_coordinates_;
+	queued_handle<vk::UniqueSampler> native_sampler_;
+
+public:
+	sampler(vk::Filter mag_filter, vk::Filter min_filter, vk::SamplerMipmapMode mipmap_mode,
+			addressing_mode address_mode, float mip_lod_bias, boost::optional<float> max_anisotropy,
+			boost::optional<vk::CompareOp> compare_op, float min_lod, float max_lod,
+			vk::BorderColor border_color, bool unnormalized_coordinates);
 	~sampler();
 };
 
 } /* namespace graphics */
-} /* namespace mce */
+} /*x namespace mce */
 
 #endif /* MCE_GRAPHICS_SAMPLER_HPP_ */
--- multicore_engine_renderer/src/graphics/sampler.cpp
+++ multicore_engine_renderer/src/graphics/sampler.cpp
@@ -9,8 +9,15 @@
 namespace mce {
 namespace graphics {
 
-sampler::sampler() {
-	// TODO Auto-generated constructor stub
+sampler::sampler(vk::Filter mag_filter, vk::Filter min_filter, vk::SamplerMipmapMode mipmap_mode,
+				 addressing_mode address_mode, float mip_lod_bias, boost::optional<float> max_anisotropy,
+				 boost::optional<vk::CompareOp> compare_op, float min_lod, float max_lod,
+				 vk::BorderColor border_color, bool unnormalized_coordinates)
+		: mag_filter_{mag_filter}, min_filter_{min_filter}, mipmap_mode_{mipmap_mode},
+		  address_mode_{address_mode}, mip_lod_bias_{mip_lod_bias}, max_anisotropy_{max_anisotropy},
+		  compare_op_{compare_op}, min_lod_{min_lod}, max_lod_{max_lod}, border_color_{border_color},
+		  unnormalized_coordinates_{unnormalized_coordinates} {
+	// TODO: Implement
 }
 
 sampler::~sampler() {}
