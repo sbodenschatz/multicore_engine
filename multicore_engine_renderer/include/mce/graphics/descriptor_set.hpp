73c99cea7d3239ab95b2f8422574aada046eb8ce
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-02 22:51:25 +0200

    Added stub for the graphics::descriptor_set class.
    See issue #114

---
+++ multicore_engine_renderer/include/mce/graphics/descriptor_set.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/descriptor_set.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_DESCRIPTOR_SET_HPP_
+#define MCE_GRAPHICS_DESCRIPTOR_SET_HPP_
+
+namespace mce {
+namespace graphics {
+
+class descriptor_set {
+public:
+	descriptor_set();
+	~descriptor_set();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_DESCRIPTOR_SET_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/descriptor_set.cpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/descriptor_set.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/descriptor_set.hpp>
+
+namespace mce {
+namespace graphics {
+
+descriptor_set::descriptor_set() {
+	// TODO Auto-generated constructor stub
+}
+
+descriptor_set::~descriptor_set() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace graphics */
+} /* namespace mce */
