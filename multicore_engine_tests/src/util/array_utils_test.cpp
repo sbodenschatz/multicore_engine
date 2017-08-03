85f1a9bc20e2f2f0c1faca7caa72c7455f3c480f
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-03 23:29:06 +0200

    Added unit test for binary array_transform.
    See issue #147

--- multicore_engine_tests/src/util/array_utils_test.cpp
+++ multicore_engine_tests/src/util/array_utils_test.cpp
@@ -41,5 +41,40 @@ TEST(util_array_utils, array_transform_from_array_test) {
 	ASSERT_EQ(19, transformed[9].val);
 }
 
+struct array_transform_test_object_2 {
+	int x;
+	int y;
+	array_transform_test_object_2(int x, int y) : x{x}, y{y} {}
+};
+
+TEST(util_array_utils, binary_array_transform_from_array_test) {
+	auto a = make_array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+	auto b = make_array(100, 101, 102, 103, 104, 105, 106, 107, 108, 109);
+	int c = 0;
+	auto transformed = util::array_transform<array_transform_test_object_2>(
+			a, b, [&c](int x, int y) { return array_transform_test_object_2(++c + x, y); });
+	ASSERT_EQ(1, transformed[0].x);
+	ASSERT_EQ(3, transformed[1].x);
+	ASSERT_EQ(5, transformed[2].x);
+	ASSERT_EQ(7, transformed[3].x);
+	ASSERT_EQ(9, transformed[4].x);
+	ASSERT_EQ(11, transformed[5].x);
+	ASSERT_EQ(13, transformed[6].x);
+	ASSERT_EQ(15, transformed[7].x);
+	ASSERT_EQ(17, transformed[8].x);
+	ASSERT_EQ(19, transformed[9].x);
+
+	ASSERT_EQ(100, transformed[0].y);
+	ASSERT_EQ(101, transformed[1].y);
+	ASSERT_EQ(102, transformed[2].y);
+	ASSERT_EQ(103, transformed[3].y);
+	ASSERT_EQ(104, transformed[4].y);
+	ASSERT_EQ(105, transformed[5].y);
+	ASSERT_EQ(106, transformed[6].y);
+	ASSERT_EQ(107, transformed[7].y);
+	ASSERT_EQ(108, transformed[8].y);
+	ASSERT_EQ(109, transformed[9].y);
+}
+
 } // namespace util
 } // namespace mce
