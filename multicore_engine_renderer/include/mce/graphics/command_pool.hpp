4877952ef5d9fa07141b6ac940ad67ef89ae3fa4
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-03 18:13:20 +0200

    Added command buffer allocation functions for dynamic amount of buffers.
    See issue #113

--- multicore_engine_renderer/include/mce/graphics/command_pool.hpp
+++ multicore_engine_renderer/include/mce/graphics/command_pool.hpp
@@ -82,6 +82,12 @@ public:
 		}
 		return ubuf;
 	}
+
+	/// Allocates and returns multiple primary command buffers from the pool with a dynamic number of buffers.
+	std::vector<vk::UniqueCommandBuffer> allocate_primary_command_buffers(uint32_t buffer_count);
+	/// \brief Allocates and returns multiple secondary command buffers from the pool with a dynamic number of
+	/// buffers.
+	std::vector<vk::UniqueCommandBuffer> allocate_secondary_command_buffers(uint32_t buffer_count);
 };
 
 } /* namespace graphics */
