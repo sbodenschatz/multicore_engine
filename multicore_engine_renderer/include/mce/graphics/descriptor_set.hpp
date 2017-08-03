2d4cb35fc870e23f845f496d020c33dd6a3fc7b5
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-03 03:33:16 +0200

    Defined structure for graphics::descriptor_set.
    See issue #114

--- multicore_engine_renderer/include/mce/graphics/descriptor_set.hpp
+++ multicore_engine_renderer/include/mce/graphics/descriptor_set.hpp
@@ -7,10 +7,18 @@
 #ifndef MCE_GRAPHICS_DESCRIPTOR_SET_HPP_
 #define MCE_GRAPHICS_DESCRIPTOR_SET_HPP_
 
+#include <boost/optional.hpp>
+#include <vulkan/vulkan.hpp>
+
 namespace mce {
 namespace graphics {
+class descriptor_set_layout;
 
 class descriptor_set {
+	boost::optional<vk::UniqueDescriptorSet> descriptor_set_unique;
+	vk::DescriptorSet native_descriptor_set_;
+	std::shared_ptr<descriptor_set_layout> layout_;
+
 public:
 	descriptor_set();
 	~descriptor_set();
