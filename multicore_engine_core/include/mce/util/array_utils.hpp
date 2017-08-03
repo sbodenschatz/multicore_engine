c62126a557a3c23c6c63c32c72f2098c50f3b17c
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-03 21:00:14 +0200

    Added overload for array_transform that works on two corresponding input sets (similar to the two-ranges std::transform overload).
    This is required for the implementation of descriptor_pool::allocate_descriptor_sets(std::array).
    
    See issue #114

--- multicore_engine_core/include/mce/util/array_utils.hpp
+++ multicore_engine_core/include/mce/util/array_utils.hpp
@@ -24,11 +24,21 @@ std::array<T, N> array_transform_impl(T_In& input, F f, std::index_sequence<I...
 	return {f(std::get<I>(input))...};
 }
 
+template <typename T, size_t N, typename T_In1, typename T_In2, typename F, size_t... I>
+std::array<T, N> array_transform_impl(T_In1& input1, T_In2& input2, F f, std::index_sequence<I...>) {
+	return {f(std::get<I>(input1), std::get<I>(input2))...};
+}
+
 } // namespace detail
 
 template <typename T, typename T_In, typename F, size_t N = std::tuple_size<T_In>::value>
 std::array<T, N> array_transform(T_In& input, F f) {
 	return detail::array_transform_impl<T, N>(input, f, std::make_index_sequence<N>{});
+}
+template <typename T, typename T_In1, typename T_In2, typename F,
+		  size_t N = std::min(std::tuple_size<T_In1>::value, std::tuple_size<T_In2>::value)>
+std::array<T, N> array_transform(T_In1& input1, T_In2& input2, F f) {
+	return detail::array_transform_impl<T, N>(input1, input2, f, std::make_index_sequence<N>{});
 }
 
 } // namespace util
