98b629704fcaafa679f19695745e09e9b8071ec7
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-04 22:33:27 +0200

    Created stub for unique_descriptor_pool to split up descriptor_pool.
    See issue #114

---
+++ multicore_engine_renderer/include/mce/graphics/unique_descriptor_pool.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/unique_descriptor_pool.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_UNIQUE_DESCRIPTOR_POOL_HPP_
+#define MCE_GRAPHICS_UNIQUE_DESCRIPTOR_POOL_HPP_
+
+namespace mce {
+namespace graphics {
+
+class unique_descriptor_pool {
+public:
+	unique_descriptor_pool();
+	~unique_descriptor_pool();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_UNIQUE_DESCRIPTOR_POOL_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/unique_descriptor_pool.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/unique_descriptor_pool.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/unique_descriptor_pool.hpp>
+
+namespace mce {
+namespace graphics {
+
+unique_descriptor_pool::unique_descriptor_pool() {
+	// TODO Auto-generated constructor stub
+}
+
+unique_descriptor_pool::~unique_descriptor_pool() {}
+
+} /* namespace graphics */
+} /* namespace mce */
