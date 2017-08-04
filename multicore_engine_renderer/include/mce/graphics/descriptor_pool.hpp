8ad214ed5acd77cf944cb627c81b70644c9bba2a
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-04 15:07:08 +0200

    Documented graphics::descriptor_pool.
    See issue #114

--- multicore_engine_renderer/include/mce/graphics/descriptor_pool.hpp
+++ multicore_engine_renderer/include/mce/graphics/descriptor_pool.hpp
@@ -7,6 +7,11 @@
 #ifndef MCE_GRAPHICS_DESCRIPTOR_POOL_HPP_
 #define MCE_GRAPHICS_DESCRIPTOR_POOL_HPP_
 
+/**
+ * \file
+ * Defines the wrapper class for vulkan descriptor pools.
+ */
+
 #include <boost/container/flat_map.hpp>
 #include <mce/graphics/descriptor_set.hpp>
 #include <mce/graphics/descriptor_set_layout.hpp>
@@ -21,6 +26,13 @@ namespace graphics {
 class descriptor_set;
 class destruction_queue_manager;
 
+/// \brief Encapsulates a bounded pool for allocating descriptors for vulkan descriptor sets (represented by
+/// descriptor_set objects).
+/**
+ * Supports either a mode where descriptor_set objects don't own the underlying resources but the pool owns
+ * them and can be reset (unique_allocation) or a mode where the descriptor_set objects own them and the pool
+ * can not be reset.
+ */
 class descriptor_pool {
 	device* dev_;
 	bool unique_allocation_;
@@ -31,48 +43,69 @@ class descriptor_pool {
 	boost::container::flat_map<vk::DescriptorType, uint32_t> available_pool_sizes_;
 
 public:
+	/// \brief Creates a descriptor_pool on the given device for the given amount of sets and descriptors per
+	/// type with the given mode.
 	descriptor_pool(device& dev, uint32_t max_sets, vk::ArrayProxy<const vk::DescriptorPoolSize> pool_sizes,
 					bool unique_allocation = false);
+	/// \brief Destroys the descriptor_pool, owning descriptor_set objects from it must be destroyed before
+	/// it, non-owning sets become invalid.
 	~descriptor_pool();
 
+	/// Returns the number of available descriptors for the given type.
 	uint32_t available_descriptors(vk::DescriptorType type) const {
 		auto it = available_pool_sizes_.find(type);
 		if(it == available_pool_sizes_.end()) return 0;
 		return it->second;
 	}
 
+	/// Returns the number of available descriptor sets.
 	uint32_t available_sets() const {
 		return available_sets_;
 	}
 
+	/// Returns the capacity for descriptors of the given type.
 	uint32_t max_descriptors(vk::DescriptorType type) const {
 		auto it = max_pool_sizes_.find(type);
 		if(it == max_pool_sizes_.end()) return 0;
 		return it->second;
 	}
 
+	/// Returns the capacity for descriptor sets.
 	uint32_t max_sets() const {
 		return max_sets_;
 	}
 
+	/// Allows access to the native vulkan descriptor pool handle.
 	vk::DescriptorPool native_pool() const {
 		return native_pool_.get();
 	}
 
+	/// Allows access to the available pool sizes map.
 	const boost::container::flat_map<vk::DescriptorType, uint32_t>& available_pool_sizes() const {
 		return available_pool_sizes_;
 	}
 
+	/// Allows access to the max pool sizes map.
 	const boost::container::flat_map<vk::DescriptorType, uint32_t>& max_pool_sizes() const {
 		return max_pool_sizes_;
 	}
 
+	/// \brief Allocates a descriptor set of the given layout, optionally using the given
+	/// destruction_queue_manager for an owning descriptor_set.
 	descriptor_set allocate_descriptor_set(const std::shared_ptr<descriptor_set_layout>& layout,
 										   destruction_queue_manager* dqm = nullptr);
+
+	/// \brief Allocates descriptor sets of the given layouts, optionally using the given
+	/// destruction_queue_manager for a owning descriptor_set objects.
 	std::vector<descriptor_set>
 	allocate_descriptor_sets(const std::vector<std::shared_ptr<descriptor_set_layout>>& layouts,
 							 destruction_queue_manager* dqm = nullptr);
 
+	/// \brief Allocates descriptor sets of the given layouts, optionally using the given
+	/// destruction_queue_manager for a owning descriptor_set objects.
+	/**
+	 * Avoids heap allocations by statically determining the number of sets.
+	 */
 	template <size_t size>
 	std::array<descriptor_set, size>
 	allocate_descriptor_sets(const std::array<std::shared_ptr<descriptor_set_layout>, size>& layouts,
@@ -98,6 +131,10 @@ public:
 				});
 	}
 
+	/// Resets the descriptor_pool, invalidating all descriptor sets allocated from it.
+	/**
+	 * This is only supported if the pool was constructed with unique_allocation = false.
+	 */
 	void reset();
 };
 
