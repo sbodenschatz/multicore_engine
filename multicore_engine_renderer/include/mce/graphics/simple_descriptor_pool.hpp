359e2683dd5fa685b322483c67579b895b3200fc
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-04 22:30:52 +0200

    Created stub for simple_descriptor_pool to split up descriptor_pool.
    See issue #114

---
+++ multicore_engine_renderer/include/mce/graphics/simple_descriptor_pool.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/simple_descriptor_pool.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_SIMPLE_DESCRIPTOR_POOL_HPP_
+#define MCE_GRAPHICS_SIMPLE_DESCRIPTOR_POOL_HPP_
+
+namespace mce {
+namespace graphics {
+
+class simple_descriptor_pool {
+public:
+	simple_descriptor_pool();
+	~simple_descriptor_pool();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_SIMPLE_DESCRIPTOR_POOL_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/simple_descriptor_pool.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/simple_descriptor_pool.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/simple_descriptor_pool.hpp>
+
+namespace mce {
+namespace graphics {
+
+simple_descriptor_pool::simple_descriptor_pool() {
+	// TODO Auto-generated constructor stub
+}
+
+simple_descriptor_pool::~simple_descriptor_pool() {}
+
+} /* namespace graphics */
+} /* namespace mce */
