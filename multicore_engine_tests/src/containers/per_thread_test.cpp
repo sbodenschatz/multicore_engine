2f276623369ac87512f701668b0093fe752872fe
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-07 20:43:55 +0200

    Added uint test stub for per_thread.
    See issue #149

---
+++ multicore_engine_tests/src/containers/per_thread_test.cpp
@@ -0,0 +1,8 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_tests/src/containers/per_thread_test.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <gtest.hpp>
+#include <mce/containers/per_thread.hpp>
