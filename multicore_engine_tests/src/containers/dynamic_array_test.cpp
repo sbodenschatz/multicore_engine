094908ab4163a8cbb34eb71b7e1fd766c4dc53e5
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-07 05:06:24 +0200

    Added unit test for construction from value for dynamic_array.
    See issue #149

--- multicore_engine_tests/src/containers/dynamic_array_test.cpp
+++ multicore_engine_tests/src/containers/dynamic_array_test.cpp
@@ -8,5 +8,23 @@
 #include <mce/containers/dynamic_array.hpp>
 
 namespace mce {
-namespace containers {} // namespace containers
+namespace containers {
+
+struct dynamic_array_test_object_1 {
+	int x;
+	int y;
+};
+
+TEST(containers_dynamic_array_test, construct_from_value) {
+	dynamic_array<dynamic_array_test_object_1> a(10, {123, 456});
+	ASSERT_EQ(10, a.size());
+	for(int i = 0; i < 10; ++i) {
+		ASSERT_EQ(123, a[i].x);
+		ASSERT_EQ(456, a[i].y);
+		ASSERT_EQ(123, a.at(i).x);
+		ASSERT_EQ(456, a.at(i).y);
+	}
+}
+
+} // namespace containers
 } // namespace mce
