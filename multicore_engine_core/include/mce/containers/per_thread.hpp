ded4c9ad282e931804822b8e97aeb7f9ffa86510
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-07 03:08:49 +0200

    Created class stub for containers::per_thread.
    See issue #149

---
+++ multicore_engine_core/include/mce/containers/per_thread.hpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/mce/containers/per_thread.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_CONTAINERS_PER_THREAD_HPP_
+#define MCE_CONTAINERS_PER_THREAD_HPP_
+
+namespace mce {
+namespace containers {
+
+template <typename T>
+class per_thread {
+
+};
+
+} // namespace containers
+} // namespace mce
+
+#endif /* MCE_CONTAINERS_PER_THREAD_HPP_ */
