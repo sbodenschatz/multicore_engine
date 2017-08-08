66f9d5b990097a602977713789408ec044e200d0
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-08 06:23:56 +0200

    Extracted thread->index mapping from per_thread into per_thread_index.
    See issue #149

--- multicore_engine_core/include/mce/containers/per_thread.hpp
+++ multicore_engine_core/include/mce/containers/per_thread.hpp
@@ -15,11 +15,50 @@
 namespace mce {
 namespace containers {
 
-template <typename T>
-class per_thread {
+class per_thread_index {
 	size_t total_slots_;
 	std::atomic<size_t> used_slots_;
 	dynamic_array<std::atomic<std::thread::id>> owners_;
+
+public:
+	using size_type = std::size_t;
+
+	per_thread_index(size_type slots)
+			: total_slots_{slots}, used_slots_{0}, owners_(slots, std::thread::id()) {}
+
+	per_thread_index(const per_thread_index&) = delete;
+	per_thread_index& operator=(const per_thread_index&) = delete;
+	per_thread_index(per_thread_index&&) = delete;
+	per_thread_index& operator=(per_thread_index&&) = delete;
+
+	size_type slot_index() {
+		auto used = used_slots_.load();
+		auto my_id = std::this_thread::get_id();
+		for(size_type i = 0; i < used; ++i) {
+			if(owners_[i].load() == my_id) return i;
+		}
+		auto my_index = used_slots_.load();
+		do {
+			if(my_index == total_slots_) {
+				throw mce::resource_depleted_exception("No more slots available.");
+			}
+		} while(!used_slots_.compare_exchange_weak(my_index, my_index + 1));
+		owners_[my_index] = my_id;
+		return my_index;
+	}
+
+	size_type total_slots() const {
+		return total_slots_;
+	}
+
+	size_type used_slots() const {
+		return used_slots_.load();
+	}
+};
+
+template <typename T>
+class per_thread {
+	per_thread_index index_mapping_;
 	dynamic_array<T> values_;
 
 public:
@@ -37,8 +76,7 @@ public:
 
 	template <typename... Args>
 	per_thread(size_type slots, Args&&... args)
-			: total_slots_{slots}, used_slots_{0}, owners_(slots, std::thread::id()),
-			  values_(slots, std::forward<Args>(args)...) {}
+			: index_mapping_{slots}, values_(slots, std::forward<Args>(args)...) {}
 
 	per_thread(const per_thread&) = delete;
 	per_thread& operator=(const per_thread&) = delete;
@@ -46,19 +84,7 @@ public:
 	per_thread& operator=(per_thread&&) = delete;
 
 	size_type slot_index() {
-		auto used = used_slots_.load();
-		auto my_id = std::this_thread::get_id();
-		for(size_type i = 0; i < used; ++i) {
-			if(owners_[i].load() == my_id) return i;
-		}
-		auto my_index = used_slots_.load();
-		do {
-			if(my_index == total_slots_) {
-				throw mce::resource_depleted_exception("No more slots available.");
-			}
-		} while(!used_slots_.compare_exchange_weak(my_index, my_index + 1));
-		owners_[my_index] = my_id;
-		return my_index;
+		return index_mapping_.slot_index();
 	}
 	reference get() {
 		return values_[slot_index()];
@@ -74,13 +100,13 @@ public:
 		return values_.begin();
 	}
 	iterator end() noexcept {
-		return values_.begin() + used_slots_.load();
+		return values_.begin() + index_mapping_.used_slots();
 	}
 	const_iterator end() const noexcept {
-		return values_.begin() + used_slots_.load();
+		return values_.begin() + index_mapping_.used_slots();
 	}
 	const_iterator cend() const noexcept {
-		return values_.begin() + used_slots_.load();
+		return values_.begin() + index_mapping_.used_slots();
 	}
 	reverse_iterator rbegin() noexcept {
 		return end();
@@ -102,11 +128,11 @@ public:
 	}
 
 	size_type total_slots() const {
-		return total_slots_;
+		return index_mapping_.total_slots();
 	}
 
 	size_type used_slots() const {
-		return used_slots_.load();
+		return index_mapping_.used_slots();
 	}
 };
 
