10e089f6a4363a33ace61e40747c1e18adbc2942
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-09 19:15:04 +0200

    Added stub for graphics_manager class.
    See issue #146

---
+++ multicore_engine_renderer/include/mce/graphics/graphics_manager.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/graphics_manager.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_GRAPHICS_MANAGER_HPP_
+#define MCE_GRAPHICS_GRAPHICS_MANAGER_HPP_
+
+namespace mce {
+namespace graphics {
+
+class graphics_manager {
+public:
+	graphics_manager();
+	~graphics_manager();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_GRAPHICS_MANAGER_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/graphics_manager.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/graphics_manager.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/graphics_manager.hpp>
+
+namespace mce {
+namespace graphics {
+
+graphics_manager::graphics_manager() {
+	// TODO Auto-generated constructor stub
+}
+
+graphics_manager::~graphics_manager() {}
+
+} /* namespace graphics */
+} /* namespace mce */
