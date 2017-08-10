8544312e6cfb6832437568eb6bf9aff4565fc8e8
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-10 03:05:24 +0200

    Defined structure for graphics_manager.
    See issue #146

--- multicore_engine_renderer/include/mce/graphics/graphics_manager.hpp
+++ multicore_engine_renderer/include/mce/graphics/graphics_manager.hpp
@@ -7,12 +7,42 @@
 #ifndef MCE_GRAPHICS_GRAPHICS_MANAGER_HPP_
 #define MCE_GRAPHICS_GRAPHICS_MANAGER_HPP_
 
+#include <boost/container/flat_map.hpp>
+#include <memory>
+#include <mutex>
+#include <string>
+
 namespace mce {
 namespace graphics {
+class shader_module;
+class device;
+class pipeline_layout;
+class pipeline;
+class framebuffer_config;
+class subpass_graph;
+class render_pass;
+class pipeline_cache;
+class pipeline_config;
+class descriptor_set_layout;
+class sampler;
 
 class graphics_manager {
+private:
+	std::mutex manager_mutex_;
+	device* dev_;
+	std::unique_ptr<pipeline_cache> pipeline_cache_;
+	boost::container::flat_map<std::string, std::shared_ptr<descriptor_set_layout>> descriptor_set_layouts_;
+	boost::container::flat_map<std::string, std::shared_ptr<pipeline_layout>> pipeline_layouts_;
+	boost::container::flat_map<std::string, std::shared_ptr<shader_module>> shader_modules_;
+	boost::container::flat_map<std::string, std::shared_ptr<pipeline_config>> pipelines_configs_;
+	boost::container::flat_map<std::string, std::shared_ptr<framebuffer_config>> framebuffer_configs_;
+	boost::container::flat_map<std::string, std::shared_ptr<subpass_graph>> subpass_graphs_;
+	boost::container::flat_map<std::string, std::shared_ptr<render_pass>> render_passes_;
+	boost::container::flat_map<std::string, std::shared_ptr<pipeline>> pipelines_;
+	boost::container::flat_map<std::string, std::shared_ptr<sampler>> samplers_;
+
 public:
-	graphics_manager();
+	graphics_manager(device& dev);
 	~graphics_manager();
 };
 
--- multicore_engine_renderer/src/graphics/graphics_manager.cpp
+++ multicore_engine_renderer/src/graphics/graphics_manager.cpp
@@ -4,12 +4,33 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
+#include <mce/graphics/descriptor_set_layout.hpp>
+#include <mce/graphics/device.hpp>
+#include <mce/graphics/framebuffer_config.hpp>
 #include <mce/graphics/graphics_manager.hpp>
+#include <mce/graphics/pipeline.hpp>
+#include <mce/graphics/pipeline_cache.hpp>
+#include <mce/graphics/pipeline_config.hpp>
+#include <mce/graphics/pipeline_layout.hpp>
+#include <mce/graphics/render_pass.hpp>
+#include <mce/graphics/sampler.hpp>
+
+class shader_module;
+class device;
+class pipeline_layout;
+class pipeline;
+class framebuffer_config;
+class subpass_graph;
+class render_pass;
+class pipeline_cache;
+class pipeline_config;
+class descriptor_set_layout;
+class sampler;
 
 namespace mce {
 namespace graphics {
 
-graphics_manager::graphics_manager() {
+graphics_manager::graphics_manager(device& dev) : dev_{&dev} {
 	// TODO Auto-generated constructor stub
 }
 
