0254617c2a6df8d23668fcebbc338109923de532
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-12 00:41:58 +0200

    Created stub for simple_uniform_buffer_pool.
    See issue #148

---
+++ multicore_engine_renderer/include/mce/graphics/simple_uniform_buffer_pool.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/simple_uniform_buffer_pool.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_SIMPLE_UNIFORM_BUFFER_POOL_HPP_
+#define MCE_GRAPHICS_SIMPLE_UNIFORM_BUFFER_POOL_HPP_
+
+namespace mce {
+namespace graphics {
+
+class simple_uniform_buffer_pool {
+public:
+	simple_uniform_buffer_pool();
+	~simple_uniform_buffer_pool();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_SIMPLE_UNIFORM_BUFFER_POOL_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/simple_uniform_buffer_pool.cpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/simple_uniform_buffer_pool.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/simple_uniform_buffer_pool.hpp>
+
+namespace mce {
+namespace graphics {
+
+simple_uniform_buffer_pool::simple_uniform_buffer_pool() {
+	// TODO Auto-generated constructor stub
+}
+
+simple_uniform_buffer_pool::~simple_uniform_buffer_pool() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace graphics */
+} /* namespace mce */
