94ed58d81a6d99dc2e25e75fe313da810c58fdbc
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-11 03:52:32 +0200

    Added window::make_framebuffer_config overload for R-value vector.
    See issue #146

--- multicore_engine_renderer/src/graphics/window.cpp
+++ multicore_engine_renderer/src/graphics/window.cpp
@@ -137,5 +137,14 @@ window::make_framebuffer_config(vk::ArrayProxy<const framebuffer_attachment_conf
 	return res;
 }
 
+framebuffer_config
+window::make_framebuffer_config(std::vector<framebuffer_attachment_config>&& additional_attachments) {
+	framebuffer_config res{std::move(additional_attachments)};
+	framebuffer_attachment_config swapchain_attachment(surface_format_);
+	swapchain_attachment.is_swapchain_image_ = true;
+	res.attachment_configs_.insert(res.attachment_configs_.begin(), swapchain_attachment);
+	return res;
+}
+
 } /* namespace graphics */
 } /* namespace mce */
