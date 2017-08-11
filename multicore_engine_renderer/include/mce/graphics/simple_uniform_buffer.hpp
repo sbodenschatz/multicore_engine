301ecacbfbfc6634dbff2ef15f0fb5de54d1d93e
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-12 00:30:35 +0200

    Created class stub for simple_uniform_buffer.
    See issue #148

---
+++ multicore_engine_renderer/include/mce/graphics/simple_uniform_buffer.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/simple_uniform_buffer.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_SIMPLE_UNIFORM_BUFFER_HPP_
+#define MCE_GRAPHICS_SIMPLE_UNIFORM_BUFFER_HPP_
+
+namespace mce {
+namespace graphics {
+
+class simple_uniform_buffer {
+public:
+	simple_uniform_buffer();
+	~simple_uniform_buffer();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_SIMPLE_UNIFORM_BUFFER_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/simple_uniform_buffer.cpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/simple_uniform_buffer.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/simple_uniform_buffer.hpp>
+
+namespace mce {
+namespace graphics {
+
+simple_uniform_buffer::simple_uniform_buffer() {
+	// TODO Auto-generated constructor stub
+}
+
+simple_uniform_buffer::~simple_uniform_buffer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace graphics */
+} /* namespace mce */
