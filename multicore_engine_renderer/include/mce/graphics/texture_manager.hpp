f336a583451fe2b33c70c18295dd269d1b6d3ba4
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-15 07:09:47 +0200

    Created class stub for texture_manager.
    See issue #155

---
+++ multicore_engine_renderer/include/mce/graphics/texture_manager.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/texture_manager.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef GRAPHICS_TEXTURE_MANAGER_HPP_
+#define GRAPHICS_TEXTURE_MANAGER_HPP_
+
+namespace mce {
+namespace graphics {
+
+class texture_manager {
+public:
+	texture_manager();
+	~texture_manager();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* GRAPHICS_TEXTURE_MANAGER_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/texture_manager.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/texture_manager.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/texture_manager.hpp>
+
+namespace mce {
+namespace graphics {
+
+texture_manager::texture_manager() {
+	// TODO Auto-generated constructor stub
+}
+
+texture_manager::~texture_manager() {}
+
+} /* namespace graphics */
+} /* namespace mce */
