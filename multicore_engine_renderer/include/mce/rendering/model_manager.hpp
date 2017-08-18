b33274ec6243ad622c1c4af06814dd1aa0d1a29a
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-19 01:52:06 +0200

    Added class stub for rendering::model_manager.
    See issue #159

---
+++ multicore_engine_renderer/include/mce/rendering/model_manager.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/rendering/model_manager.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_RENDERING_MODEL_MANAGER_HPP_
+#define MCE_RENDERING_MODEL_MANAGER_HPP_
+
+namespace mce {
+namespace rendering {
+
+class model_manager {
+public:
+	model_manager();
+	~model_manager();
+};
+
+} /* namespace rendering */
+} /* namespace mce */
+
+#endif /* MCE_RENDERING_MODEL_MANAGER_HPP_ */
---
+++ multicore_engine_renderer/src/rendering/model_manager.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/rendering/model_manager.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/rendering/model_manager.hpp>
+
+namespace mce {
+namespace rendering {
+
+model_manager::model_manager() {
+	// TODO Auto-generated constructor stub
+}
+
+model_manager::~model_manager() {}
+
+} /* namespace rendering */
+} /* namespace mce */
