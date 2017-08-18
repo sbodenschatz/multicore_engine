b5182cbab8e6f3196f95f8a1d2c38e33907610b2
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-19 01:55:11 +0200

    Added class stub for rendering::model.
    See issue #159

---
+++ multicore_engine_renderer/src/rendering/model.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/rendering/model.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/rendering/model.hpp>
+
+namespace mce {
+namespace rendering {
+
+model::model() {
+	// TODO Auto-generated constructor stub
+}
+
+model::~model() {}
+
+} /* namespace rendering */
+} /* namespace mce */
