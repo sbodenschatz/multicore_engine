d6ed1405fd82f8591f97d8631081a29815830fbf
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-18 02:33:55 +0200

    Removed specific attachment color format selection member functions.
    See issue #158

--- multicore_engine_renderer/include/mce/graphics/device.hpp
+++ multicore_engine_renderer/include/mce/graphics/device.hpp
@@ -75,18 +75,6 @@ public:
 			vk::FormatFeatureFlags additional_required_flags = {},
 			format_support_query_type query_type = format_support_query_type::optimal_tiling_image);
 
-	vk::Format best_unsigned_color_attachment_format(
-			int components, vk::FormatFeatureFlags additional_required_flags = {},
-			format_support_query_type query_type = format_support_query_type::optimal_tiling_image);
-
-	vk::Format best_signed_color_attachment_format(
-			int components, vk::FormatFeatureFlags additional_required_flags = {},
-			format_support_query_type query_type = format_support_query_type::optimal_tiling_image);
-
-	vk::Format best_float_color_attachment_format(
-			int components, vk::FormatFeatureFlags additional_required_flags = {},
-			format_support_query_type query_type = format_support_query_type::optimal_tiling_image);
-
 	/// Allows calling native device member function using the operator -> on the wrapper device.
 	const vk::Device* operator->() const {
 		return native_device_.operator->();
