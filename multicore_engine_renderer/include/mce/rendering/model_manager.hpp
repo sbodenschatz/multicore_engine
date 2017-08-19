f007606402a8464b765d5163e0eea417222432d0
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-19 21:38:55 +0200

    Documented rendering::model_manager.
    See issue #159

--- multicore_engine_renderer/include/mce/rendering/model_manager.hpp
+++ multicore_engine_renderer/include/mce/rendering/model_manager.hpp
@@ -29,6 +29,7 @@ class transfer_manager;
 namespace rendering {
 class static_model;
 
+/// Manages the loading and lifetime of renderable models in the renderer.
 class model_manager {
 	model::model_data_manager& model_data_mgr_;
 	graphics::device& dev_;
@@ -43,23 +44,34 @@ class model_manager {
 	friend class static_model;
 
 public:
+	/// \brief Creates a model_manager using the given underlying model::model_data_manager and the given
+	/// required graphics subsystem resources.
 	explicit model_manager(model::model_data_manager& model_data_mgr, graphics::device& dev,
 						   graphics::device_memory_manager_interface& mem_mgr,
 						   graphics::destruction_queue_manager* destruction_manager,
 						   graphics::transfer_manager& transfer_mgr)
 			: model_data_mgr_{model_data_mgr}, dev_{dev}, mem_mgr_{mem_mgr},
 			  destruction_manager_{destruction_manager}, transfer_mgr_{transfer_mgr} {}
+	/// Destroys the model_manager and releases the underlying resources.
 	~model_manager();
 
+	/// Forbids copying the model_manager.
 	model_manager(const model_manager&) = delete;
+	/// Forbids copying the model_manager.
 	model_manager& operator=(const model_manager&) = delete;
 
+	/// \brief Asynchronously loads the static_model with the given name and calls the completion_handler
+	/// function object on success or the error_handler function object on error.
+	/**
+	 * For the requirements for the function objects see static_model::run_when_ready.
+	 */
 	template <typename F, typename E>
 	static_model_ptr load_static_model(const std::string& name, F completion_handler, E error_handler) {
 		auto tmp = internal_load_static_model(name);
 		tmp->run_when_ready(std::move(completion_handler), std::move(error_handler));
 		return tmp;
 	}
+	/// Asynchronously loads the static_model with the given name.
 	static_model_ptr load_static_model(const std::string& name) {
 		return internal_load_static_model(name);
 	}
