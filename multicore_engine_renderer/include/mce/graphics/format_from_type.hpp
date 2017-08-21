dc47bcc44e4dde871de4b171cfbf2bb41275bc9f
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-21 12:09:50 +0200

    Implemented graphics::format_from_type<T> for floating point types.
    See issue #160

---
+++ multicore_engine_renderer/include/mce/graphics/format_from_type.hpp
@@ -0,0 +1,42 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/format_from_type.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_FORMAT_FROM_TYPE_HPP_
+#define MCE_GRAPHICS_FORMAT_FROM_TYPE_HPP_
+
+#include <glm/glm.hpp>
+#include <vulkan/vulkan.hpp>
+
+namespace mce {
+namespace graphics {
+
+template <typename T>
+struct format_from_type {};
+
+template <>
+struct format_from_type<float> {
+	static constexpr vk::Format default_format = vk::Format::eR32Sfloat;
+};
+
+template <>
+struct format_from_type<glm::vec2> {
+	static constexpr vk::Format default_format = vk::Format::eR32G32Sfloat;
+};
+
+template <>
+struct format_from_type<glm::vec3> {
+	static constexpr vk::Format default_format = vk::Format::eR32G32B32Sfloat;
+};
+
+template <>
+struct format_from_type<glm::vec4> {
+	static constexpr vk::Format default_format = vk::Format::eR32G32B32A32Sfloat;
+};
+
+} // namespace graphics
+} // namespace mce
+
+#endif /* MCE_GRAPHICS_FORMAT_FROM_TYPE_HPP_ */
