c32cee8b4caccf6d92dacb1c8c0c2cc2e12a7fc1
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-21 20:33:45 +0200

    Documented debug helper functions that allow dumping model geometry data.

--- multicore_engine_base/include/mce/model/dump_model.hpp
+++ multicore_engine_base/include/mce/model/dump_model.hpp
@@ -7,12 +7,27 @@
 #ifndef MCE_MODEL_DUMP_MODEL_HPP_
 #define MCE_MODEL_DUMP_MODEL_HPP_
 
+/**
+ * \file
+ * Defines debug helper functions that allow dumping model geometry data.
+ */
+
 #include <iostream>
 
 namespace mce {
 namespace model {
 
+/// \brief Dumps the model geometry information in the given number of vertices starting at the given address
+/// to the given stream.
+/**
+ * Accesses memory through memcpy to avoid strict aliasing violations and problems caused by unaligned access.
+ */
 void dump_model_vertices(std::ostream& ostr, const void* vertex_data, size_t vertices);
+/// \brief Dumps the model geometry information represented in the vertex and index data memory location for
+/// the given number of indices to the given stream.
+/**
+ * Accesses memory through memcpy to avoid strict aliasing violations and problems caused by unaligned access.
+ */
 void dump_model_vertices(std::ostream& ostr, const void* vertex_data, const void* index_data, size_t indices);
 
 } /* namespace model */
