0458d6cfb4d1b3cb91f78844794b1a0e96718094
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-22 13:41:04 +0200

    Added class stub for texture.
    See issue #155

---
+++ multicore_engine_renderer/include/mce/graphics/texture.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/texture.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_TEXTURE_HPP_
+#define MCE_GRAPHICS_TEXTURE_HPP_
+
+namespace mce {
+namespace graphics {
+
+class texture {
+public:
+	texture();
+	~texture();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_TEXTURE_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/texture.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/texture.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/texture.hpp>
+
+namespace mce {
+namespace graphics {
+
+texture::texture() {
+	// TODO Auto-generated constructor stub
+}
+
+texture::~texture() {}
+
+} /* namespace graphics */
+} /* namespace mce */
