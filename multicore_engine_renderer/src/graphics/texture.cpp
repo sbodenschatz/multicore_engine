c7f1fde7cd216e203390f5628bfd3fff3e0ce786
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-22 23:45:56 +0200

    Implemented texture::raise_error_flag.
    See issue #155

--- multicore_engine_renderer/src/graphics/texture.cpp
+++ multicore_engine_renderer/src/graphics/texture.cpp
@@ -11,5 +11,21 @@ namespace graphics {
 
 texture::~texture() {}
 
+void texture::raise_error_flag(std::exception_ptr e) noexcept {
+	current_state_ = state::error;
+	std::unique_lock<std::mutex> lock(modification_mutex);
+	for(auto& handler : error_handlers) {
+		try {
+			handler(e);
+		} catch(...) {
+			// Drop exceptions escaped from completion handlers
+		}
+	}
+	error_handlers.clear();
+	completion_handlers.clear();
+	error_handlers.shrink_to_fit();
+	completion_handlers.shrink_to_fit();
+}
+
 } /* namespace graphics */
 } /* namespace mce */
