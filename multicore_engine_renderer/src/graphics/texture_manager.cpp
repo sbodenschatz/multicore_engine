803445c2e287e2a2ab59c5e45a0b5467a58bc265
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-23 01:15:39 +0200

    Implemented texture_manager::internal_load_texture.
    See issue #155

--- multicore_engine_renderer/src/graphics/texture_manager.cpp
+++ multicore_engine_renderer/src/graphics/texture_manager.cpp
@@ -4,12 +4,41 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
+#include <mce/asset/asset_manager.hpp>
 #include <mce/graphics/texture_manager.hpp>
 
 namespace mce {
 namespace graphics {
 
 texture_manager::~texture_manager() {}
+
+std::shared_ptr<texture> texture_manager::internal_load_texture(const std::string& name) {
+	{
+		// Acquire read lock
+		std::shared_lock<std::shared_timed_mutex> lock(loaded_textures_rw_lock_);
+		auto it = loaded_textures_.find(name);
+		if(it != loaded_textures_.end()) {
+			return it->second;
+		}
+	}
+	{
+		// Acquire write lock
+		std::unique_lock<std::shared_timed_mutex> lock(loaded_textures_rw_lock_);
+		// Double check if the texture is still not in the map.
+		auto it = loaded_textures_.find(name);
+		if(it != loaded_textures_.end()) {
+			return it->second;
+		} else {
+			auto tmp = std::make_shared<texture>(*this, name);
+			loaded_textures_[name] = tmp;
+			asset_mgr_.load_asset_async(
+					name,
+					[tmp](const asset::asset_ptr& texture_asset) { tmp->complete_loading(texture_asset); },
+					[tmp](std::exception_ptr e) { tmp->raise_error_flag(e); });
+			return tmp;
+		}
+	}
+}
 
 } /* namespace graphics */
 } /* namespace mce */
