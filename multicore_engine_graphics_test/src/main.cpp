1820ea3483e325d91151a3c9b59be8e2a9204270
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-24 02:34:12 +0200

    Added exception handler to graphics test.
    See issue #154

--- multicore_engine_graphics_test/src/main.cpp
+++ multicore_engine_graphics_test/src/main.cpp
@@ -4,10 +4,15 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
+#include <iostream>
 #include <mce/graphics/graphics_test.hpp>
 
 int main(int, char* argv[]) {
-	mce::graphics::graphics_test::exe_path = boost::filesystem::path(argv[0]).parent_path();
-	mce::graphics::graphics_test gt;
-	gt.run();
+	try {
+		mce::graphics::graphics_test::exe_path = boost::filesystem::path(argv[0]).parent_path();
+		mce::graphics::graphics_test gt;
+		gt.run();
+	} catch(std::exception e) {
+		std::cerr << e.what();
+	}
 }
