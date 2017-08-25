e294d00eb5b64e0b7fe409d7a697429a943d775a
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-26 01:28:29 +0200

    Defined structure for material_manager.
    See issue #67

--- multicore_engine_renderer/include/mce/rendering/material_manager.hpp
+++ multicore_engine_renderer/include/mce/rendering/material_manager.hpp
@@ -7,6 +7,14 @@
 #ifndef MCE_RENDERING_MATERIAL_MANAGER_HPP_
 #define MCE_RENDERING_MATERIAL_MANAGER_HPP_
 
+#include <boost/container/flat_map.hpp>
+#include <mce/rendering/material.hpp>
+#include <mce/rendering/material_library.hpp>
+#include <mce/rendering/rendering_defs.hpp>
+#include <memory>
+#include <shared_mutex>
+#include <string>
+
 namespace mce {
 namespace graphics {
 class texture_manager;
@@ -15,12 +23,56 @@ namespace rendering {
 
 class material_manager {
 	graphics::texture_manager& tex_mgr;
+	std::shared_timed_mutex rw_lock_;
+	boost::container::flat_map<std::string, std::shared_ptr<material>> loaded_materials_;
+	std::vector<std::shared_ptr<material_library>> loaded_material_libs_;
+
+	std::shared_ptr<material> internal_load_material(const std::string& name);
+	std::shared_ptr<material_library> internal_load_material_lib(const std::string& name);
+	void recheck_pending_material_loads();
 
 	friend class material;
 
 public:
 	explicit material_manager(graphics::texture_manager& tex_mgr) : tex_mgr{tex_mgr} {}
 	~material_manager();
+
+	/// Forbids copying the material_manager.
+	material_manager(const material_manager&) = delete;
+	/// Forbids copying the material_manager.
+	material_manager& operator=(const material_manager&) = delete;
+
+	/// \brief Asynchronously loads the material with the given name and calls the completion_handler
+	/// function object on success or the error_handler function object on error.
+	/**
+	 * For the requirements for the function objects see material::run_when_ready.
+	 */
+	template <typename F, typename E>
+	material_ptr load_material(const std::string& name, F completion_handler, E error_handler) {
+		auto tmp = internal_load_material(name);
+		tmp->run_when_ready(std::move(completion_handler), std::move(error_handler));
+		return tmp;
+	}
+	/// Asynchronously loads the material with the given name.
+	material_ptr load_material(const std::string& name) {
+		return internal_load_material(name);
+	}
+	/// \brief Asynchronously loads the material library with the given name and calls the completion_handler
+	/// function object on success or the error_handler function object on error.
+	/**
+	 * For the requirements for the function objects see material_library::run_when_ready.
+	 */
+	template <typename F, typename E>
+	material_library_ptr load_material_library(const std::string& name, F completion_handler,
+											   E error_handler) {
+		auto tmp = internal_load_material_lib(name);
+		tmp->run_when_ready(std::move(completion_handler), std::move(error_handler));
+		return tmp;
+	}
+	/// Asynchronously loads the material library with the given name.
+	material_library_ptr load_material_library(const std::string& name) {
+		return internal_load_material_lib(name);
+	}
 };
 
 } /* namespace rendering */
