424bc23a9acb1beaebe2ec51e4b1931ff62e5eaf
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-25 14:36:20 +0200

    Added class stub for material_library
    See issue #67

---
+++ multicore_engine_renderer/include/mce/rendering/material_library.hpp
@@ -0,0 +1,23 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/rendering/material_library.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_RENDERING_MATERIAL_LIBRARY_HPP_
+#define MCE_RENDERING_MATERIAL_LIBRARY_HPP_
+
+namespace mce {
+namespace rendering {
+
+class material_library {
+
+public:
+	material_library();
+	~material_library();
+};
+
+} /* namespace rendering */
+} /* namespace mce */
+
+#endif /* MCE_RENDERING_MATERIAL_LIBRARY_HPP_ */
---
+++ multicore_engine_renderer/src/rendering/material_library.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/rendering/material_library.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/rendering/material_library.hpp>
+
+namespace mce {
+namespace rendering {
+
+material_library::material_library() {
+	// TODO Auto-generated constructor stub
+}
+
+material_library::~material_library() {}
+
+} /* namespace rendering */
+} /* namespace mce */
