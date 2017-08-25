95bee0f59c31e457db8216b3337ba2fb94f92ec2
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-25 19:01:48 +0200

    Added missing catch blocks in static_model and texture.

--- multicore_engine_renderer/src/graphics/texture.cpp
+++ multicore_engine_renderer/src/graphics/texture.cpp
@@ -68,43 +68,49 @@ base_image* texture::create_image_layered(vk::Format format, const gli::texture&
 
 void texture::complete_loading(const asset::asset_ptr& tex_asset) noexcept {
 	std::unique_lock<std::mutex> lock(modification_mutex);
-	auto tex = gli::load_dds(tex_asset->data(), tex_asset->size());
-	base_image* bimg = nullptr;
-	auto gl_format = gli::gl(gli::gl::PROFILE_GL33).translate(tex.format(), tex.swizzles());
-	auto format = static_cast<vk::Format>(vkGetFormatFromOpenGLInternalFormat(gl_format.Internal));
-	switch(tex.target()) {
-	case gli::TARGET_1D: bimg = create_image<image_1d>(format, tex); break;
-	case gli::TARGET_2D: bimg = create_image<image_2d>(format, tex); break;
-	case gli::TARGET_3D: bimg = create_image<image_3d>(format, tex); break;
-	case gli::TARGET_CUBE: bimg = create_image<image_cube>(format, tex); break;
-	case gli::TARGET_1D_ARRAY: bimg = create_image_layered<image_1d_layered>(format, tex); break;
-	case gli::TARGET_2D_ARRAY: bimg = create_image_layered<image_2d_layered>(format, tex); break;
-	case gli::TARGET_CUBE_ARRAY: bimg = create_image_layered<image_cube_layered>(format, tex); break;
-	default:
-		raise_error_flag(std::make_exception_ptr(mce::graphics_exception("Unsupported texture type.")));
-		break;
-	}
+	try {
+		auto tex = gli::load_dds(tex_asset->data(), tex_asset->size());
+		base_image* bimg = nullptr;
+		auto gl_format = gli::gl(gli::gl::PROFILE_GL33).translate(tex.format(), tex.swizzles());
+		auto format = static_cast<vk::Format>(vkGetFormatFromOpenGLInternalFormat(gl_format.Internal));
+		switch(tex.target()) {
+		case gli::TARGET_1D: bimg = create_image<image_1d>(format, tex); break;
+		case gli::TARGET_2D: bimg = create_image<image_2d>(format, tex); break;
+		case gli::TARGET_3D: bimg = create_image<image_3d>(format, tex); break;
+		case gli::TARGET_CUBE: bimg = create_image<image_cube>(format, tex); break;
+		case gli::TARGET_1D_ARRAY: bimg = create_image_layered<image_1d_layered>(format, tex); break;
+		case gli::TARGET_2D_ARRAY: bimg = create_image_layered<image_2d_layered>(format, tex); break;
+		case gli::TARGET_CUBE_ARRAY: bimg = create_image_layered<image_cube_layered>(format, tex); break;
+		default:
+			raise_error_flag(std::make_exception_ptr(mce::graphics_exception("Unsupported texture type.")));
+			break;
+		}
 
-	auto this_shared = this->shared_from_this();
-	current_state_ = state::staging;
-	lock.unlock();
-	std::vector<vk::BufferImageCopy> regions;
-	auto base = static_cast<const char*>(tex.data());
-	for(std::size_t layer = 0; layer < tex.layers(); ++layer) {
-		for(std::size_t face = 0; face < tex.faces(); ++face) {
-			for(std::size_t level = 0; level < tex.levels(); ++level) {
-				vk::DeviceSize offset = static_cast<const char*>(tex.data(layer, face, level)) - base;
-				auto ext = tex.extent(level);
-				regions.emplace_back(offset, 0, 0, vk::ImageSubresourceLayers(
-														   vk::ImageAspectFlagBits::eColor, uint32_t(level),
-														   uint32_t(layer * tex.faces() + face), 1),
-									 vk::Offset3D(0, 0, 0), vk::Extent3D(ext.x, ext.y, ext.z));
+		auto this_shared = this->shared_from_this();
+		current_state_ = state::staging;
+		lock.unlock();
+		std::vector<vk::BufferImageCopy> regions;
+		auto base = static_cast<const char*>(tex.data());
+		for(std::size_t layer = 0; layer < tex.layers(); ++layer) {
+			for(std::size_t face = 0; face < tex.faces(); ++face) {
+				for(std::size_t level = 0; level < tex.levels(); ++level) {
+					vk::DeviceSize offset = static_cast<const char*>(tex.data(layer, face, level)) - base;
+					auto ext = tex.extent(level);
+					regions.emplace_back(offset, 0, 0,
+										 vk::ImageSubresourceLayers(vk::ImageAspectFlagBits::eColor,
+																	uint32_t(level),
+																	uint32_t(layer * tex.faces() + face), 1),
+										 vk::Offset3D(0, 0, 0), vk::Extent3D(ext.x, ext.y, ext.z));
+				}
 			}
 		}
-	}
 
-	mgr_.transfer_mgr_.upload_image(tex.data(), tex.size(), *bimg, vk::ImageLayout::eShaderReadOnlyOptimal,
-									regions, [this_shared](vk::Image) { this_shared->complete_staging(); });
+		mgr_.transfer_mgr_.upload_image(tex.data(), tex.size(), *bimg,
+										vk::ImageLayout::eShaderReadOnlyOptimal, regions,
+										[this_shared](vk::Image) { this_shared->complete_staging(); });
+	} catch(...) {
+		raise_error_flag(std::current_exception());
+	}
 }
 
 void texture::complete_staging() noexcept {
