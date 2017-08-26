5ca2f8e725cf5f4dc60e1d7e27fffca293e1c3f3
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-26 03:10:02 +0200

    Implemented material_manager::internal_load_material.
    See issue #67

--- multicore_engine_renderer/src/rendering/material_manager.cpp
+++ multicore_engine_renderer/src/rendering/material_manager.cpp
@@ -58,6 +58,54 @@ std::shared_ptr<material_library> material_manager::internal_load_material_lib(c
 		}
 	}
 }
+std::shared_ptr<material> material_manager::internal_load_material(const std::string& name) {
+	{
+		// Acquire read lock
+		std::shared_lock<std::shared_timed_mutex> lock(rw_lock_);
+		auto it = loaded_materials_.find(name);
+		if(it != loaded_materials_.end()) {
+			return it->second;
+		}
+	}
+	{
+		// Acquire write lock
+		std::unique_lock<std::shared_timed_mutex> lock(rw_lock_);
+		// Double check if the material is still not in the map.
+		auto it = loaded_materials_.find(name);
+		if(it != loaded_materials_.end()) {
+			return it->second;
+		} else {
+			const material_description* desc = nullptr;
+			std::vector<std::shared_ptr<material_library>> pending_libs;
+			for(const auto& lib : loaded_material_libs_) {
+				if(lib.second->ready()) {
+					desc = lib.second->find_material_description(name);
+				} else {
+					pending_libs.push_back(lib.second);
+				}
+			}
+			if(!desc && pending_libs.empty()) {
+				throw mce::path_not_found_exception("Material '" + name + "' not found.");
+			}
+			auto tmp = std::make_shared<material>(name);
+			loaded_materials_[name] = tmp;
+			if(desc) {
+				tmp->try_start_loading(tex_mgr, *desc);
+				return tmp;
+			}
+			lock.unlock();
+			for(const auto& pl : pending_libs) {
+				pl->run_when_ready(
+						[tmp, this](const material_library_ptr& lib) { process_pending_material_loads(lib); },
+						[tmp](std::exception_ptr e) {
+							// TODO Handle failed loads.
+							static_cast<void>(e);
+						});
+			}
+			return tmp;
+		}
+	}
+}
 
 } /* namespace rendering */
 } /* namespace mce */
