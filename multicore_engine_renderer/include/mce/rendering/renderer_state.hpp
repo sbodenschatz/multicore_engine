6e754be0e784c5bd92dbdf6ee7b97e29c763fe05
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-27 17:06:20 +0200

    Added class stub for renderer_state.
    See issue #117

---
+++ multicore_engine_renderer/include/mce/rendering/renderer_state.hpp
@@ -0,0 +1,24 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/rendering/renderer_state.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_RENDERING_RENDERER_STATE_HPP_
+#define MCE_RENDERING_RENDERER_STATE_HPP_
+
+#include <mce/core/system_state.hpp>
+
+namespace mce {
+namespace rendering {
+
+class renderer_state : public core::system_state {
+public:
+	renderer_state(core::system* sys);
+	~renderer_state();
+};
+
+} /* namespace rendering */
+} /* namespace mce */
+
+#endif /* MCE_RENDERING_RENDERER_STATE_HPP_ */
---
+++ multicore_engine_renderer/src/rendering/renderer_state.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/rendering/renderer_state.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/rendering/renderer_state.hpp>
+
+namespace mce {
+namespace rendering {
+
+renderer_state::renderer_state(core::system* sys) : system_state(sys) {
+	// TODO Auto-generated constructor stub
+}
+
+renderer_state::~renderer_state() {}
+
+} /* namespace rendering */
+} /* namespace mce */
