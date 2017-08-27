b3b628cbf63837d84589b54dd77993ef539873f7
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-27 02:05:20 +0200

    Documented graphics/sync_utils.hpp.

--- multicore_engine_renderer/include/mce/graphics/sync_utils.hpp
+++ multicore_engine_renderer/include/mce/graphics/sync_utils.hpp
@@ -7,13 +7,24 @@
 #ifndef MCE_GRAPHICS_SYNC_UTILS_HPP_
 #define MCE_GRAPHICS_SYNC_UTILS_HPP_
 
+/**
+ * \file
+ * Defines helper functions for synchronization in vulkan.
+ * - flag_for_layout functions can be used to calculate access flags for layout transitions based on the
+ * layout as checked by validation layers.
+ */
+
 #include <vulkan/vulkan.hpp>
 
 namespace mce {
 namespace graphics {
 
+/// Returns all access flags required for layout transitions from / to the given layout.
 vk::AccessFlags required_flags_for_layout(vk::ImageLayout layout);
+/// \brief Returns all access flags that can optionally be specified for layout transitions from / to the
+/// given layout.
 vk::AccessFlags optional_flags_for_layout(vk::ImageLayout layout);
+/// Returns all access flags that are valid for layout transitions from / to the given layout.
 vk::AccessFlags allowed_flags_for_layout(vk::ImageLayout layout);
 
 } /* namespace graphics */
