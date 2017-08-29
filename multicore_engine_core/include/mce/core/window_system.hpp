32329f25a8bc9a9a9a3f57b3841bd24e0cae277b
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-30 01:18:09 +0200

    Implemented core::window_system.

---
+++ multicore_engine_core/include/mce/core/window_system.hpp
@@ -0,0 +1,53 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/mce/core/window_system.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_CORE_WINDOW_SYSTEM_HPP_
+#define MCE_CORE_WINDOW_SYSTEM_HPP_
+
+#include <cassert>
+#include <mce/core/system.hpp>
+#include <mce/glfw/instance.hpp>
+#include <memory>
+
+namespace mce {
+namespace glfw {
+class window;
+} // namespace glfw
+namespace core {
+class engine;
+
+class window_system : public system {
+	engine& eng;
+	glfw::instance instance_;
+	std::unique_ptr<glfw::window> window_;
+
+public:
+	window_system(engine& eng);
+	virtual ~window_system();
+
+	const glfw::instance& instance() const {
+		return instance_;
+	}
+
+	glfw::instance& instance() {
+		return instance_;
+	}
+
+	const glfw::window& window() const {
+		assert(window_);
+		return *window_;
+	}
+
+	glfw::window& window() {
+		assert(window_);
+		return *window_;
+	}
+};
+
+} /* namespace core */
+} /* namespace mce */
+
+#endif /* MCE_CORE_WINDOW_SYSTEM_HPP_ */
---
+++ multicore_engine_core/src/core/window_system.cpp
@@ -0,0 +1,21 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/src/core/window_system.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/core/window_system.hpp>
+#include <mce/glfw/window.hpp>
+
+namespace mce {
+namespace core {
+
+window_system::window_system(engine& eng) : eng{eng}, instance_() {
+	// TODO Parameterize
+	window_ = std::make_unique<glfw::window>("multicore_engine", glm::ivec2{800, 600});
+}
+
+window_system::~window_system() {}
+
+} /* namespace core */
+} /* namespace mce */
