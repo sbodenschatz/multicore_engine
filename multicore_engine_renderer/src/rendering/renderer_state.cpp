eda35be7f2f3c1acfd553fbd89142ebb2f4aa352
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-29 08:01:28 +0000

    Fixed build error on gcc.
    See issue #117

--- multicore_engine_renderer/src/rendering/renderer_state.cpp
+++ multicore_engine_renderer/src/rendering/renderer_state.cpp
@@ -15,10 +15,10 @@ renderer_state::renderer_state(core::system* sys) : system_state(sys) {}
 renderer_state::~renderer_state() {}
 
 void renderer_state::register_with_entity_manager(entity::entity_manager& em) {
-	REGISTER_COMPONENT_TYPE_SIMPLE(em, camera, create_camera_component(owner, config), this);
-	REGISTER_COMPONENT_TYPE_SIMPLE(em, point_light, create_point_light_component(owner, config), this);
-	REGISTER_COMPONENT_TYPE_SIMPLE(em, static_model, create_static_model_component(*this, owner, config),
-								   this);
+	REGISTER_COMPONENT_TYPE_SIMPLE(em, camera, this->create_camera_component(owner, config), this);
+	REGISTER_COMPONENT_TYPE_SIMPLE(em, point_light, this->create_point_light_component(owner, config), this);
+	REGISTER_COMPONENT_TYPE_SIMPLE(em, static_model,
+								   this->create_static_model_component(*this, owner, config), this);
 }
 
 } /* namespace rendering */
