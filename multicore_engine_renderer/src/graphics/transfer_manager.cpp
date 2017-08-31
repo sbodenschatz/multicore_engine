0e9cdae808fd6f11c90fedfbafc374032cac9da6
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-31 12:29:55 +0200

    Extracted buffer queue ownership transfer from transfer manager.
    See issue #118

--- multicore_engine_renderer/src/graphics/transfer_manager.cpp
+++ multicore_engine_renderer/src/graphics/transfer_manager.cpp
@@ -60,17 +60,12 @@ void transfer_manager::record_buffer_copy(void* staging_ptr, size_t data_size, v
 			staging_buffer.native_buffer(), dst_buffer,
 			{{chunk_placer.to_offset(staging_ptr), dst_offset, data_size}});
 	if(dev.graphics_queue_index().first != dev.transfer_queue_index().first) {
-		transfer_command_bufers[current_ring_index]->pipelineBarrier(
-				vk::PipelineStageFlagBits::eTransfer, vk::PipelineStageFlagBits::eAllCommands, {}, {},
-				{vk::BufferMemoryBarrier(vk::AccessFlagBits::eTransferWrite, {},
-										 dev.transfer_queue_index().first, dev.graphics_queue_index().first,
-										 dst_buffer, 0, VK_WHOLE_SIZE)},
-				{});
-		pending_ownership_command_buffers[current_ring_index]->pipelineBarrier(
-				vk::PipelineStageFlagBits::eTransfer, vk::PipelineStageFlagBits::eAllCommands, {}, {},
-				{vk::BufferMemoryBarrier({}, ~vk::AccessFlags{}, dev.transfer_queue_index().first,
-										 dev.graphics_queue_index().first, dst_buffer, 0, VK_WHOLE_SIZE)},
-				{});
+		buffer_queue_ownership_transfer(dst_buffer, transfer_command_bufers[current_ring_index].get(),
+										pending_ownership_command_buffers[current_ring_index].get(),
+										dev.transfer_queue_index().first, dev.graphics_queue_index().first,
+										vk::PipelineStageFlagBits::eTransfer,
+										vk::PipelineStageFlagBits::eAllCommands,
+										vk::AccessFlagBits::eTransferWrite, ~vk::AccessFlags{});
 	}
 }
 void transfer_manager::record_image_copy(void* staging_ptr, size_t data_size, vk::Image dst_img,
