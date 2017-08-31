7c2cd7b8701005fb0bb56f431f06221c98a58510
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-31 21:58:49 +0200

    Defined descriptor set layouts for renderer_system.
    See issue #118

--- multicore_engine_renderer/src/rendering/renderer_system.cpp
+++ multicore_engine_renderer/src/rendering/renderer_system.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <mce/core/engine.hpp>
+#include <mce/graphics/descriptor_set_layout.hpp>
 #include <mce/graphics/graphics_system.hpp>
 #include <mce/rendering/renderer_system.hpp>
 
@@ -13,8 +14,40 @@ namespace rendering {
 
 renderer_system::renderer_system(core::engine& eng, graphics::graphics_system& gs)
 		: gs_{gs}, mdl_mgr(eng.model_data_manager(), gs.device(), gs.memory_manager(),
-				  		   &(gs.destruction_queue_manager()), gs.transfer_manager()),
-		  mat_mgr(eng.asset_manager(), gs.texture_manager()) {}
+						   &(gs.destruction_queue_manager()), gs.transfer_manager()),
+		  mat_mgr(eng.asset_manager(), gs.texture_manager()) {
+	gs_.graphics_manager().create_descriptor_set_layout(
+			"per_scene",
+			{graphics::descriptor_set_layout_binding_element{
+					0, vk::DescriptorType::eUniformBuffer, 1, vk::ShaderStageFlagBits::eAllGraphics, {}}});
+	gs_.graphics_manager().create_descriptor_set_layout(
+			"per_material",
+			{graphics::descriptor_set_layout_binding_element{0, // Albedo
+															 vk::DescriptorType::eCombinedImageSampler,
+															 1,
+															 vk::ShaderStageFlagBits::eFragment,
+															 {/*TODO*/}},
+			 graphics::descriptor_set_layout_binding_element{1, // Normal map
+															 vk::DescriptorType::eCombinedImageSampler,
+															 1,
+															 vk::ShaderStageFlagBits::eFragment,
+															 {/*TODO*/}},
+			 graphics::descriptor_set_layout_binding_element{2, // Material
+															 vk::DescriptorType::eCombinedImageSampler,
+															 1,
+															 vk::ShaderStageFlagBits::eFragment,
+															 {/*TODO*/}},
+			 graphics::descriptor_set_layout_binding_element{3, // Emission
+															 vk::DescriptorType::eCombinedImageSampler,
+															 1,
+															 vk::ShaderStageFlagBits::eFragment,
+															 {/*TODO*/}}});
+	/* Currently unneeded because the model matrix can be passed using push constants.
+	 gs_.graphics_manager().create_descriptor_set_layout(
+			"per_scene",
+			{{0, vk::DescriptorType::eUniformBuffer, 1, vk::ShaderStageFlagBits::eAllGraphics, {}}});
+	*/
+}
 
 renderer_system::~renderer_system() {}
 
