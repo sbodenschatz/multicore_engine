985ff4a93fce89c03fc8ada152ff62fabb46c0a9
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-02 00:34:29 +0000

    Fixed unmatched suppression for cppcheck.
    See issue #118

--- multicore_engine_renderer/src/rendering/technique.cpp
+++ multicore_engine_renderer/src/rendering/technique.cpp
@@ -12,10 +12,8 @@
 namespace mce {
 namespace rendering {
 
-// cppcheck-suppress passedByValue
 technique::technique(core::engine& eng, renderer_system& sys, std::string name,
 					 graphics::window& swapchain_window,
-					 // cppcheck-suppress passedByValue
 					 std::vector<graphics::framebuffer_attachment_config> attachment_configs)
 		: eng{eng}, sys{sys}, name_{std::move(name)} {
 	framebuffer_config_ = sys.gs_.graphics_manager().create_framebuffer_config(
