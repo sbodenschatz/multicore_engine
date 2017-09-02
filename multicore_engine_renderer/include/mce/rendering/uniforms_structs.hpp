2c9a1588b2bd46aac6a7dd59cdeb3f39e7c9e2b4
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-03 01:57:52 +0200

    Documented uniforms_struct.hpp.
    See issue #118

--- multicore_engine_renderer/include/mce/rendering/uniforms_structs.hpp
+++ multicore_engine_renderer/include/mce/rendering/uniforms_structs.hpp
@@ -7,25 +7,33 @@
 #ifndef MCE_RENDERING_UNIFORMS_STRUCTS_HPP_
 #define MCE_RENDERING_UNIFORMS_STRUCTS_HPP_
 
+/**
+ * Defines structs for uniform and push constant data.
+ */
+
 namespace mce {
 namespace rendering {
 
+/// Defines the uniform data required for each point light.
 struct per_point_light_uniforms {
-	glm::vec3 position;
-	glm::vec3 color;
-	float radius;
+	glm::vec3 position; ///< The position of the light in world coordinates.
+	glm::vec3 color;	///< The color of the light.
+	float radius;		///< The radius of influence of the light.
 };
 
+/// The maximum number of lights supported in the forward rendering pass.
 constexpr size_t max_forward_lights = 8;
 
+/// Defines the uniform data passed to the shader per scene.
 struct per_scene_uniforms {
-	glm::mat4 projection;
-	glm::mat4 view;
-	per_point_light_uniforms forward_lights[max_forward_lights];
+	glm::mat4 projection;										 ///< The projection matrix of the camera.
+	glm::mat4 view;												 ///< The view matrix of the camera.
+	per_point_light_uniforms forward_lights[max_forward_lights]; ///< Lights supplied for froward rendering.
 };
 
+/// Defines the push constant data passed to the shader per object.
 struct per_object_push_constants {
-	glm::mat4 model;
+	glm::mat4 model; ///< The model matrix representing the transform applied to the object.
 };
 
 } // namespace rendering
