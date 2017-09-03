2b42b694ea3cd14ecce3fcc318e4652c553c19e1
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-04 01:17:39 +0200

    Added class stub for graphics::shader_loader.
    See issue #118

---
+++ multicore_engine_renderer/include/mce/graphics/shader_loader.hpp
@@ -0,0 +1,22 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/include/mce/graphics/shader_loader.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_GRAPHICS_SHADER_LOADER_HPP_
+#define MCE_GRAPHICS_SHADER_LOADER_HPP_
+
+namespace mce {
+namespace graphics {
+
+class shader_loader {
+public:
+	shader_loader();
+	~shader_loader();
+};
+
+} /* namespace graphics */
+} /* namespace mce */
+
+#endif /* MCE_GRAPHICS_SHADER_LOADER_HPP_ */
---
+++ multicore_engine_renderer/src/graphics/shader_loader.cpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_renderer/src/graphics/shader_loader.cpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#include <mce/graphics/shader_loader.hpp>
+
+namespace mce {
+namespace graphics {
+
+shader_loader::shader_loader() {
+	// TODO Auto-generated constructor stub
+}
+
+shader_loader::~shader_loader() {}
+
+} /* namespace graphics */
+} /* namespace mce */
