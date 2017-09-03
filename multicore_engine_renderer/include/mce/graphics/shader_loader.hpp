f8704d0b613f3b32d4b0a44a59e7a05402bbd706
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-04 01:32:00 +0200

    Defined structure for shader_loader.
    See issue #118

--- multicore_engine_renderer/include/mce/graphics/shader_loader.hpp
+++ multicore_engine_renderer/include/mce/graphics/shader_loader.hpp
@@ -9,11 +9,22 @@
 
 namespace mce {
 namespace graphics {
+class shader_module;
 
 class shader_loader {
+	asset::asset_manager& amgr;
+	graphics_manager& gmgr;
+	size_t pending_loads;
+	mutable std::mutex shaders_mtx;
+	mutable std::condition_variable shaders_cv;
+	boost::container::flat_map<std::string, std::shared_ptr<const shader_module>> shaders;
+
 public:
-	shader_loader();
+	shader_loader(asset::asset_manager& amgr, graphics_manager& gmgr);
 	~shader_loader();
+
+	void load_shader(const std::string& name);
+	std::shared_ptr<const shader_module> get_shader(const std::string& name) const;
 };
 
 } /* namespace graphics */
--- multicore_engine_renderer/src/graphics/shader_loader.cpp
+++ multicore_engine_renderer/src/graphics/shader_loader.cpp
@@ -9,7 +9,8 @@
 namespace mce {
 namespace graphics {
 
-shader_loader::shader_loader() {
+shader_loader::shader_loader(asset::asset_manager& amgr, graphics_manager& gmgr)
+		: amgr{amgr}, gmgr{gmgr}, pending_loads{0} {
 	// TODO Auto-generated constructor stub
 }
 
