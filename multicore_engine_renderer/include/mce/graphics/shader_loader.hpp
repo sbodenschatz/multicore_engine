50b72a82759956d40331d8c260c876086fd87278
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-04 01:54:55 +0200

    Implemented shader_loader::load_shader.
    See issue #118

--- multicore_engine_renderer/include/mce/graphics/shader_loader.hpp
+++ multicore_engine_renderer/include/mce/graphics/shader_loader.hpp
@@ -23,17 +23,17 @@ class graphics_manager;
 class shader_loader {
 	asset::asset_manager& amgr;
 	graphics_manager& gmgr;
-	size_t pending_loads;
+	size_t pending_loads = 0;
+	bool errors = false;
 	mutable std::mutex shaders_mtx;
 	mutable std::condition_variable shaders_cv;
-	boost::container::flat_map<std::string, std::shared_ptr<const shader_module>> shaders;
 
 public:
 	shader_loader(asset::asset_manager& amgr, graphics_manager& gmgr);
 	~shader_loader();
 
 	void load_shader(const std::string& name);
-	std::shared_ptr<const shader_module> get_shader(const std::string& name) const;
+	void wait_for_completion();
 };
 
 } /* namespace graphics */
--- multicore_engine_renderer/src/graphics/shader_loader.cpp
+++ multicore_engine_renderer/src/graphics/shader_loader.cpp
@@ -4,17 +4,47 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
+#include <mce/asset/asset_manager.hpp>
+#include <mce/graphics/graphics_manager.hpp>
 #include <mce/graphics/shader_loader.hpp>
 
 namespace mce {
 namespace graphics {
 
-shader_loader::shader_loader(asset::asset_manager& amgr, graphics_manager& gmgr)
-		: amgr{amgr}, gmgr{gmgr}, pending_loads{0} {}
+shader_loader::shader_loader(asset::asset_manager& amgr, graphics_manager& gmgr) : amgr{amgr}, gmgr{gmgr} {}
 
 shader_loader::~shader_loader() {
 	std::unique_lock<std::mutex> lock(shaders_mtx);
 	shaders_cv.wait(lock, [this]() { return pending_loads == 0; });
+}
+
+void shader_loader::load_shader(const std::string& name) {
+	amgr.load_asset_async(name + ".spv",
+						  [name, this](const asset::asset_ptr& shader_asset) {
+							  try {
+								  {
+									  std::lock_guard<std::mutex> lock(shaders_mtx);
+									  gmgr.create_shader_module(name, *shader_asset);
+									  --pending_loads;
+								  }
+								  shaders_cv.notify_one();
+							  } catch(...) {
+								  {
+									  std::lock_guard<std::mutex> lock(shaders_mtx);
+									  errors = true;
+									  --pending_loads;
+								  }
+								  shaders_cv.notify_one();
+							  }
+						  },
+						  [this](std::exception_ptr) {
+							  {
+								  std::lock_guard<std::mutex> lock(shaders_mtx);
+								  errors = true;
+								  --pending_loads;
+							  }
+							  shaders_cv.notify_one();
+						  });
 }
 
 } /* namespace graphics */
