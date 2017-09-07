0967520cd2624a0e9204bae9492df6715ce8a34c
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-07 22:58:32 +0200

    Implemented component creation for first_person_flyer_component in input_state.
    See issue #42

--- multicore_engine_core/include/mce/input/input_state.hpp
+++ multicore_engine_core/include/mce/input/input_state.hpp
@@ -7,15 +7,30 @@
 #ifndef MCE_INPUT_INPUT_STATE_HPP_
 #define MCE_INPUT_INPUT_STATE_HPP_
 
+#include <mce/containers/smart_object_pool.hpp>
+#include <mce/containers/smart_pool_ptr.hpp>
 #include <mce/core/system_state.hpp>
+#include <mce/input/first_person_flyer_component.hpp>
 
 namespace mce {
 namespace input {
 
 class input_state : public core::system_state {
+	containers::smart_object_pool<first_person_flyer_component> first_person_flyer_comps;
+
 public:
 	explicit input_state(mce::core::system* system);
 	~input_state();
+
+	/// Creates a first_person_flyer_component for the given entity and using the given configuration.
+	containers::smart_pool_ptr<first_person_flyer_component>
+	create_first_person_flyer_component(entity::entity& owner,
+										const entity::component_configuration& configuration) {
+		return first_person_flyer_comps.emplace(owner, configuration);
+	}
+
+	/// Registers the component types managed by input_state to the given entity_manager object.
+	void register_to_entity_manager(entity::entity_manager& em);
 };
 
 } /* namespace input */
--- multicore_engine_core/src/input/input_state.cpp
+++ multicore_engine_core/src/input/input_state.cpp
@@ -4,16 +4,20 @@
  * Copyright 2017 by Stefan Bodenschatz
  */
 
+#include <mce/entity/entity_manager.hpp>
 #include <mce/input/input_state.hpp>
 
 namespace mce {
 namespace input {
 
-input_state::input_state(mce::core::system* system) : system_state(system) {
-	// TODO Auto-generated constructor stub
-}
+input_state::input_state(mce::core::system* system) : system_state(system) {}
 
 input_state::~input_state() {}
+
+void input_state::register_to_entity_manager(entity::entity_manager& em) {
+	REGISTER_COMPONENT_TYPE_SIMPLE(em, first_person_flyer,
+								   this->create_first_person_flyer_component(owner, config), this);
+}
 
 } /* namespace input */
 } /* namespace mce */
