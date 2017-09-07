27e76395ed4cbc871e5225e56a8a1ad457e21d65
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-08 00:43:57 +0200

    Added utility function to create an inverse map for a given map.
    See issue #42

---
+++ multicore_engine_core/include/mce/util/map_utils.hpp
@@ -0,0 +1,28 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/mce/util/map_utils.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_UTIL_MAP_UTILS_HPP_
+#define MCE_UTIL_MAP_UTILS_HPP_
+
+#include <boost/container/flat_map.hpp>
+
+namespace mce {
+namespace util {
+
+template <typename Key, typename Val>
+boost::container::flat_map<Val, Key> inverse_map(const boost::container::flat_map<Key, Val>& map) {
+	boost::container::flat_map<Val, Key> res;
+	res.reserve(map.size());
+	for(const auto& elem : map) {
+		res.emplace(elem.second, elem.first);
+	}
+	return res;
+}
+
+} // namespace util
+} // namespace mce
+
+#endif /* MCE_UTIL_MAP_UTILS_HPP_ */
