fc9e3638ae5c0694ba917efedf89d4df9c6890d6
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-09 22:50:07 +0200

    Documented input::input_system.
    See issue #42

--- multicore_engine_headed/include/mce/input/input_system.hpp
+++ multicore_engine_headed/include/mce/input/input_system.hpp
@@ -7,6 +7,11 @@
 #ifndef MCE_INPUT_INPUT_SYSTEM_HPP_
 #define MCE_INPUT_INPUT_SYSTEM_HPP_
 
+/**
+ * \file
+ * Defines the system class for the input system.
+ */
+
 #include <boost/container/flat_map.hpp>
 #include <glm/vec2.hpp>
 #include <mce/core/system.hpp>
@@ -21,13 +26,17 @@ class window_system;
 } // namespace windowing
 namespace input {
 
+/// Encapsulates the state data related to the mouse.
 struct mouse_state {
-	glm::dvec2 position;
-	glm::dvec2 velocity;
-	glm::dvec2 acceleration;
+	glm::dvec2 position;	 ///< The position of the mouse in this state.
+	glm::dvec2 velocity;	 ///< The velocity of the mouse in this state.
+	glm::dvec2 acceleration; ///< The acceleration of the mouse in this state.
+	/// Indicates for each glfw::mouse_button whether it is pressed in this state.
 	bool buttons[size_t(glfw::mouse_button::last) + 1] = {};
 };
 
+/// \brief Implements a system that handles the logic for input processing (in combination with input_state
+/// and associated component classes).
 class input_system : public core::system {
 	core::engine& eng;
 	windowing::window_system& win_sys;
@@ -48,27 +57,34 @@ public:
 		return 0x1100;
 	}
 
+	/// Constructs the input system for the given engine and the given window_system required as a dependency.
 	input_system(core::engine& eng, windowing::window_system& win_sys);
+	/// Destroys the input_system and the associated resources.
 	~input_system();
 
+	/// Hook function to obtain current key and mouse state at the start of the frame.
 	void preprocess(const mce::core::frame_time& frame_time) override;
 
+	/// Returns a bool indicating whether the given glfw::key is pressed in the current frame.
 	bool current_key_state(glfw::key k) const noexcept {
 		auto it = current_key_state_.find(k);
 		if(it != current_key_state_.end()) return it->second;
 		return false;
 	}
 
+	/// Returns the mouse_state for the current frame.
 	const mouse_state& current_mouse_state() const noexcept {
 		return current_mouse_state_;
 	}
 
+	/// Returns a bool indicating whether the given glfw::key was pressed in the last frame.
 	bool last_key_state(glfw::key k) const noexcept {
 		auto it = last_key_state_.find(k);
 		if(it != last_key_state_.end()) return it->second;
 		return false;
 	}
 
+	/// Returns the mouse_state for the current frame.
 	const mouse_state& last_mouse_state() const noexcept {
 		return last_mouse_state_;
 	}
