5f7dd713f8dc4f0eec63adbe79736106bd907b37
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-09 23:04:26 +0200

    Documented glfw/name_mapping.hpp.
    See issue #42

--- multicore_engine_headed/include/mce/glfw/name_mapping.hpp
+++ multicore_engine_headed/include/mce/glfw/name_mapping.hpp
@@ -7,6 +7,11 @@
 #ifndef MCE_GLFW_NAME_MAPPING_HPP_
 #define MCE_GLFW_NAME_MAPPING_HPP_
 
+/**
+ * \file
+ * Defines functions to map keys and mouse buttons to strings and vice-versa.
+ */
+
 #include <boost/optional.hpp>
 #include <mce/glfw/glfw_defs.hpp>
 #include <string>
@@ -14,11 +19,17 @@
 namespace mce {
 namespace glfw {
 
+/// Returns a string representation for the given key.
 const std::string& to_string(key k);
+/// Returns the key represented by the given name or key::unknown if the name doesn't map to a valid key.
 key key_from_string(const std::string& name);
+/// Returns a vector listing all known keys.
 const std::vector<key>& all_keys();
 
+/// Returns a string representation for the given mouse_button.
 const std::string& to_string(mouse_button b);
+/// Returns the mouse_button represented by the given name or an empty optional if the name doesn't map to a
+/// valid mouse_button.
 boost::optional<mouse_button> mouse_button_from_string(const std::string& name);
 
 } /* namespace glfw */
