f9f5a12fe84b90c1142f9d3dc876ca02ae4d7e71
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-10 00:32:33 +0200

    Fixed possible infinite / NaN values through input processing.
    See issue #42

--- multicore_engine_headed/src/input/input_state.cpp
+++ multicore_engine_headed/src/input/input_state.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <mce/containers/smart_object_pool_range.hpp>
+#include <mce/core/core_defs.hpp>
 #include <mce/entity/entity_manager.hpp>
 #include <mce/glfw/window.hpp>
 #include <mce/input/input_state.hpp>
@@ -31,14 +32,16 @@ void input_state::register_to_entity_manager(entity::entity_manager& em) {
 }
 
 void input_state::process(const mce::core::frame_time& frame_time) {
-	const input_system& sys = *static_cast<input_system*>(system_);
-	tbb::parallel_for(containers::make_pool_range(first_person_flyer_comps),
-					  [&frame_time, &sys](const containers::smart_object_pool_range<decltype(
-												  first_person_flyer_comps)::iterator>& range) {
-						  for(auto& comp : range) {
-							  comp.process(frame_time, sys);
-						  }
-					  });
+	if(frame_time.delta_t > 0.0f) {
+		const input_system& sys = *static_cast<input_system*>(system_);
+		tbb::parallel_for(containers::make_pool_range(first_person_flyer_comps),
+						  [&frame_time, &sys](const containers::smart_object_pool_range<decltype(
+													  first_person_flyer_comps)::iterator>& range) {
+							  for(auto& comp : range) {
+								  comp.process(frame_time, sys);
+							  }
+						  });
+	}
 }
 
 } /* namespace input */
