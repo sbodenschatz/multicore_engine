14fe345c46d835c9487f0604e10e04431b4f1c29
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-11 23:36:32 +0200

    Documented actuator_system.
    See issue #168

--- multicore_engine_core/include/mce/simulation/actuator_system.hpp
+++ multicore_engine_core/include/mce/simulation/actuator_system.hpp
@@ -7,6 +7,11 @@
 #ifndef MCE_SIMULATION_ACTUATOR_SYSTEM_HPP_
 #define MCE_SIMULATION_ACTUATOR_SYSTEM_HPP_
 
+/**
+ * \file
+ * Defines the actuator_system class.
+ */
+
 #include <boost/container/flat_map.hpp>
 #include <mce/core/system.hpp>
 #include <mce/entity/ecs_types.hpp>
@@ -20,6 +25,17 @@ class engine;
 } // namespace core
 namespace simulation {
 
+/// Manages movement patterns that can be applied to entities using actuator_state and actuator_component.
+/**
+ * The movement patterns are defined by function objects that are callable using the signature
+ * <code>void(const mce::core::frame_time&, entity::entity&)</code>.
+ * These function objects are called once per frame and can manipulate the entity object given to them to
+ * create the desired movement. The registered function objects are copied into the actuator_component objects
+ * to provide each instance with a separate local state. The function objects on different actuator_component
+ * objects are called in parallel and are not synchronized by the actuator_system and must therefore only
+ * manipulate their owner entity and local state and read read-only objects or ensure synchronization for
+ * other accesses.
+ */
 class actuator_system : public core::system {
 	core::engine& eng_;
 	boost::container::flat_map<std::string, movement_pattern_function> movement_patterns_;
@@ -35,19 +51,26 @@ public:
 		return 0x1200;
 	}
 
+	/// Creates an actuator_system for the given engine objects.
 	explicit actuator_system(core::engine& eng);
+	/// Destroys the actuator_system.
 	~actuator_system();
 
+	/// \brief Sets the movement_pattern_function object for the given movement pattern name, replacing the
+	/// previous value if it was already set.
 	void set_movement_pattern(const std::string& name, const movement_pattern_function& pattern_function) {
 		std::lock_guard<std::mutex> lock(mtx);
 		movement_patterns_[name] = pattern_function;
 	}
 
+	/// \brief Looks up the movement_pattern_function with the given name, throwing a std::out_of_range
+	/// exception if no object was set for the given name.
 	movement_pattern_function find_movement_pattern(const std::string& name) const {
 		std::lock_guard<std::mutex> lock(mtx);
 		return movement_patterns_.at(name);
 	}
 
+	/// Removes the mapped movement_pattern_function object for the given name.
 	void remove_movement_pattern(const std::string& name) {
 		std::lock_guard<std::mutex> lock(mtx);
 		movement_patterns_.erase(name);
