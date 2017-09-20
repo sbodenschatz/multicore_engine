419403796ce6c0a52d6d48369a7ccd1cf1a40bae
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-09-21 00:39:00 +0200

    Created class stub for simple_smart_object_pool.
    See issue #172

---
+++ multicore_engine_core/include/mce/containers/simple_smart_object_pool.hpp
@@ -0,0 +1,19 @@
+/*
+ * Multi-Core Engine project
+ * File /multicore_engine_core/include/mce/containers/simple_smart_object_pool.hpp
+ * Copyright 2017 by Stefan Bodenschatz
+ */
+
+#ifndef MCE_CONTAINERS_SIMPLE_SMART_OBJECT_POOL_HPP_
+#define MCE_CONTAINERS_SIMPLE_SMART_OBJECT_POOL_HPP_
+
+namespace mce {
+namespace containers {
+
+template <typename T>
+class simple_smart_object_pool {};
+
+} // namespace containers
+} // namespace mce
+
+#endif /* MCE_CONTAINERS_SIMPLE_SMART_OBJECT_POOL_HPP_ */
