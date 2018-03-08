2eb023f0c30b71bdaeb494b22d2bab30fab1c93b
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2018-03-08 10:32:14 +0000

    Fixed breaking changes coming in from glm in component_ math tool functions.

--- multicore_engine_core/include/mce/util/math_tools.hpp
+++ multicore_engine_core/include/mce/util/math_tools.hpp
@@ -58,11 +58,11 @@ struct vector_size<glm::tvec4<T, p>> {
 };
 
 /// Returns the maximum value of all components a glm vector.
-template <typename T, glm::qualifier p, template <typename, glm::qualifier> class Vector_Type>
-T component_max(const Vector_Type<T, p>& v) {
+template <glm::length_t s, typename T, glm::qualifier p, template <glm::length_t, typename, glm::qualifier> class Vector_Type>
+T component_max(const Vector_Type<s, T, p>& v) {
 	using std::max;
 	T m = v[0];
-	for(int i = 1; i < vector_size<Vector_Type<T, p>>::value; ++i) {
+	for(int i = 1; i < vector_size<Vector_Type<s, T, p>>::value; ++i) {
 		m = max(m, v[i]);
 	}
 	return m;
@@ -75,10 +75,10 @@ T component_max(T v) {
 }
 
 /// Returns the minimum value of all components a glm vector.
-template <typename T, glm::qualifier p, template <typename, glm::qualifier> class Vector_Type>
-T component_min(const Vector_Type<T, p>& v) {
+template <glm::length_t s, typename T, glm::qualifier p, template <glm::length_t, typename, glm::qualifier> class Vector_Type>
+T component_min(const Vector_Type<s, T, p>& v) {
 	T m = v[0];
-	for(int i = 1; i < vector_size<Vector_Type<T, p>>::value; ++i) {
+	for(int i = 1; i < vector_size<Vector_Type<s, T, p>>::value; ++i) {
 		m = min(m, v[i]);
 	}
 	return m;
@@ -91,10 +91,10 @@ T component_min(T v) {
 }
 
 /// Returns the sum of all components a glm vector.
-template <typename T, glm::qualifier p, template <typename, glm::qualifier> class Vector_Type>
-T component_add(const Vector_Type<T, p>& v) {
+template <glm::length_t s, typename T, glm::qualifier p, template <glm::length_t, typename, glm::qualifier> class Vector_Type>
+T component_add(const Vector_Type<s, T, p>& v) {
 	T m = v[0];
-	for(int i = 1; i < vector_size<Vector_Type<T, p>>::value; ++i) {
+	for(int i = 1; i < vector_size<Vector_Type<s, T, p>>::value; ++i) {
 		m = m + v[i];
 	}
 	return m;
@@ -107,10 +107,10 @@ T component_add(T v) {
 }
 
 /// Returns the product of all components a glm vector.
-template <typename T, glm::qualifier p, template <typename, glm::qualifier> class Vector_Type>
-T component_mul(const Vector_Type<T, p>& v) {
+template <glm::length_t s, typename T, glm::qualifier p, template <glm::length_t, typename, glm::qualifier> class Vector_Type>
+T component_mul(const Vector_Type<s, T, p>& v) {
 	T m = v[0];
-	for(int i = 1; i < vector_size<Vector_Type<T, p>>::value; ++i) {
+	for(int i = 1; i < vector_size<Vector_Type<s, T, p>>::value; ++i) {
 		m = m * v[i];
 	}
 	return m;
