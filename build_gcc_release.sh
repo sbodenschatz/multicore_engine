7e39a3bf285c6ee63955db84395f968e1a3e3448
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2018-03-08 10:24:23 +0000

    Reduced build parallelism to not overwhelm private cloud instances.

--- build_gcc_release.sh
+++ build_gcc_release.sh
@@ -2,6 +2,6 @@
 cmake -E make_directory build_gcc_release
 cd build_gcc_release
 cmake -U*_INCLUDE -U*_LIBS -ULIBS_DIR -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . -- -j 16
+cmake --build . -- -j 10
 echo "export LD_LIBRARY_PATH=/usr/local/lib/gcc-release/"
 
