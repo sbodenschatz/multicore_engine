cd46f861b36c559c3a2031d327815d6c9883fb2b
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2018-04-27 13:37:34 +0200

    Fixed create_license_pack_linux.sh.

--- create_license_pack_linux.sh
+++ create_license_pack_linux.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+SRCDIR=$(readlink -f $(dirname "$0"))
 mkdir -p licenses/linux
 cd licenses/linux
 mkdir Boost glfw gli glm TBB vulkan-sdk zlib libc++ libstdc++ libgcc llvm-runtime g++ gcc-base gtest gmock vkglformat
 cp /usr/local/share/doc/boost/LICENSE_1_0.txt Boost
 cp /usr/share/doc/libglfw3/copyright glfw
-cp //usr/local/share/doc/gli/manual.md gli
-cp //usr/local/share/doc/gli/readme.md gli
+cp /usr/local/share/doc/gli/manual.md gli
+cp /usr/local/share/doc/gli/readme.md gli
 cp /usr/local/share/doc/glm/manual.md glm
 cp /usr/local/share/doc/glm/readme.md glm
-cp /usr/share/doc/libtbb-dev/copyright TBB
+cp /usr/local/share/doc/tbb/* TBB
 cp /usr/local/share/doc/vulkan-sdk/LICENSE.txt vulkan-sdk
 cp /usr/local/share/doc/vulkan-sdk/sdk_license.html vulkan-sdk
 cp /usr/share/doc/zlib1g-dev/copyright zlib
@@ -22,5 +23,5 @@ cp /usr/local/share/doc/googletest/LICENSE gtest
 cp /usr/local/share/doc/googletest/CONTRIBUTORS gtest
 cp /usr/local/share/doc/googlemock/LICENSE gmock
 cp /usr/local/share/doc/googlemock/CONTRIBUTORS gmock
-cp /usr/local/share/doc/vkglformat/LICENSE  vkglformat
-cp /usr/local/share/doc/vkglformat/Source.txt  vkglformat
+cp $SRCDIR/vkglformat/LICENSE  vkglformat
+cp $SRCDIR/vkglformat/Source.txt  vkglformat
