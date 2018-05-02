0f0406c8a3c1a11127e1e0a3669a7335d07469d1
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2018-05-02 18:25:24 +0200

    Let shared_libs_copy return the collected list of shared libraries.

--- cmake_modules/SharedLibsCopy.cmake
+++ cmake_modules/SharedLibsCopy.cmake
@@ -1,14 +1,14 @@
-function(shared_libs_copy EXE_TARGET)
+function(shared_libs_copy EXE_TARGET OUTPUT_LIST)
 	if(WIN32)
 		set(file_list)
 		foreach(LIB_TARGET ${ARGN})
 			get_target_property(LOC_DEBUG ${LIB_TARGET} IMPORTED_LOCATION_DEBUG)
 			get_target_property(LOC_RELEASE ${LIB_TARGET} IMPORTED_LOCATION_RELEASE)
 			list(APPEND file_list 
-				$<$<CONFIG:DEBUG>:"${LOC_DEBUG}">
-				$<$<CONFIG:RELWITHDEBINFO>:"${LOC_RELEASE}">
-				$<$<CONFIG:MINSIZEREL>:"${LOC_RELEASE}">
-				$<$<CONFIG:RELEASE>:"${LOC_RELEASE}">
+				$<$<CONFIG:DEBUG>:${LOC_DEBUG}>
+				$<$<CONFIG:RELWITHDEBINFO>:${LOC_RELEASE}>
+				$<$<CONFIG:MINSIZEREL>:${LOC_RELEASE}>
+				$<$<CONFIG:RELEASE>:${LOC_RELEASE}>
 			)
 		endforeach()
 		add_custom_command(
@@ -18,4 +18,5 @@ function(shared_libs_copy EXE_TARGET)
 				$<TARGET_FILE_DIR:${EXE_TARGET}>
 			)
 	endif()
+	set(${OUTPUT_LIST} ${file_list} PARENT_SCOPE)
 endfunction()
