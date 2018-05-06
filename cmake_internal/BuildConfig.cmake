f944edb268c7d53b82c66edd5e92d8462e883a73
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2018-05-06 22:32:27 +0200

    Disable linker warning regarding missing debug symbol files.

--- cmake_internal/BuildConfig.cmake
+++ cmake_internal/BuildConfig.cmake
@@ -7,7 +7,7 @@ include(BuildTypeSettings)
 include(LibsDir)
 
 if (MSVC)
-	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /WX /ignore:4221 notelemetry.obj")
+	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /WX /ignore:4221 /ignore:4099 notelemetry.obj")
 	set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} /WX /ignore:4221 notelemetry.obj")
 
 	set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} /debug:fastlink /INCREMENTAL")
